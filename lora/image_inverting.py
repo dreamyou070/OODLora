946aad4b037e545f6bf49c546e2d870a2b557e7c
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-08 23:39:35 +0900

    Update image_inverting.py

--- lora/image_inverting.py
+++ lora/image_inverting.py
@@ -63,7 +63,8 @@ def register_attention_control(unet : nn.Module, controller:AttentionStore) :
                                                         value_value=value.detach().cpu(),
                                                         layer_name=layer_name)
             if is_cross_attention :
-                controller.cross_key_caching(key_value=query.detach().cpu(),)
+                controller.cross_key_caching(key_value=query.detach().cpu(),
+                                             layer_name=layer_name)
 
             hidden_states = torch.bmm(attention_probs, value)
             hidden_states = self.reshape_batch_dim_to_heads(hidden_states)
