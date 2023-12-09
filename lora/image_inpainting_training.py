64258f6a9cb5d799d450cae8d915dc54c6b67f19
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-09 23:06:54 +0900

    Update image_inpainting_training.py

--- lora/image_inpainting_training.py
+++ lora/image_inpainting_training.py
@@ -9,10 +9,11 @@ def main():
     print(f'\n step 1. make model')
     device = 'cuda:1'
     pipe = StableDiffusionInpaintPipeline.from_pretrained("runwayml/stable-diffusion-inpainting",
-                                                          cache_dir = r'/data7/sooyeon/pretrained_stable_diffusion/models--runwayml--stable-diffusion-inpainting').to(device)
+                                                          cache_dir = r'/data7/sooyeon/pretrained_stable_diffusion').to(device)
     vae = pipe.vae
     text_encoder = pipe.text_encoder
-    unet = UNet2DConditionModel.from_config(config_dir = '/data7/sooyeon/pretrained_stable_diffusion/models--runwayml--stable-diffusion-inpainting/snapshots/51388a731f57604945fddd703ecb5c50e8e7b49d/unet/config.json').to(device)
+    unet_config_dir = '/data7/sooyeon/pretrained_stable_diffusion/models--runwayml--stable-diffusion-inpainting/snapshots/51388a731f57604945fddd703ecb5c50e8e7b49d/unet/config.json'
+    unet = UNet2DConditionModel.from_config(unet_config_dir).to(device)
 
 if __name__ == "__main__":
     main()
