1027ac4a013db9b4806ba18b531986219e65d02f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-09 14:22:29 +0900

    Update lora.py

--- lora/networks/lora.py
+++ lora/networks/lora.py
@@ -121,7 +121,7 @@ class LoRAModule(torch.nn.Module):
         #del self.org_module
 
     def forward(self, x):
-        print(f'infer with lora')
+        print(f'infer with lora' )
         org_forwarded = self.org_forward(x)
         # module dropout
         if self.module_dropout is not None and self.training:
