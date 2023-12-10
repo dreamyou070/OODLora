e7593813d6a72d050ce5d1da04fc1eeb8a6d8c1b
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-10 22:01:50 +0900

    Update train_erasing_concept.py

--- lora/train_erasing_concept.py
+++ lora/train_erasing_concept.py
@@ -451,7 +451,7 @@ class NetworkTrainer:
         for t_enc in text_encoders:
             t_enc.requires_grad_(False)
 
-        from diffusers.image_processors import VaeImageProcessor
+        from diffusers.image_processor import VaeImageProcessor
         mask_processor = VaeImageProcessor(vae_scale_factor=self.vae_scale_factor,
                                            do_normalize=False,
                                            do_binarize=True,
