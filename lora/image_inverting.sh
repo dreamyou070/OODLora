1a2a3cfb188798eb535281daedcf01ad484855f1
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-11 10:14:12 +0900

    .

--- lora/image_inverting.sh
+++ lora/image_inverting.sh
@@ -1,10 +1,10 @@
-python image_inverting.py --device cuda:1 \
+python image_inverting.py --device cuda:6 \
   --process_title parksooyeon \
   --pretrained_model_name_or_path /data7/sooyeon/pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned-emaonly.ckpt \
   --network_module networks.lora \
   --network_dim 64 --network_alpha 4 \
-  --network_weights '/data7/sooyeon/Lora/OODLora/result/MVTec_experiment/bagel/3_strong_good_training/last.safetensors' \
-  --prompt 'good' --inversion_experiment --sample_sampler ddim --num_ddim_steps 50 \
-  --output_dir '/data7/sooyeon/Lora/OODLora/result/MVTec_experiment/bagel/3_strong_good_training/inference_check' \
+  --network_weights '/data7/sooyeon/Lora/OODLora/result/MVTec_experiment/bagel/6_img_negative_guidance/epoch-000040.safetensors' \
+  --prompt 'good' --inversion_experiment --sample_sampler ddim --num_ddim_steps 200 \
+  --output_dir '/data7/sooyeon/Lora/OODLora/result/MVTec_experiment/bagel/6_img_negative_guidance/inference_check' \
   --concept_image_folder /data7/sooyeon/MyData/anomaly_detection/MVTecAD/bagel \
-  --repeat_time 50 --threshold_time 1000 --cfg_check 1000 --inversion_weight 3.0
+  --repeat_time 50 --threshold_time 1000 --cfg_check 0 --inversion_weight 0.0 --interpolate_alpha 0.25
--- lora/train_erasing_concept.sh
+++ lora/train_erasing_concept.sh
@@ -1,20 +1,15 @@
 accelerate launch --config_file /data7/sooyeon/LyCORIS/gpu_config/gpu_6_7_config --main_process_port 26724 train_erasing_concept.py \
   --logging_dir ../result/logs --process_title parksooyeon --max_token_length 225 \
   --log_with wandb --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc \
-  --wandb_init_name bagel_normal_concept_trainingiom_pretraining --wandb_run_name 6_img_negative_guidance \
+  --wandb_init_name 'bagel_normal_concept_trainingiom_pretraining' --wandb_run_name 6_img_negative_guidance \
   --seed 42 --output_dir ../result/MVTec_experiment/bagel/6_img_negative_guidance \
   --pretrained_model_name_or_path /data7/sooyeon/pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
-  --network_module networks.lora \
+  --network_module networks.lora --network_dim 64 --network_alpha 4 --train_batch_size 2 \
+  --network_weights '/data7/sooyeon/Lora/OODLora/result/MVTec_experiment/bagel/6_img_negative_guidance/epoch-000040.safetensors' \
   --optimizer_type AdamW --lr_scheduler cosine_with_restarts --lr_warmup_steps 144 \
   --learning_rate 0.0003 --unet_lr 0.0001 --text_encoder_lr 0.00005 \
   --save_every_n_epochs 1 --sample_every_n_epochs 1 --sample_prompts /data7/sooyeon/MyData/object/bagel_inference.txt \
-  --max_train_steps 48000
-
-
-
-
-
-
-
-  --train_data_dir /data7/sooyeon/MyData/object/bagel/good \
-  --trg_concept 'good' --resolution 512,512 --net_key_names text --network_dim 64 --network_alpha 4 --train_batch_size 1 \
+  --max_train_steps 48000 --train_data_dir r'/data7/sooyeon/MyData/anomaly_detection/MVTecAD/bad' --resolution 512,512  \
+  --train_data_dir /data7/sooyeon/MyData/anomaly_detection/MVTecAD/bad \
+  --trg_concept 'good' \
+  --sample_prompts /data7/sooyeon/MyData/object/bagel_inference.txt
