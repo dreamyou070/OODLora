9b007aaaa86acd87814fa9bb2a6c41583a72ec9a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-14 23:31:33 +0900

    Update training_contrastive.py

--- lora/training_contrastive.py
+++ lora/training_contrastive.py
@@ -431,7 +431,7 @@ class NetworkTrainer:
                 len(train_dataloader) / accelerator.num_processes / args.gradient_accumulation_steps)
             accelerator.print(f"override steps. steps for {args.max_train_epochs} epochs is / 指定エポックまでのステップ数: {args.max_train_steps}")
         # データセット側にも学習ステップを送信
-        train_dataset_group.set_max_train_steps(args.max_train_steps)
+        #train_dataset_group.set_max_train_steps(args.max_train_steps)
 
         # lr schedulerを用意する
         lr_scheduler = train_util.get_scheduler_fix(args,
