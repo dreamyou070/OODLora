bdf33369060f07318687672fe002d1d6b9dcf7bf
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-15 11:20:36 +0900

    Update __init__.py

--- lora/dataset/__init__.py
+++ lora/dataset/__init__.py
@@ -27,7 +27,7 @@ class SYDataset(Dataset):
             image_class_dir = os.path.join(self.image_dir, image_class)
             mask_class_dir = os.path.join(self.masked_dir, image_class)
             image_names = os.listdir(image_class_dir)
-            for j in range(repeat) :
+            for j in range(int(repeat.strip())) :
                 for image_name in image_names :
                     image_path = os.path.join(image_class_dir, image_name)
                     mask_path = os.path.join(mask_class_dir, image_name)
