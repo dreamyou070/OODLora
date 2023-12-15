21a5866d43d411fc98c1ec618e85587e3a01826f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-15 15:33:56 +0900

    Update training_contrastive.sh

--- lora/training_contrastive.sh
+++ lora/training_contrastive.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # start of the script
+#!/bin/bash
+echo $CUDA_VISIBLE_DEVICES
+echo $SLURM_NODELIST
+echo $SLURM_NODEID
 
-#SBATCH --job-name=parksooyeon_job
-#SBATCH --gres=gpu:4
-#SBTACH --ntasks=4
-#SBATCH --tasks-per-node=4
-#SBATCH --cpus-per-task=1
-#SBATCH --nodes=1
-#SBATCH --output=example.out
-#SBATCH --time 0-23:00:00
-launch --config_file /data7/sooyeon/LyCORIS/gpu_config/gpu_4_5_config
-accelerate config ~~~ training_contrastive.py \
+source ~/.bashrc
+conda activate ttp
+ml purge
+ml load cuda/11.0
+
+accelerate training_contrastive.py \
   --logging_dir ../result/logs --process_title parksooyeon --max_token_length 225 \
   --log_with wandb --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc \
   --wandb_init_name bagel_training --wandb_run_name 2_contrastive_learning_eps_0.0_new_code_highrepeat \
