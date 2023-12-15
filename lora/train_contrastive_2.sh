5f98fad4ea8d603f984337f488353f9c2626517c
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-16 00:54:37 +0900

    Update train_contrastive_2.sh

--- lora/train_contrastive_2.sh
+++ lora/train_contrastive_2.sh
@@ -10,8 +10,8 @@
 #SBATCH --exclusive
 
 # sbatch -q big_qos --nodes 2 --output=../result/printing_screen/2_contrastive_learning_eps_0.0_new_code_highrepeat_test.txt training_contrastive.sh
-# sbatch -q big_qos --nodes 2 --gres=gpu:4 training_contrastive_2.sh
-module load intelmpi
+# sbatch --partition=compute-od-gpu -q big_qos --nodes 2 --gres=gpu:4 --ntasks-per-node 1 --cpus-per-gpu=6 train_contrastive_2.sh
+#module load intelmpi
 
 source ~/.bashrc
 conda activate venv
