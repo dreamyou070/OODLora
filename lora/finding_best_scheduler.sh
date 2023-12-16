b3577d6033944d63f05f5461dda7502812c8519a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-16 15:02:47 +0900

    Update finding_best_scheduler.sh

--- lora/finding_best_scheduler.sh
+++ lora/finding_best_scheduler.sh
@@ -20,15 +20,3 @@ source ~/.bashrc
 conda activate venv
 ml purge
 ml load cuda/11.0
-
-python finding_best_scheduler.py --device cuda \
-  --process_title parksooyeon --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned-emaonly.ckpt \
-  --network_module networks.lora --network_dim 64 --network_alpha 4 --prompt 'good' --sample_sampler ddim --num_ddim_steps 50 --output_dir ../../../result \
-  --network_weights ../result/MVTec_experiment/bagel/2_contrastive_learning_eps_0.0_new_code_highrepeat/epoch-000003.safetensors \
-  --concept_image_folder ../../../MyData/anomaly_detection/MVTecAD/bagel
-
-
-
-
-
-
