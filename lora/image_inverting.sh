4741a8b7492dbe6e3f6cff569038701905ab7ecb
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-16 16:34:16 +0900

    .

--- lora/image_inverting.sh
+++ lora/image_inverting.sh
@@ -2,7 +2,7 @@ python image_inverting.py --device cuda:5 \
   --process_title parksooyeon \
   --pretrained_model_name_or_path /data7/sooyeon/pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned-emaonly.ckpt \
   --network_module networks.lora --network_dim 64 --network_alpha 4 \
-  --network_weights /data7/sooyeon/Lora/OODLora/result/MVTec_experiment/bagel/3_contrastive_learning_eps_0.0_increase_generality/epoch-000003.safetensors \
+  --network_weights /data7/sooyeon/Lora/OODLora/result/MVTec_experiment/bagel/3_contrastive_learning_eps_0.0_increase_generality/epoch-000005.safetensors \
   --prompt 'good' \
   --inversion_experiment \
   --sample_sampler ddim \
--- sy.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-torch_a = torch.randn((4,64,64))
-a = [torch_a, torch_a]
-
-img = a[0]
-print(img.shape)
-b= torch.stack(a, dim=0)
-print(b.shape)
