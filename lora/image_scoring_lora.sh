3abfd9e20c086889c022f832666430b1762b0555
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-16 19:49:50 +0900

    Update image_scoring_lora.sh

--- lora/image_scoring_lora.sh
+++ lora/image_scoring_lora.sh
@@ -12,6 +12,6 @@ ml load cuda/11.0
 
 
 python image_scoring_lora.py --device cuda \
-  --pretrained_model_name_or_path /data7/sooyeon/pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned-emaonly.ckpt \
+  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned-emaonly.ckpt \
   --network_module networks.lora --network_dim 64 --network_alpha 4 \
-  --network_weights /data7/sooyeon/Lora/OODLora/result/MVTec_experiment/bagel/3_contrastive_learning_eps_0.0_increase_generality/epoch-000003.safetensors
+  --network_weights ../result/MVTec_experiment/bagel/model/epoch-000003.safetensors
