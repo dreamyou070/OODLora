dbc9939ee5dc3f06493807a51893938a01f05727
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-16 14:14:40 +0900

    Update finding_best_scheduler.sh

--- lora/finding_best_scheduler.sh
+++ lora/finding_best_scheduler.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-#SBATCH --partition=big_suma_rtx3090
-#SBATCH --qos big_qos
-#SBATCH --job-name=parksooyeon_finding_best_scheduler
-#SBATCH --nodes 1
-#SBATCH --ntasks-per-node 1
-#SBATCH --gres=gpu:2
-#SBATCH --output=./result/printing_screen/finding_best_scheduler.log
-
-# sbatch -q big_qos --nodes 2 --output=../result/printing_screen/2_contrastive_learning_eps_0.0_new_code_highrepeat_test.txt training_contrastive.sh
-# sbatch -p big_suma_rtx3090 --qos big_qos --job-name=parksooyeon_finding_best_scheduler --nodes 1 --ntasks-per-node 1 -gres=gpu:1 --output=./result/printing_screen/finding_best_scheduler.log finding_best_scheduler.sh
 
 echo $CUDA_VISIBLE_DEVICES
 echo $SLURM_NODELIST
 echo $SLURM_NODEID
+
 source ~/.bashrc
 conda activate venv
 ml purge
