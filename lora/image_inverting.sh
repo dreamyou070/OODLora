547ecd6bd9aa6e7b20bbe2acd990876d506b9c3a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-16 09:34:58 +0900

    .

--- lora/image_inverting.sh
+++ lora/image_inverting.sh
@@ -2,12 +2,12 @@ python image_inverting.py --device cuda:5 \
   --process_title parksooyeon \
   --pretrained_model_name_or_path /data7/sooyeon/pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned-emaonly.ckpt \
   --network_module networks.lora --network_dim 64 --network_alpha 4 \
-  --network_weights '../../../result/MVTec_experiment/bagel/2_contrastive_learning_eps_0.0_new_code_highrepeat/epoch-000005.safetensors' \
+  --network_weights /data7/sooyeon/Lora/OODLora/result/MVTec_experiment/bagel/2_contrastive_learning_eps_0.0_new_code_highrepeat/epoch-000005.safetensors \
   --prompt 'good' \
   --inversion_experiment \
   --sample_sampler ddim \
   --num_ddim_steps 50 \
-  --output_dir '../../../result/MVTec_experiment/bagel/2_contrastive_learning_eps_0.0_new_code_highrepeat' \
+  --output_dir /data7/sooyeon/Lora/OODLora/result/MVTec_experiment/bagel/2_contrastive_learning_eps_0.0_new_code_highrepeat \
   --concept_image_folder ../../../MyData/anomaly_detection/MVTecAD/bagel \
   --repeat_time 51 \
   --self_attn_threshold_time 10000 \
