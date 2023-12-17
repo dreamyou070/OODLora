3b000def151c942a1826a5846f6e6f63bcfcd3f4
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-17 19:48:23 +0900

    Update autoencoder_inference.py

--- lora/autoencoder_inference.py
+++ lora/autoencoder_inference.py
@@ -108,7 +108,7 @@ def main(args):
 
         # (2) decoder
         z = vae.post_quant_conv(latent)
-        recon_img = vae_decoder(z).sample
+        recon_img = vae_decoder(z)#.sample
         recon_img = (recon_img / 2 + 0.5).clamp(0, 1).cpu().permute(0, 2, 3, 1).numpy()[0]
         image = (recon_img * 255).astype(np.uint8)
         image = Image.fromarray(image)
