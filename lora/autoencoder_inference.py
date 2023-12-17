8574b74e8860a20674da4d7ac173e00237d61609
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-17 11:23:23 +0900

    autoencoder inference checking

--- lora/autoencoder_inference.py
+++ lora/autoencoder_inference.py
@@ -69,68 +69,32 @@ def main(args):
                                        norm="BATCH", bias=False, padding=1, )
 
     print(f' (3) loading state')
-    vae_pretrained_dir = r'/data7/sooyeon/result/MVTec_experiment/bagel/vae_training/'
-    discriminator_pretrained_dir = r'/data7/sooyeon/result/MVTec_experiment/bagel/vae_training'
+    vae_pretrained_dir = '/data7/sooyeon/Lora/OODLora/result/MVTec_experiment/bagel/vae_training/vae_model/vae_epoch_000001/pytorch_model.bin'
+    discriminator_pretrained_dir = '/data7/sooyeon/Lora/OODLora/result/MVTec_experiment/bagel/vae_training/discriminator_model_model/discriminator_model_epoch_000001/pytorch_model.bin'
+    vae.load_state_dict(torch.load(vae_pretrained_dir))
+    discriminator.load_state_dict(torch.load(discriminator_pretrained_dir))
+    vae.requires_grad_(False)
+    vae.eval()
+    vae.to(dtype=vae_dtype, device = args.device)
+    discriminator.requires_grad_(False)
+    discriminator.eval()
+    discriminator.to(dtype=vae_dtype, device = args.device)
 
-
-
-
-
-    vae.requires_grad_(True)
-    vae.train()
-    vae.to(dtype=vae_dtype)
-    #vae.to(dtype=weight_dtype)
-    discriminator.requires_grad_(True)
-    discriminator.train()
-    discriminator.to(dtype=weight_dtype)
-
-    l1_loss = L1Loss()
-    adv_loss = PatchAdversarialLoss(criterion="least_squares")
-    adv_weight = 0.01
-    perceptual_weight = 0.001
-
-
-    # epoch数を計算する
-    num_update_steps_per_epoch = math.ceil(len(train_dataloader) / args.gradient_accumulation_steps)
-    num_train_epochs = math.ceil(args.max_train_steps / num_update_steps_per_epoch)
-    if (args.save_n_epoch_ratio is not None) and (args.save_n_epoch_ratio > 0):
-        args.save_every_n_epochs = math.floor(num_train_epochs / args.save_n_epoch_ratio) or 1
-
-
-    if args.sample_every_n_epochs is not None and epoch+1 % args.sample_every_n_epochs == 0 :
-        print('sampling')
-        sample_data_dir = r'../../../MyData/anomaly_detection/VisA/MVTecAD/bagel/test/crack/rgb/000.png'
-        h,w = args.resolution
-        img = load_image(sample_data_dir, int(h), int(w))
-        img = IMAGE_TRANSFORMS(img).to(dtype=vae_dtype).unsqueeze(0)
-        with torch.no_grad():
-            if accelerator.is_main_process:
-                inf_vae = accelerator.unwrap_model(vae).to(dtype=vae_dtype)
-                img = img.to(inf_vae.device)
-                recon_img = inf_vae(img).sample
-                recon_img = (recon_img / 2 + 0.5).clamp(0, 1).cpu().permute(0, 2, 3, 1).numpy()[0]
-                image = (recon_img * 255).astype(np.uint8)
-                image = Image.fromarray(image)
-                save_dir = os.path.join(args.output_dir, 'sample')
-                os.makedirs(save_dir, exist_ok=True)
-                image.save(os.path.join(save_dir, f'anormal_recon_epoch_{epoch}.png'))
-        sample_data_dir = r'../../../MyData/anomaly_detection/VisA/MVTecAD/bagel/test/good/rgb/000.png'
-        img = load_image(sample_data_dir, int(h), int(w))
-        img = IMAGE_TRANSFORMS(img).to(dtype=vae_dtype).unsqueeze(0)
-        with torch.no_grad():
-            if accelerator.is_main_process:
-                inf_vae = accelerator.unwrap_model(vae).to(dtype=vae_dtype)
-                img = img.to(inf_vae.device)
-                recon_img = inf_vae(img).sample
-                recon_img = (recon_img / 2 + 0.5).clamp(0, 1).cpu().permute(0, 2, 3, 1).numpy()[0]
-                image = (recon_img * 255).astype(np.uint8)
-                image = Image.fromarray(image)
-                save_dir = os.path.join(args.output_dir, 'sample')
-                os.makedirs(save_dir, exist_ok=True)
-                image.save(os.path.join(save_dir, f'normal_recon_epoch_{epoch}.png'))
-
-
-
+    print(f'\n step 3. inference')
+    print('sampling')
+    sample_data_dir = r'../../../MyData/anomaly_detection/VisA/MVTecAD/bagel/test/crack/rgb/000.png'
+    h,w = args.resolution
+    img = load_image(sample_data_dir, int(h), int(w))
+    img = IMAGE_TRANSFORMS(img).to(dtype=vae_dtype).unsqueeze(0)
+    with torch.no_grad():
+        img = img.to(vae.device)
+        recon_img = vae(img).sample
+        recon_img = (recon_img / 2 + 0.5).clamp(0, 1).cpu().permute(0, 2, 3, 1).numpy()[0]
+        image = (recon_img * 255).astype(np.uint8)
+        image = Image.fromarray(image)
+        save_dir = os.path.join(args.output_dir, 'test')
+        os.makedirs(save_dir, exist_ok=True)
+        image.save(os.path.join(save_dir, f'anormal_recon_test.png'))
 
 
 if __name__ == "__main__":
@@ -144,60 +108,16 @@ if __name__ == "__main__":
     # step 2. dataset
     train_util.add_dataset_arguments(parser, True, True, True)
     parser.add_argument("--mask_dir", type=str, default='')
-    parser.add_argument("--class_caption", type=str, default='')
     parser.add_argument("--no_metadata", action="store_true",
                         help="do not save metadata in output model / メタデータを出力先モデルに保存しない")
 
-    # step 3. model
-    train_util.add_sd_models_arguments(parser)
-    parser.add_argument("--network_weights", type=str, default=None,
-                        help="pretrained weights for network / 学習するネットワークの初期重み")
-    parser.add_argument("--network_module", type=str, default=None,
-                        help="network module to train / 学習対象のネットワークのモジュール")
-    parser.add_argument("--network_dim", type=int, default=None,
-                        help="network dimensions (depends on each network) / モジュールの次元数（ネットワークにより定義は異なります）")
-    parser.add_argument("--network_alpha", type=float, default=1,
-                        help="alpha for LoRA weight scaling, default 1 (same as network_dim for same behavior as old version)", )
-    parser.add_argument("--network_dropout", type=float, default=None,
-                        help="Drops neurons out of training every step (0 or None is default behavior (no dropout), 1 would drop all neurons)", )
-    parser.add_argument("--network_args", type=str, default=None, nargs="*",
-                        help="additional argmuments for network (key=value) / ネットワークへの追加の引数")
-
-    # step 4. training
-    train_util.add_training_arguments(parser, True)
-    custom_train_functions.add_custom_train_arguments(parser)
-    parser.add_argument("--unet_lr", type=float, default=None, help="learning rate for U-Net / U-Netの学習率")
-    parser.add_argument("--text_encoder_lr", type=float, default=None,
-                        help="learning rate for Text Encoder / Text Encoderの学習率")
-
     # step 5. optimizer
-    train_util.add_optimizer_arguments(parser)
-
     config_util.add_config_arguments(parser)
-
     parser.add_argument("--save_model_as", type=str, default="safetensors",
                         choices=[None, "ckpt", "pt", "safetensors"],
                         help="format to save the model (default is .safetensors) / モデル保存時の形式（デフォルトはsafetensors）", )
-    parser.add_argument("--network_train_unet_only", action="store_true",
-                        help="only training U-Net part / U-Net関連部分のみ学習する")
-    parser.add_argument("--network_train_text_encoder_only", action="store_true",
-                        help="only training Text Encoder part / Text Encoder関連部分のみ学習する")
-    parser.add_argument("--training_comment", type=str, default=None,
-                        help="arbitrary comment string stored in metadata / メタデータに記録する任意のコメント文字列")
-    parser.add_argument("--dim_from_weights", action="store_true",
-                        help="automatically determine dim (rank) from network_weights / dim (rank)をnetwork_weightsで指定した重みから自動で決定する", )
-    parser.add_argument("--scale_weight_norms", type=float, default=None,
-                        help="Scale the weight of each key pair to help prevent overtraing via exploding gradients. ", )
-    parser.add_argument("--base_weights", type=str, default=None, nargs="*",
-                        help="network weights to merge into the model before training / 学習前にあらかじめモデルにマージするnetworkの重みファイル", )
-    parser.add_argument("--base_weights_multiplier", type=float, default=None, nargs="*",
-                        help="multiplier for network weights to merge into the model before training / 学習前にあらかじめモデルにマージするnetworkの重みの倍率", )
-    parser.add_argument("--no_half_vae", action="store_true",
-                        help="do not use fp16/bf16 VAE in mixed precision (use float VAE) / mixed precisionでも fp16/bf16 VAEを使わずfloat VAEを使う", )
-    parser.add_argument("--net_key_names", type=str, default='text')
-    parser.add_argument("--mask_threshold", type=float, default=0.5)
-    parser.add_argument("--contrastive_eps", type=float, default=0.00005)
-    # class_caption
+    parser.add_argument("--device", type=str, default='cuda:0')
+
     args = parser.parse_args()
     args = train_util.read_config_from_file(args, parser)
     main(args)
---
+++ lora/autoencoder_inference.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# start of the script
+#!/bin/bash
+# sbatch -q big_qos --nodes 2 --output=../result/printing_screen/2_contrastive_learning_eps_0.0_new_code_highrepeat_test.txt training_contrastive.sh
+# sbatch -q big_qos --nodes 2 training_contrastive.sh
+echo $CUDA_VISIBLE_DEVICES
+echo $SLURM_NODELIST
+echo $SLURM_NODEID
+
+source ~/.bashrc
+conda activate venv
+ml purge
+ml load cuda/11.0
+
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port 50189 autoencoder_inference.py \
+  --device 'cuda:4' \
+  --logging_dir ../result/logs --process_title parksooyeon --max_token_length 225 \
+  --log_with wandb --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc \
+  --wandb_init_name bagel_training --wandb_run_name 3_contrastive_learning_eps_0.0_increase_generality \
+  --seed 42 --output_dir ../result/MVTec_experiment/bagel/vae_training \
+  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
+  --network_module networks.lora --network_dim 64 --network_alpha 4 --train_batch_size 3 \
+  --optimizer_type AdamW --lr_scheduler cosine_with_restarts --lr_warmup_steps 144 \
+  --learning_rate 0.0003 --unet_lr 0.0001 --text_encoder_lr 0.00005 \
+  --resolution 512,512 \
+  --save_every_n_epochs 1 \
+  --sample_every_n_epochs 1 \
+  --max_train_steps 48000 \
+  --train_data_dir ../../../MyData/anomaly_detection/autoencoder_training/bad \
+  --class_caption 'good' \
+  --contrastive_eps 0.0
