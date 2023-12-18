61ecd9a0c8bd7960f7e6986a27f1d5ee2677b345
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-18 21:13:15 +0900

    Update autoencoder_training_3.sh

--- lora/autoencoder_training_3.sh
+++ lora/autoencoder_training_3.sh
@@ -12,7 +12,7 @@ conda activate venv
 ml purge
 ml load cuda/11.0
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_1_config --main_process_port 50189 autoencoder_training_3.py \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_1_config autoencoder_training_3.py \
   --logging_dir ../result/logs --process_title parksooyeon --max_token_length 225 \
   --log_with wandb --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc \
   --wandb_init_name bagel_training --wandb_run_name 2_vae_decoder_training_uniform_initialize \
