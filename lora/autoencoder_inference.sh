a8170a9f96c3aae12de2743fcf9b29832bfa2108
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-19 16:52:41 +0900

    Update autoencoder_inference.sh

--- lora/autoencoder_inference.sh
+++ lora/autoencoder_inference.sh
@@ -13,7 +13,6 @@ ml purge
 ml load cuda/11.0
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 53489 autoencoder_inference.py \
-  --device 'cuda:4' \
   --process_title parksooyeon \
   --seed 42 \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
