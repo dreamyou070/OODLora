3ddd2c815378d981bcaaabe9f6fa9474b1bd7990
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-19 09:46:22 +0900

    Update autoencoder_inference.py

--- lora/autoencoder_inference.py
+++ lora/autoencoder_inference.py
@@ -92,7 +92,7 @@ def main(args):
 
     student_pretrained_dir = args.student_pretrained_dir
     tensors = {}
-    with safe_open("model.safetensors", framework="pt", device="cpu") as f:
+    with safe_open(student_pretrained_dir, framework="pt", device="cpu") as f:
         for key in f.keys():
             tensors[key] = f.get_tensor(key)
     student.load_state_dict(tensors)
