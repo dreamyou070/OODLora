143c4016456aa5332fd3e5d8300fba3099d32693
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-20 01:56:14 +0900

    Update customizing_scheduling.sh

--- lora/customizing_scheduling.sh
+++ lora/customizing_scheduling.sh
@@ -14,9 +14,10 @@ ml load cuda/11.0
 
 python customizing_scheduling.py --device cuda:0 \
   --process_title parksooyeon \
-  --pretrained_model_name_or_path /data7/sooyeon/pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned-emaonly.ckpt \
-  --network_module networks.lora --network_dim 64 --network_alpha 4 --prompt 'good' --sample_sampler ddim --num_ddim_steps 50 \
-  --output_dir '/data7/sooyeon/Lora/OODLora/result' \
-  --network_weights /data7/sooyeon/Lora/OODLora/result/MVTec_experiment/bagel/model/epoch-000003.safetensors \
-  --concept_image_folder /data7/sooyeon/MyData/anomaly_detection/autoencoder_training/bad/100_good \
+  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned-emaonly.ckpt \
+  --network_module networks.lora --network_dim 64 --network_alpha 4 \
+  --prompt 'good' --sample_sampler ddim --num_ddim_steps 50 \
+  --output_dir '../result' \
+  --network_weights ../result/MVTec_experiment/bagel/unet_training/model/epoch-000003.safetensors \
+  --concept_image_folder ../../../MyData/anomaly_detection/autoencoder_training/bad/100_good \
   --student_pretrained_dir ../result/MVTec_experiment/bagel/vae_training/2_TS_test_contrastive/vae_student_model/student_epoch_000004.pth
