36be21aff07a1e4dadb5b13c08797414f4f9f6d8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-20 20:19:24 +0900

    .

--- lora/training_end_to_end.sh
+++ lora/training_end_to_end.sh
@@ -24,20 +24,20 @@ ml purge
 ml load cuda/11.0
 
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config --main_process_port 50189 training_end_to_end.py \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port 50189 training_end_to_end.py \
   --logging_dir ../result/logs --process_title parksooyeon --max_token_length 225 \
   --log_with wandb --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc \
-  --wandb_init_name bagel_training --wandb_run_name 5_train_end_to_end \
-  --seed 42 --output_dir ../result/MVTec_experiment/bagel/5_train_end_to_end \
+  --wandb_init_name bagel_training --wandb_run_name 6_train_end_to_end \
+  --seed 42 --output_dir ../result/MVTec_experiment/bagel/6_train_end_to_end \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
-  --network_module networks.lora --network_dim 64 --network_alpha 4 --train_batch_size 2 \
+  --network_module networks.lora --network_dim 64 --network_alpha 4 --train_batch_size 3 \
   --optimizer_type AdamW --lr_scheduler cosine_with_restarts --lr_warmup_steps 144 \
   --learning_rate 0.0003 --unet_lr 0.0001 --text_encoder_lr 0.00005 \
   --resolution 512,512 \
   --save_every_n_epochs 1 \
   --sample_every_n_epochs 1 \
   --sample_prompts ../../../MyData/object/bagel_inference.txt \
-  --max_train_steps 48000 \
+  --max_train_steps 4800 \
   --train_data_dir ../../../MyData/anomaly_detection/VisA/MVTecAD/paired_data/bad \
   --class_caption 'good' \
   --contrastive_eps 0.0
