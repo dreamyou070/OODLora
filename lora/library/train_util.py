a52da607e09a9ad2e39bf480ed533411d83e1775
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-21 20:52:07 +0900

    Update train_util.py

--- lora/library/train_util.py
+++ lora/library/train_util.py
@@ -4516,7 +4516,7 @@ def get_noise_noisy_latents_and_timesteps_customizing(args, noise_scheduler, lat
     min_timestep = 0 if args.min_timestep is None else args.min_timestep
     max_timestep = len(noise_scheduler.timesteps)
     step_ratio = noise_scheduler.config.num_train_timesteps // noise_scheduler.num_inference_steps
-    timesteps = torch.randint(min_timestep, max_timestep, (3,)) * step_ratio
+    timesteps = torch.randint(min_timestep, max_timestep, (b_size,)) * step_ratio
     timesteps = timesteps.long()
     # Add noise to the latents according to the noise magnitude at each timestep
     # (this is the forward diffusion process)
