7c08db0eecccae88e54bff52f99437e9ee394f6d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-21 12:08:08 +0900

    Update image_inverting_vae_encoder.sh

--- lora/image_inverting_vae_encoder.sh
+++ lora/image_inverting_vae_encoder.sh
@@ -13,7 +13,7 @@ ml purge
 ml load cuda/11.0
 
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port 57689 image_inverting_vaee_encoder.py \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port 57689 image_inverting_vae_encoder.py \
   --process_title parksooyeon \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned-emaonly.ckpt \
   --network_module networks.lora --network_dim 64 --network_alpha 4 \
