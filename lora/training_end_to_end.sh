c5a38241d6e7fdc78ad90cb75bf0e574553748cf
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-21 09:42:35 +0900

    Update training_end_to_end.sh

--- lora/training_end_to_end.sh
+++ lora/training_end_to_end.sh
@@ -38,7 +38,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config --mai
   --save_every_n_epochs 1 \
   --sample_every_n_epochs 1 \
   --sample_prompts ../../../MyData/object/bagel_inference.txt \
-  --max_train_steps 400 \
+  --max_train_steps 48000 \
   --train_data_dir ../../../MyData/anomaly_detection/VisA/MVTecAD/paired_data/bad \
   --class_caption 'good' \
   --contrastive_eps 0.0
