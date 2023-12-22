158b99f3e1cc85809f68c66957fe7bf7d9145a4f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-22 20:32:10 +0900

    .

--- lora/dataset/train_test_split.py
+++ lora/dataset/train_test_split.py
@@ -22,20 +22,46 @@ def main(args):
 
     for cls in classes:
         class_dir = os.path.join(data_folder, cls)
+        train_dir = os.path.join(class_dir, 'train')
+        os.makedirs(train_dir, exist_ok=True)
 
         corrected_dir = os.path.join(class_dir, 'corrected')
         bad_dir = os.path.join(class_dir, 'bad')
         damages = os.listdir(corrected_dir)
         for damage in damages:
-            damage_dir = os.path.join(corrected_dir, damage)
-            images = os.listdir(damage_dir)
-            for image in images:
-                image_path = os.path.join(damage_dir, image)
-                pil = Image.open(image_path)
-                np_img = np.array(pil)
-                np_sum = np.sum(np_img)
-                if np_sum == 0:
-                    print(f'{image_path} is removed')
+            if damage != 'good':
+
+                train_anomaly_dir = os.path.join(train_dir, damage)
+                os.makedirs(train_anomaly_dir, exist_ok=True)
+
+                damage_dir = os.path.join(corrected_dir, damage)
+                images = os.listdir(damage_dir)
+                for image in images:
+                    image_path = os.path.join(damage_dir, image)
+                    pil = Image.open(image_path)
+                    np_img = np.array(pil)
+                    np_sum = np.sum(np_img)
+                    if np_sum == 0:
+                        # save again
+                        bad_dir = os.path.join(bad_dir, f'{damage}/{image}')
+                        os.rename(bad_dir, os.path.join(train_anomaly_dir, image))
+
+                        os.remove(image_path)
+            """
+            else :
+                train_good_dir = os.path.join(train_dir, 'good')
+                os.makedirs(train_good_dir, exist_ok=True)
+
+                damage_dir = os.path.join(corrected_dir, damage)
+                damage_corrected_dir = os.path.join(corrected_dir, 'good')
+                images = os.listdir(damage_dir)
+                for image in images:
+                    image_path = os.path.join(damage_dir, image)
+                    os.rename(image_path, os.path.join(train_good_dir, image))
+                    a = os.path.join(damage_corrected_dir, image)
+                    os.remove(a)
+            """
+
 
 
 if __name__ == "__main__":
