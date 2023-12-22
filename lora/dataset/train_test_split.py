a6d3ef58ed8599135f661935ab5d9be3eff62b1e
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-22 20:40:10 +0900

    .

--- lora/dataset/train_test_split.py
+++ lora/dataset/train_test_split.py
@@ -13,20 +13,19 @@ def main(args):
                                                           cache_dir=r'../../../../pretrained_stable_diffusion').to(device)
 
     print(f'\n step 2. dataset')
-    parent, child = os.path.split(args.data_folder)
-    save_folder = os.path.join(parent, f'{child}_Experiment')
-    os.makedirs(save_folder, exist_ok=True)
 
     data_folder = args.data_folder
     classes = os.listdir(data_folder)
 
     for cls in classes:
         class_dir = os.path.join(data_folder, cls)
+
+        # (1) train dir make
         train_dir = os.path.join(class_dir, 'train')
         os.makedirs(train_dir, exist_ok=True)
 
-        corrected_dir = os.path.join(class_dir, 'corrected')
         bad_dir = os.path.join(class_dir, 'bad')
+        corrected_dir = os.path.join(class_dir, 'corrected')
         damages = os.listdir(corrected_dir)
         for damage in damages:
             if damage != 'good':
@@ -34,21 +33,28 @@ def main(args):
                 train_anomaly_dir = os.path.join(train_dir, damage)
                 os.makedirs(train_anomaly_dir, exist_ok=True)
 
-                bad_damage_dir = os.path.join(bad_dir, damage)
-                damage_dir = os.path.join(corrected_dir, damage)
+                bad_damage_dir = os.path.join(bad_dir,       damage) # combined
+                damage_dir     = os.path.join(corrected_dir, damage)
+                train_damage_dir = os.path.join(train_dir, damage)
+                os.makedirs(train_damage_dir, exist_ok=True)
+
                 images = os.listdir(damage_dir)
+
                 for image in images:
+
+                    bad_image_path = os.path.join(bad_damage_dir, image)
                     image_path = os.path.join(damage_dir, image)
+
                     pil = Image.open(image_path)
                     np_img = np.array(pil)
                     np_sum = np.sum(np_img)
-                    if np_sum == 0:
-                        # save again
-                        bad_dir = os.path.join(bad_damage_dir, image)
-                        redir = os.path.join(train_anomaly_dir, image)
-                        os.rename(bad_dir, redir)
 
-                        #os.remove(image_path)
+                    if np_sum == 0:
+
+                        redir = os.path.join(train_damage_dir, image)
+                        os.rename(bad_image_path, redir)
+
+                        os.remove(image_path)
             """
             else :
                 train_good_dir = os.path.join(train_dir, 'good')
