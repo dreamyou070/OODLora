14c63fd8c4334d3ba8e7740084e179b8c4171636
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-22 15:43:18 +0900

    Update data_check_visa.py

--- lora/dataset/data_check_visa.py
+++ lora/dataset/data_check_visa.py
@@ -15,51 +15,52 @@ def main(args):
 
     base_folder_dir = '/home/dreamyou070/MyData/anomaly_detection/VisA/split_csv'
     files = os.listdir(base_folder_dir)
-    for file in files:
+    for  file in files:
         file_dir = os.path.join(base_folder_dir, file)
         with open(file_dir, 'r') as f:
             lines = f.readlines()
-        for line in lines:
-            line = line.strip()
-            line_list = line.split(',')
-            category = line_list[0]
-            save_category_dir = os.path.join(save_folder, category)
-            os.makedirs(save_category_dir, exist_ok=True)
-            split = line_list[1]
-            save_split_dir = os.path.join(save_category_dir, split)
-            os.makedirs(save_split_dir, exist_ok=True)
-            if split == 'test' :
-                normal_anomal_info = line_list[2]
-                final_image_dir = line_list[3].strip()
-                img_name = final_image_dir.split('/')[-1]
+        for i, line in enumerate(lines):
+            if i > 0 :
+                line = line.strip()
+                line_list = line.split(',')
+                category = line_list[0]
+                save_category_dir = os.path.join(save_folder, category)
+                os.makedirs(save_category_dir, exist_ok=True)
+                split = line_list[1]
+                save_split_dir = os.path.join(save_category_dir, split)
+                os.makedirs(save_split_dir, exist_ok=True)
+                if split == 'test' :
+                    normal_anomal_info = line_list[2]
+                    final_image_dir = line_list[3].strip()
+                    img_name = final_image_dir.split('/')[-1]
 
-                if normal_anomal_info == 'normal' :
-                    org_img_dir = os.path.join(args.data_folder, f'{final_image_dir}')
+                    if normal_anomal_info == 'normal' :
+                        org_img_dir = os.path.join(args.data_folder, f'{final_image_dir}')
+                        normal_anomal_info_dir = os.path.join(save_split_dir, 'good')
+                        os.makedirs(normal_anomal_info_dir, exist_ok=True)
+                        new_img_dir = os.path.join( normal_anomal_info_dir, img_name)
+                        Image.open(org_img_dir).save(new_img_dir)
+                    else :
+                        org_img_dir = os.path.join(args.data_folder, f'{final_image_dir}')
+                        normal_anomal_info_dir = os.path.join(save_split_dir, 'bad')
+                        os.makedirs(normal_anomal_info_dir, exist_ok=True)
+                        new_img_dir = os.path.join( normal_anomal_info_dir, img_name)
+                        Image.open(org_img_dir).save(new_img_dir)
+
+                        mask_img_dir = os.path.join(args.data_folder, f'{line_list[-1].strip()}')
+                        mask_folder_dir = os.path.join(save_split_dir, 'ground_truth')
+                        os.makedirs(mask_folder_dir, exist_ok=True)
+                        Image.open(mask_img_dir).save(os.path.join(mask_folder_dir, img_name))
+
+                else :
                     normal_anomal_info_dir = os.path.join(save_split_dir, 'good')
                     os.makedirs(normal_anomal_info_dir, exist_ok=True)
-                    new_img_dir = os.path.join( normal_anomal_info_dir, img_name)
-                    Image.open(org_img_dir).save(new_img_dir)
-                else :
+                    final_image_dir = line_list[3].strip()
+                    print(final_image_dir)
+                    img_name = final_image_dir.split('/')[-1]
                     org_img_dir = os.path.join(args.data_folder, f'{final_image_dir}')
-                    normal_anomal_info_dir = os.path.join(save_split_dir, 'bad')
-                    os.makedirs(normal_anomal_info_dir, exist_ok=True)
                     new_img_dir = os.path.join( normal_anomal_info_dir, img_name)
                     Image.open(org_img_dir).save(new_img_dir)
-
-                    mask_img_dir = os.path.join(args.data_folder, f'{line_list[-1].strip()}')
-                    mask_folder_dir = os.path.join(save_split_dir, 'ground_truth')
-                    os.makedirs(mask_folder_dir, exist_ok=True)
-                    Image.open(mask_img_dir).save(os.path.join(mask_folder_dir, img_name))
-
-            else :
-                normal_anomal_info_dir = os.path.join(save_split_dir, 'good')
-                os.makedirs(normal_anomal_info_dir, exist_ok=True)
-                final_image_dir = line_list[3].strip()
-                print(final_image_dir)
-                img_name = final_image_dir.split('/')[-1]
-                org_img_dir = os.path.join(args.data_folder, f'{final_image_dir}')
-                new_img_dir = os.path.join( normal_anomal_info_dir, img_name)
-                Image.open(org_img_dir).save(new_img_dir)
 
 
 
