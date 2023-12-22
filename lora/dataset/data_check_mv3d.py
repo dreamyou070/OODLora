f23be25dc21fdda20c6b9f02ca965209d5312e9f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-22 20:12:49 +0900

    .

--- lora/dataset/data_check_mv3d.py
+++ lora/dataset/data_check_mv3d.py
@@ -19,9 +19,11 @@ def main(args):
 
     data_folder = args.data_folder
     classes = os.listdir(data_folder)
-    before_classes = []
+    before_classes = ['bagel','cable_gland','carrot','cookie',
+                      'dowel','foam','peach','potato','rope','tire']
     for cls in classes:
         if cls not in before_classes:
+            print(f'cls : {cls}')
             class_dir = os.path.join(data_folder, cls)
             save_class_dir = os.path.join(save_folder, cls)
             os.makedirs(save_class_dir, exist_ok=True)
