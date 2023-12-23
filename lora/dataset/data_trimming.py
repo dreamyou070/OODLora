aa1a96ae18cdcaf3e3f29ca8eaa3aa05b9ff9841
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-23 12:15:20 +0900

    Update data_trimming.py

--- lora/dataset/data_trimming.py
+++ lora/dataset/data_trimming.py
@@ -28,37 +28,41 @@ def main(args):
             sub_classes = os.listdir(org_folder)
             for sub_cls in sub_classes:
                 repeat_num, sub = sub_cls.split('_')
-                sub_org_folder = os.path.join(org_folder, sub_cls)
-                sub_inpainted_folder = os.path.join(inpainted_folder, sub_cls)
-                new_sub_folder = os.path.join(new_folder, sub_cls)
-                os.makedirs(new_sub_folder, exist_ok=True)
-                images = os.listdir(sub_org_folder)
-                for image in images:
-                    org_image_path = os.path.join(sub_org_folder, image)
-                    org_pil = Image.open(org_image_path)
-                    org_pil = org_pil.resize((512, 512))
-                    np_org = np.array(org_pil)
+                if sub != 'good' :
 
-                    inpainted_image_path = os.path.join(sub_inpainted_folder, image)
-                    inpainted_pil = Image.open(inpainted_image_path)
-                    np_inpainted = np.array(inpainted_pil)
+                    sub_org_folder = os.path.join(org_folder, sub_cls)
+                    sub_inpainted_folder = os.path.join(inpainted_folder, sub_cls)
+                    new_sub_folder = os.path.join(new_folder, sub_cls)
+                    os.makedirs(new_sub_folder, exist_ok=True)
+                    images = os.listdir(sub_org_folder)
+                    for image in images:
+                        org_image_path = os.path.join(sub_org_folder, image)
+                        org_pil = Image.open(org_image_path)
+                        org_pil = org_pil.resize((512, 512))
+                        np_org = np.array(org_pil)
 
-                    mask_image_path = os.path.join(test_mask_folder, sub, image)
-                    mask_pil = Image.open(mask_image_path).convert('RGB')
-                    mask_pil = mask_pil.resize((512, 512))
-                    np_mask = np.array(mask_pil)
-                    np_mask = np.where(np_mask < 50, 0, 1) # black = 0 = background, white = 1
+                        inpainted_image_path = os.path.join(sub_inpainted_folder, image)
+                        inpainted_pil = Image.open(inpainted_image_path)
+                        np_inpainted = np.array(inpainted_pil)
 
-                    #print(f'np_org : {np_org.shape}')
-                    #print(f'np_inpainted : {np_inpainted.shape}')
-                    #print(f'np_mask : {np_mask.shape}')
+                        mask_image_path = os.path.join(test_mask_folder, sub, image)
+                        mask_pil = Image.open(mask_image_path).convert('RGB')
+                        mask_pil = mask_pil.resize((512, 512))
+                        np_mask = np.array(mask_pil)
+                        np_mask = np.where(np_mask < 100, 0, 1) # black = 0 = background, white = 1
 
-                    new_np = np_org * (1-np_mask) + np_inpainted * (np_mask)
-                    new_pil = Image.fromarray(new_np.astype(np.uint8))
-                    new_dir = os.path.join(new_sub_folder, image)
-                    new_pil.save(new_dir)
+                        #print(f'np_org : {np_org.shape}')
+                        #print(f'np_inpainted : {np_inpainted.shape}')
+                        #print(f'np_mask : {np_mask.shape}')
 
-
+                        new_np = np_org * (1-np_mask) + np_inpainted * (np_mask)
+                        new_pil = Image.fromarray(new_np.astype(np.uint8))
+                        new_dir = os.path.join(new_sub_folder, image)
+                        new_pil.save(new_dir)
+                else :
+                    sub_org_folder = os.path.join(org_folder, sub_cls)
+                    new_sub_folder = os.path.join(new_folder, sub_cls)
+                    shutil.copytree(sub_org_folder, new_sub_folder)
 
 
 if __name__ == "__main__":
