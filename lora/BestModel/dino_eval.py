82f1d338f4f268a210761cbfa9d0f9551d2d5010
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-24 20:32:10 +0900

    Update dino_eval.py

--- lora/BestModel/dino_eval.py
+++ lora/BestModel/dino_eval.py
@@ -2,11 +2,16 @@ import os
 import argparse
 import torch
 
-def main() :
+def main(args) :
 
-    model_dir = r'../'
+    model_dir = args.model_dir
+    model_state = torch.load(model_dir, map_location="cpu")
+    print(model_state.keys())
 
-    vits16 = torch.hub.load('facebookresearch/dino:main', 'dino_vits16')
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='DINO evaluation script')
+    parser.add_argument('--model_dir', default=r'../../../../pretrained_models/dino_deitsmall16_pretrain_full_checkpoint.pth',
+                        type=str, help='path to the model')
+    args = parser.parse_args()
     main()
