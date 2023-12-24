adef9fc05842851224cdfa4296d9d94f4ffabbad
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-24 13:48:18 +0900

    Update image_inverting_vae_encoder.py

--- lora/image_inverting_vae_encoder.py
+++ lora/image_inverting_vae_encoder.py
@@ -344,25 +344,26 @@ def main(args) :
                     org_vae_latent = image2latent(image_gt_np, vae, device=device, weight_dtype=weight_dtype)
                     st_latent = customizing_image2latent(image_gt_np, student, device=device, weight_dtype=weight_dtype)
                     standard_noise = torch.randn_like(org_vae_latent).to(device)
+                    noising_time = 20
 
-                    org_noise_latent = scheduler.add_noise(original_samples = org_vae_latent, noise = standard_noise, timesteps = torch.tensor(int(700)))
-                    st_noise_latent = scheduler.add_noise(original_samples = st_latent, noise = standard_noise, timesteps = torch.tensor(int(700)))
+                    org_noise_latent = scheduler.add_noise(original_samples = org_vae_latent, noise = standard_noise, timesteps = torch.tensor(int(noising_time)))
+                    st_noise_latent = scheduler.add_noise(original_samples = st_latent, noise = standard_noise, timesteps = torch.tensor(int(noising_time)))
 
                     Image.fromarray(latent2image(org_noise_latent, vae, return_type='np')).save(os.path.join(class_base_folder,
-                                                                                                      f'{name}_org_vae_noise_latent_700{ext}'))
+                                                                                                      f'{name}_org_vae_noise_latent_{noising_time}{ext}'))
                     Image.fromarray(latent2image(st_noise_latent, vae, return_type='np')).save(
                         os.path.join(class_base_folder,
-                                     f'{name}_st_vae_noise_latent_700{ext}'))
+                                     f'{name}_st_vae_noise_latent_{noising_time}{ext}'))
 
                     org_pred_noise = call_unet(invers_unet, org_noise_latent, 700, invers_context.chunk(2)[0], trg_indexs_list=None, mask_imgs=None)
                     st_pred_noise = call_unet(unet, st_noise_latent, 700, context.chunk(2)[1], trg_indexs_list=None, mask_imgs=None)
 
                     org_recon = scheduler.step(org_pred_noise, 700, org_vae_latent, return_dict = True)['pred_original_sample']
                     Image.fromarray(latent2image(org_recon, vae, return_type='np')).save(os.path.join(class_base_folder,
-                                                                                                      f'{name}_org_vae_org_unet_recon{ext}'))
+                                                                                                      f'{name}_org_vae_org_unet_recon_from_{noising_time}{ext}'))
                     st_recon  = scheduler.step(st_pred_noise, 700, st_latent, return_dict = True)['pred_original_sample']
                     Image.fromarray(latent2image(st_recon, vae, return_type='np')).save(os.path.join(class_base_folder,
-                                                                                                     f'{name}_st_vae_lora_unet_recon{ext}'))
+                                                                                                     f'{name}_st_vae_lora_unet_recon_from_{noising_time}{ext}'))
 
                     mse = ((st_latent - org_vae_latent).square() * 2) - thredhold
                     mse_threshold = mse < 0  # if true = 1, false = 0 # if true -> bad
