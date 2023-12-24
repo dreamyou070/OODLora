0cf12d9e1ae9917c5a6c487e0f2f013107b0c250
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-24 13:19:47 +0900

    .

--- lora/sy.py
+++ lora/sy.py
@@ -1,5 +1,10 @@
 import numpy as np
 import random
+import torch
 
-
-print(x)
+"""
+alphas_cumprod = scheduler.alphas_cumprod
+sqrt_alpha_prod = alphas_cumprod[700] ** 0.5
+"""
+a = torch.IntTensor([700])
+a.to('cpu')
