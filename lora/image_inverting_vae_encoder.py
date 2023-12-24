c5952d6f623f3ee50f3e86f602a84d7631e390d4
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-24 15:54:21 +0900

    Update image_inverting_vae_encoder.py

--- lora/image_inverting_vae_encoder.py
+++ lora/image_inverting_vae_encoder.py
@@ -478,12 +478,11 @@ def main(args) :
                                                                     vae=vae,
                                                                     base_folder_dir=class_base_folder,
                                                                     is_org = False)
-                    print(f'latent_dict : {latent_dict.keys()}')
                     base_num = 40
                     noising_time = inference_times[base_num]  # 100
                     recon_1_times = inference_times[:base_num+1].tolist()
                     recon_latent_dict, _, _ = recon_loop(None,
-                                                         start_latent=latent_dict(time_steps[-1]),
+                                                         start_latent=latent_dict[int(time_steps[-1])],
                                                          context=context,
                                                          inference_times=recon_1_times,
                                                          scheduler=scheduler,
