0891d12bda8a1bd88ccd1c174f393ad1f0f6c69a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-25 10:04:09 +0900

    Update 6_background_blended.py

--- lora/dataset/6_background_blended.py
+++ lora/dataset/6_background_blended.py
@@ -1,26 +1,29 @@
 import os, shutil
 from PIL import Image
 import numpy as np
+import argparse
 
-f_der = 'sample_data'
-classes = os.listdir(f_der)
-trg_num = 1000
-for class_name in classes:  # bagel
-    if class_name == 'potato':
 
-        class_dir = os.path.join(f_der, class_name)
-        train_dir = os.path.join(class_dir, 'train')
+def main(args) :
 
-        bad_train_dir = os.path.join(train_dir, 'bad')
-        corrected_train_dir = os.path.join(train_dir, 'corrected')
-        gt_train_dir = os.path.join(train_dir, 'gt')
-        cor_2_dir = os.path.join(train_dir, 'cor_2')
-        os.makedirs(cor_2_dir, exist_ok=True)
+    base_folder = args.base_folder
+    classes = os.listdir(base_folder)
+    trg_num = 1000
+    for class_name in classes:  # bagel
 
-        categories = os.listdir(bad_train_dir)
-        for category in categories:
+        if class_name == 'bagel':
+            class_dir = os.path.join(base_folder, class_name)
 
-            if 'good' not in category:
+            train_dir = os.path.join(class_dir, 'train')
+            bad_train_dir = os.path.join(train_dir, 'bad')
+            corrected_train_dir = os.path.join(train_dir, 'corrected')
+            cor_2_dir = os.path.join(train_dir, 'cor_2')
+            os.makedirs(cor_2_dir, exist_ok=True)
+            gt_train_dir = os.path.join(train_dir, 'gt')
+            test_dir = os.path.join(class_dir, 'test')
+            categories = os.listdir(bad_train_dir)
+            for category in categories:
+
                 category_dir = os.path.join(bad_train_dir, category)
                 category_corrected_dir = os.path.join(corrected_train_dir, category)
                 category_cor_2_dir = os.path.join(cor_2_dir, category)
@@ -28,9 +31,11 @@ for class_name in classes:  # bagel
 
                 if '_' in category:
                     category = '_'.join(category.split('_')[1:])
+
                 category_gt_dir = os.path.join(gt_train_dir, category)
 
                 images = os.listdir(category_dir)
+
                 for img in images:
                     background_img_dir = os.path.join(category_dir, img)
                     object_img_dir = os.path.join(category_corrected_dir, img)
@@ -45,10 +50,16 @@ for class_name in classes:  # bagel
                     new_np = back_np * (1 - mask_np) + obj_np * mask_np
                     new_img = Image.fromarray(new_np, "RGB")
                     new_img.save(os.path.join(category_cor_2_dir, img))
-            else :
-                category_corrected_dir = os.path.join(corrected_train_dir, category)
-                category_cor_2_dir = os.path.join(cor_2_dir, category)
-                os.makedirs(category_cor_2_dir, exist_ok=True)
-                shutil.copytree(category_corrected_dir, category_cor_2_dir)
 
+                """
+                else :
+                    category_corrected_dir = os.path.join(corrected_train_dir, category)
+                    category_cor_2_dir = os.path.join(cor_2_dir, category)
+                    os.makedirs(category_cor_2_dir, exist_ok=True)
+                    shutil.copytree(category_corrected_dir, category_cor_2_dir)
+                """
 
+if __name__ == '__main__' :
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--base_folder', type=str, default='../../MyData/anomaly_detection/MVTec3D-AD_Experiment_SDXL')
+    main()
