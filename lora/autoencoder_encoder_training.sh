ec0d967aea26b09b8312739806c978da995cc636
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-26 16:14:50 +0900

    training code again

--- lora/autoencoder_encoder_training.sh
+++ lora/autoencoder_encoder_training.sh
@@ -18,11 +18,13 @@ ml load cuda/11.0
 # conda activate venv_lora
 # cd ./Lora/OODLora/lora
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config --main_process_port 54189 autoencoder_encoder_training.py \
-  --process_title parksooyeon --max_token_length 225 --logging_dir ../result/logs --log_with wandb --seed 42 \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config --main_process_port 50189 autoencoder_encoder_training.py \
+  --process_title parksooyeon --max_token_length 225 --log_with wandb --seed 42 \
   --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --resolution 512,512 --save_every_n_epochs 1 --sample_every_n_epochs 1 --train_batch_size 2 --max_train_steps 100000 --start_epoch 0 \
-  --wandb_init_name carrot_training \
-  --wandb_run_name 2_TS_encoder_normal_anormal_no_aug \
-  --output_dir ../result/MVTec3D-AD_experiment/carrot/vae_training/2_TS_encoder_normal_anormal_no_aug \
-  --train_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD_Experiment_SDXL/carrot/train/bad
+  --wandb_init_name cable_gland_training \
+  --wandb_run_name 1_TS_encoder_normal_anormal_aug --color_aug \
+  --logging_dir ../result/MVTec3D-AD_experiment/carrot/vae_training/1_TS_encoder_normal_anormal_aug \
+  --output_dir ../result/MVTec3D-AD_experiment/carrot/vae_training/1_TS_encoder_normal_anormal_aug \
+  --train_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD_Experiment_SDXL/cable_gland/train/bad \
+  --valid_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD_Experiment_SDXL/cable_gland/test/bad
