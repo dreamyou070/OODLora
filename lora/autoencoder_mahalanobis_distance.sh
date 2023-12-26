5af7e7f3e8134b91df013d0878a38b45ab17400a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-26 14:00:21 +0900

    Update autoencoder_mahalanobis_distance.sh

--- lora/autoencoder_mahalanobis_distance.sh
+++ lora/autoencoder_mahalanobis_distance.sh
@@ -12,7 +12,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --process_title parksooyeon \
   --seed 42 \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
-  --student_encoder_pretrained_dir ../result/MVTec3D-AD_experiment/potato/vae_training/1_TS_encoder_patchwise_augmenting/vae_student_model \
+  --student_encoder_pretrained_dir ../../../result/MVTec3D-AD_experiment/potato/vae_training/1_TS_encoder_patchwise_augmenting/vae_student_model \
   --output_dir ../../../MyData/anomaly_detection/MVTec3D-AD_Experiment_SDXL/cable_gland/teacher_normal_embedding \
   --anormal_folder ../../../MyData/anomaly_detection/MVTec3D-AD_Experiment_SDXL/cable_gland \
   --resolution 512,512
