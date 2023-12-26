2595e238d5cfbfb88b8213787622cbada473df49
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-26 15:22:49 +0900

    Update 2_finding_best_student_model.py

--- lora/2_finding_best_student_model.py
+++ lora/2_finding_best_student_model.py
@@ -18,45 +18,47 @@ def main(args) :
         if title_list not in total_diff :
             total_diff.append(title_list)
         for cat in cats :
-            test_cat_dir = os.path.join(test_dir, cat)
-            images = os.listdir(test_cat_dir)
-            cat_diff = []
-            for image in images :
-                name, ext = os.path.splitext(image)
+            if 'good' not in cat :
+                test_cat_dir = os.path.join(test_dir, cat)
+                images = os.listdir(test_cat_dir)
+                cat_diff = []
+                for image in images :
+                    name, ext = os.path.splitext(image)
+                    if 'recon' not in name and 'gt' not in name and 'mask' not in name :
+                        base_img_dir = os.path.join(test_cat_dir, image)
+                        recon_img_dir = os.path.join(test_cat_dir, f'{name}_recon{ext}')
+                        base_pil = Image.open(base_img_dir).convert('RGB').resize((512, 512))
+                        base_np = np.array(base_pil)
 
-                if 'recon' not in name and 'gt' not in name and 'mask' not in name :
-                    base_img_dir = os.path.join(test_cat_dir, image)
-                    recon_img_dir = os.path.join(test_cat_dir, f'{name}_recon{ext}')
-                    base_pil = Image.open(base_img_dir).convert('RGB').resize((512, 512))
-                    base_np = np.array(base_pil)
+                        recon_pil = Image.open(recon_img_dir).convert('RGB').resize((512, 512))
+                        recon_np = np.array(recon_pil)
 
-                    recon_pil = Image.open(recon_img_dir).convert('RGB').resize((512, 512))
-                    recon_np = np.array(recon_pil)
+                        if 'good' not in cat :
+                            mask_dir = os.path.join(test_cat_dir, f'{name}_mask{ext}')
+                            mask_pil = Image.open(mask_dir).convert('RGB').resize((512, 512))
+                            mask_np = np.array(mask_pil)
+                            mask_np = np.where(mask_np > 100, 1, 0)
+                        else :
+                            mask_np = np.zeros((512,512,3))
 
-                    if 'good' not in cat :
-                        mask_dir = os.path.join(test_cat_dir, f'{name}_mask{ext}')
-                        mask_pil = Image.open(mask_dir).convert('RGB').resize((512, 512))
-                        mask_np = np.array(mask_pil)
-                        mask_np = np.where(mask_np > 100, 1, 0)
-                    else :
-                        mask_np = np.zeros((512,512,3))
+                        diff = (base_np - recon_np) **2
 
-                    diff = (base_np - recon_np) **2
+                        background_diff = diff * mask_np
+                        background_pixel_num = 512*512 - np.sum(mask_np)
+                        background_diff = np.sum(background_diff/ background_pixel_num)
 
-                    background_diff = diff * mask_np
-                    background_pixel_num = 512*512 - np.sum(mask_np)
-                    background_diff = np.sum(background_diff/ background_pixel_num)
-
-                    object_diff = diff * (1-mask_np)
-                    object_pixel_num = np.sum(mask_np)
-                    if object_pixel_num > 0 :
-                        object_diff = np.sum(object_diff/ object_pixel_num)
-                        cat_diff.append(float(background_diff - object_diff))
-                    else :
-                        cat_diff.append(float(background_diff))
-                    cat_diff_score = np.mean(np.array(cat_diff))
-                    print(f'cat_diff : {cat_diff_score}')
-                    epoch_elem.append(cat_diff_score)
+                        object_diff = diff * (1-mask_np)
+                        object_pixel_num = np.sum(mask_np)
+                        if object_pixel_num > 0 :
+                            object_diff = np.sum(object_diff/ object_pixel_num)
+                            score = abs(float(object_diff-background_diff ))
+                            cat_diff.append(score)
+                        else :
+                            cat_diff.append(float(background_diff))
+                        #cat_diff_score = np.mean(np.array(cat_diff))
+            category_score = np.mean(np.array(cat_diff))
+            print(f'epoch : {epoch_num}, cat : {cat}, category_score : {category_score}')
+            epoch_elem.append(category_score)
         total_diff.append(epoch_elem)
 
     import csv
