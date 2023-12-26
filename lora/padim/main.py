701974cd85f5591a5aee3e8f74760f5ff2c716eb
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-26 12:26:32 +0900

    Update main.py

--- lora/padim/main.py
+++ lora/padim/main.py
@@ -75,46 +75,50 @@ def main(args):
 
         train_dataset = mvtec.MVTecDataset(args.data_path, class_name=class_name, is_train=True)
         train_dataloader = DataLoader(train_dataset, batch_size=32, pin_memory=True)
-
         test_dataset = mvtec.MVTecDataset(args.data_path, class_name=class_name, is_train=False)
         test_dataloader = DataLoader(test_dataset, batch_size=32, pin_memory=True)
-        train_outputs = OrderedDict([('layer1', []),
-                                     ('layer2', []),
-                                     ('layer3', [])])
+
+        train_outputs = OrderedDict([('layer1', []), ('layer2', []), ('layer3', [])])
         test_outputs = OrderedDict([('layer1', []), ('layer2', []), ('layer3', [])])
+
         # extract train set features
         train_feature_filepath = os.path.join(args.save_path, 'temp_%s' % args.arch, 'train_%s.pkl' % class_name)
-        print(f'train_feature_filepath : {train_feature_filepath}')
         if not os.path.exists(train_feature_filepath):
             for (x, _, _) in tqdm(train_dataloader, '| feature extraction | train | %s |' % class_name):
+                # model prediction
                 with torch.no_grad():
-                    # wide_resnet50_2 model output
                     _ = model(x.to(device))
+                # get intermediate layer outputs
                 for k, v in zip(train_outputs.keys(), outputs):
-                    # layer_1, layer_2, layer_3
                     train_outputs[k].append(v.cpu().detach())
                 # initialize hook outputs
                 outputs = []
             for k, v in train_outputs.items():
-                train_outputs[k] = torch.cat(v, 0) # [N, C, H, W]
+                train_outputs[k] = torch.cat(v, 0)
+
             # Embedding concat
             embedding_vectors = train_outputs['layer1']
             for layer_name in ['layer2', 'layer3']:
                 embedding_vectors = embedding_concat(embedding_vectors, train_outputs[layer_name])
+
             # randomly select d dimension
-            print(f'total embedding_vectors : {embedding_vectors.shape}') # 209,(N), 1792(dim), 56, 56
             embedding_vectors = torch.index_select(embedding_vectors, 1, idx)
             # calculate multivariate Gaussian distribution
-            B, C, H, W = embedding_vectors.size() # 550, 1792, 56, 56
+            B, C, H, W = embedding_vectors.size()
             embedding_vectors = embedding_vectors.view(B, C, H * W)
+
+            # (1) get mean vector
             mean = torch.mean(embedding_vectors, dim=0).numpy()
+
+            # (2) covariance vector
             cov = torch.zeros(C, C, H * W).numpy()
             I = np.identity(C)
             for i in range(H * W):
+                # cov[:, :, i] = LedoitWolf().fit(embedding_vectors[:, :, i].numpy()).covariance_
                 cov[:, :, i] = np.cov(embedding_vectors[:, :, i].numpy(), rowvar=False) + 0.01 * I
+
             # save learned distribution
-            # mean = 1792, 3136
-            # cov = 1792, 1792, 3136
+
             train_outputs = [mean, cov]
             with open(train_feature_filepath, 'wb') as f:
                 pickle.dump(train_outputs, f)
@@ -122,32 +126,29 @@ def main(args):
             print('load train set feature from: %s' % train_feature_filepath)
             with open(train_feature_filepath, 'rb') as f:
                 train_outputs = pickle.load(f)
-        mean, cov = train_outputs # mean = [1792 dim, 3136 pixel num], cov = [1792, 1792, 3136 pixel num]
 
         gt_list = []
         gt_mask_list = []
         test_imgs = []
+
+        mean, cov = train_outputs
+        print(f'mean : {mean.shape} | cov : {cov.shape}')
         # extract test set features
-        idx = 0
         for (x, y, mask) in tqdm(test_dataloader, '| feature extraction | test | %s |' % class_name):
-            print(f' idx {idx} : x = {x} y = {y} ')
             test_imgs.extend(x.cpu().detach().numpy())
             gt_list.extend(y.cpu().detach().numpy())
             gt_mask_list.extend(mask.cpu().detach().numpy())
             # model prediction
             with torch.no_grad():
                 _ = model(x.to(device))
-                print(f'model output : {_}')
             # get intermediate layer outputs
             for k, v in zip(test_outputs.keys(), outputs):
-                # k = layer_1 / layer_2 / layer_3
                 test_outputs[k].append(v.cpu().detach())
             # initialize hook outputs
             outputs = []
-
         for k, v in test_outputs.items():
             test_outputs[k] = torch.cat(v, 0)
-        
+
         # Embedding concat
         embedding_vectors = test_outputs['layer1']
         for layer_name in ['layer2', 'layer3']:
@@ -155,10 +156,10 @@ def main(args):
 
         # randomly select d dimension
         embedding_vectors = torch.index_select(embedding_vectors, 1, idx)
-        
+
         # calculate distance matrix
         B, C, H, W = embedding_vectors.size()
-        embedding_vectors = embedding_vectors.view(B, C, H * W).numpy() #
+        embedding_vectors = embedding_vectors.view(B, C, H * W).numpy()
         dist_list = []
         for i in range(H * W):
             mean = train_outputs[0][:, i]
@@ -172,16 +173,16 @@ def main(args):
         dist_list = torch.tensor(dist_list)
         score_map = F.interpolate(dist_list.unsqueeze(1), size=x.size(2), mode='bilinear',
                                   align_corners=False).squeeze().numpy()
-        
+
         # apply gaussian smoothing on the score map
         for i in range(score_map.shape[0]):
             score_map[i] = gaussian_filter(score_map[i], sigma=4)
-        
+
         # Normalization
         max_score = score_map.max()
         min_score = score_map.min()
         scores = (score_map - min_score) / (max_score - min_score)
-        
+
         # calculate image-level ROC AUC score
         img_scores = scores.reshape(scores.shape[0], -1).max(axis=1)
         gt_list = np.asarray(gt_list)
@@ -190,7 +191,7 @@ def main(args):
         total_roc_auc.append(img_roc_auc)
         print('image ROCAUC: %.3f' % (img_roc_auc))
         fig_img_rocauc.plot(fpr, tpr, label='%s img_ROCAUC: %.3f' % (class_name, img_roc_auc))
-        
+
         # get optimal threshold
         gt_mask = np.asarray(gt_mask_list)
         precision, recall, thresholds = precision_recall_curve(gt_mask.flatten(), scores.flatten())
