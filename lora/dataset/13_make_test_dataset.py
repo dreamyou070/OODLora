42e5fb7297e0ea452945980603f78a28a13209df
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-27 13:18:24 +0900

    test dataset making

---
+++ lora/dataset/13_make_test_dataset.py
@@ -0,0 +1,34 @@
+import os
+from PIL import Image
+import numpy as np
+import argparse, shutil
+from random import sample
+
+
+def main(args):
+    base_folder = args.base_folder
+    categories = os.listdir(base_folder)
+    for category in categories:
+
+        if category == args.trg_category:
+
+            category_folder = os.path.join(base_folder, category)
+            test_folder = os.path.join(category_folder, 'test')
+            folders = os.listdir(test_folder)
+            for folder in folders:
+                if '_' not in folder and folder != 'bad' :
+                    folder_dir = os.path.join(test_folder, folder)
+                    rgb_dir = os.path.join(folder_dir, 'rgb')
+                    gt_dir = os.path.join(folder_dir, 'gt')
+                    new_rgb_dir = os.path.join(test_folder, f'bad/{folder}')
+                    new_gt_dir = os.path.join(test_folder, f'corrected/{folder}')
+                    shutil.copytree(rgb_dir, new_rgb_dir)
+                    shutil.copytree(gt_dir, new_gt_dir)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--base_folder', type=str, default=r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD')
+    parser.add_argument('--trg_category', type=str, default='bagel')
+    args = parser.parse_args()
+    main(args)
--- lora/lora_mytraining.sh
+++ lora/lora_mytraining.sh
@@ -1,101 +1,53 @@
 #!/bin/bash
-
-# sbatch -q big_qos --nodes 2 --output=../result/printing_screen/2_contrastive_learning_eps_0.0_new_code_highrepeat_test.txt training_contrastive.sh
-# sbatch -p big_suma_rtx3090 --qos big_qos --ntasks-per-node 1 --output /home/dreamyou070/Lora/OODLora/result/printing_screen/test_log.log   --cpus-per-gpu=6 train_contrastive_2.sh
-# 1) gpu config : nprocess 2
-echo $CUDA_VISIBLE_DEVICES
-echo $SLURM_NODELIST
-echo $SLURM_NODEID
-source ~/.bashrc
-conda activate venv_lora
-ml purge
-ml load cuda/11.0
-
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port 51189 lora_mytraining.py \
+# dreamyou070
+# qkrtndus0701?!
+# srun -p suma_a6000 -q big_qos --gres=gpu:2 --job-name=lora_9 --time=48:00:00 --pty bash -i
+# conda activate venv_lora
+# cd ./Lora/OODLora/lora
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port 51389 lora_mytraining.py \
   --process_title parksooyeon --max_token_length 225 --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc --wandb_init_name bagel_lora \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --network_module networks.lora --network_dim 64 --network_alpha 4 --train_batch_size 2 \
   --optimizer_type AdamW --lr_scheduler cosine_with_restarts --lr_warmup_steps 144 --learning_rate 0.0003 --unet_lr 0.0001 --text_encoder_lr 0.00005 \
   --resolution '512,512' --save_every_n_epochs 1 --sample_every_n_epochs 1 --sample_prompts ../../../MyData/anomaly_detection/inference.txt --max_train_steps 48000 \
   --train_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD/bagel/train/bad \
-  --valid_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD/bagel/test/bad --seed 42 \
-  --class_caption 'good' --contrastive_eps 0.0 --start_epoch 0 \
-  --logging_dir ../result/MVTec3D-AD_experiment/bagel/lora_training/1_lora_normal_trining_all_res_attnmap/logs --normal_training \
-  --wandb_run_name 1_lora_normal_trining_all_res_attnmap --cross_map_res [64,32,16,8] \
-  --output_dir ../result/MVTec3D-AD_experiment/bagel/lora_training/1_lora_normal_trining_all_res_attnmap
+  --valid_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD/bagel/test/bad --seed 42 --class_caption 'good' --contrastive_eps 0.0 --start_epoch 0 \
+  --logging_dir ../result/MVTec3D-AD_experiment/bagel/lora_training/3_lora_normal_trining_32_res_attnmap/logs --normal_training \
+  --wandb_run_name 3_lora_normal_trining_32_res_attnmap --cross_map_res [32] \
+  --output_dir ../result/MVTec3D-AD_experiment/bagel/lora_training/3_lora_normal_trining_32_res_attnmap
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port 51689 lora_mytraining.py \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port 51889 lora_mytraining.py \
   --process_title parksooyeon --max_token_length 225 --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc --wandb_init_name bagel_lora \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --network_module networks.lora --network_dim 64 --network_alpha 4 --train_batch_size 2 \
   --optimizer_type AdamW --lr_scheduler cosine_with_restarts --lr_warmup_steps 144 --learning_rate 0.0003 --unet_lr 0.0001 --text_encoder_lr 0.00005 \
   --resolution '512,512' --save_every_n_epochs 1 --sample_every_n_epochs 1 --sample_prompts ../../../MyData/anomaly_detection/inference.txt --max_train_steps 48000 \
   --train_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD/bagel/train/bad \
-  --valid_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD/bagel/test/bad --seed 42 \
-  --class_caption 'good' --contrastive_eps 0.0 --start_epoch 0 \
-  --logging_dir ../result/MVTec3D-AD_experiment/bagel/lora_training/6_lora_diff_trining_all_res_attnmap/logs \
-  --wandb_run_name 6_lora_diff_trining_all_res_attnmap --cross_map_res [64,32,16,8] \
-  --output_dir ../result/MVTec3D-AD_experiment/bagel/lora_training/6_lora_diff_trining_all_res_attnmap
+  --valid_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD/bagel/test/bad --seed 42 --class_caption 'good' --contrastive_eps 0.0 --start_epoch 0 \
+  --logging_dir ../result/MVTec3D-AD_experiment/bagel/lora_training/8_lora_diff_trining_32_res_attnmap/logs \
+  --wandb_run_name 8_lora_diff_trining_32_res_attnmap --cross_map_res [32] \
+  --output_dir ../result/MVTec3D-AD_experiment/bagel/lora_training/8_lora_diff_trining_32_res_attnmap
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port 50289 lora_mytraining.py \
-  --logging_dir ../result/logs --process_title parksooyeon --max_token_length 225 \
-  --log_with wandb --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc \
-  --wandb_init_name potato_training \
-  --wandb_run_name 2_2_lora_trining_using_noise_diff_map_res_16 \
-  --seed 42 \
-  --output_dir ../result/MVTec3D-AD_experiment/potato/unet_training/2_2_lora_trining_using_noise_diff_map_res_16 \
-  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
-  --network_module networks.lora \
-  --network_dim 64 --network_alpha 4 --train_batch_size 2 \
-  --optimizer_type AdamW --lr_scheduler cosine_with_restarts --lr_warmup_steps 144 \
-  --learning_rate 0.0003 --unet_lr 0.0001 --text_encoder_lr 0.00005 \
-  --resolution 512,512 \
-  --save_every_n_epochs 1 \
-  --sample_every_n_epochs 1 \
-  --sample_prompts ../../../MyData/object/inference.txt \
-  --max_train_steps 48000 \
-  --train_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD_Experiment_SDXL/potato/train/bad \
-  --class_caption 'good' \
-  --contrastive_eps 0.0 \
-  --start_epoch 0 \
-  --cross_map_res 16
-
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port 53289 lora_mytraining.py \
-  --logging_dir ../result/logs --process_title parksooyeon --max_token_length 225 --seed 42 \
-  --log_with wandb --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc \
-  --wandb_init_name cable_gland_training \
-  --wandb_run_name 2_lora_trining_normal_data_detail_objective \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port 51489 lora_mytraining.py \
+  --process_title parksooyeon --max_token_length 225 --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc --wandb_init_name bagel_lora \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --network_module networks.lora --network_dim 64 --network_alpha 4 --train_batch_size 2 \
-  --optimizer_type AdamW --lr_scheduler cosine_with_restarts --lr_warmup_steps 144 \
-  --learning_rate 0.0003 --unet_lr 0.0001 --text_encoder_lr 0.00005 \
-  --resolution 512,512 --save_every_n_epochs 1 --sample_every_n_epochs 1 \
-  --sample_prompts ../../../MyData/object/inference.txt --max_train_steps 48000 \
-  --output_dir ../result/MVTec3D-AD_experiment/cable_gland/unet_training/2_lora_trining_normal_data_detail_objective \
-  --train_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD_Experiment_SDXL/cable_gland/train_normal/bad \
-  --class_caption 'good' --contrastive_eps 0.0 --start_epoch 0 \
-  --normal_training
+  --optimizer_type AdamW --lr_scheduler cosine_with_restarts --lr_warmup_steps 144 --learning_rate 0.0003 --unet_lr 0.0001 --text_encoder_lr 0.00005 \
+  --resolution '512,512' --save_every_n_epochs 1 --sample_every_n_epochs 1 --sample_prompts ../../../MyData/anomaly_detection/inference.txt --max_train_steps 48000 \
+  --train_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD/bagel/train/bad \
+  --valid_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD/bagel/test/bad --seed 42 --class_caption 'good' --contrastive_eps 0.0 --start_epoch 0 \
+  --logging_dir ../result/MVTec3D-AD_experiment/bagel/lora_training/4_lora_normal_trining_16_res_attnmap/logs --normal_training \
+  --wandb_run_name 4_lora_normal_trining_16_res_attnmap --cross_map_res [16] \
+  --output_dir ../result/MVTec3D-AD_experiment/bagel/lora_training/4_lora_normal_trining_16_res_attnmap
+
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port 51989 lora_mytraining.py \
+  --process_title parksooyeon --max_token_length 225 --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc --wandb_init_name bagel_lora \
+  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
+  --network_module networks.lora --network_dim 64 --network_alpha 4 --train_batch_size 2 \
+  --optimizer_type AdamW --lr_scheduler cosine_with_restarts --lr_warmup_steps 144 --learning_rate 0.0003 --unet_lr 0.0001 --text_encoder_lr 0.00005 \
+  --resolution '512,512' --save_every_n_epochs 1 --sample_every_n_epochs 1 --sample_prompts ../../../MyData/anomaly_detection/inference.txt --max_train_steps 48000 \
+  --train_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD/bagel/train/bad \
+  --valid_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD/bagel/test/bad --seed 42 --class_caption 'good' --contrastive_eps 0.0 --start_epoch 0 \
+  --logging_dir ../result/MVTec3D-AD_experiment/bagel/lora_training/8_lora_diff_trining_16_res_attnmap/logs \
+  --wandb_run_name 8_lora_diff_trining_16_res_attnmap --cross_map_res [16] \
+  --output_dir ../result/MVTec3D-AD_experiment/bagel/lora_training/8_lora_diff_trining_16_res_attnmap
