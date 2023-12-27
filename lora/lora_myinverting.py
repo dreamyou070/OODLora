0bf7130658cf3999af2da9597a4560ba62c363dc
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-27 20:50:33 +0900

    Update lora_myinverting.py

--- lora/lora_myinverting.py
+++ lora/lora_myinverting.py
@@ -74,7 +74,9 @@ def register_attention_control(unet: nn.Module, controller: AttentionStore,  mas
                             attn_vector = back_attn_vector * (1-mask) + obj_attn_vector * (mask)
                             attention_probs_object_sub[:, :, word_idx] = attn_vector
 
-                    map = torch.cat(map_list, dim=0).mean(dim=0)
+                    map = torch.cat(map_list, dim=0)
+                    print(f'before mean, map : {map.shape}')
+                    map = map.mean([0])
                     np_map = np.array(map) * 255
                     aug_map = Image.fromarray(np_map).resize((64, 64))
                     np_aug_map = np.array(aug_map)
