490f2b3d828fea80757410e3546ead6fd0b2f991
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-27 21:28:25 +0900

    .

--- lora/sy.py
+++ lora/sy.py
@@ -4,5 +4,16 @@ from PIL import Image
 import torch
 
 
-map = torch.randn(8,1024)
-map = map.mean(dim=0)
+mask1 = torch.randn((1, 64, 64))
+mask2 = torch.randn((1, 64, 64))
+mask3 = torch.randn((1, 64, 64))
+mask4 = torch.randn((1, 64, 64))
+mask5 = torch.randn((1, 64, 64))
+masks = [mask1, mask2, mask3, mask4, mask5]
+mask_latent = torch.cat(masks, dim=0)
+mask_latent = mask_latent.mean(dim=0, dtype=torch.float32)
+print(mask_latent.shape)
+mask_latent = mask_latent.unsqueeze(0).unsqueeze(0)
+z_noise_pred = torch.randn((1, 4, 64, 64))
+mask_latent = mask_latent.expand(z_noise_pred.shape)
+print(mask_latent.shape)
--- lora/utils/scheduling_utils.py
+++ lora/utils/scheduling_utils.py
@@ -161,10 +161,8 @@ def recon_loop(args, latent_dict, start_latent, context, inference_times, schedu
                 masks = []
                 for layer in layers:
                     mask = mask_dict[layer]
-                    print(f'len of mask : {len(mask)}')
                     mask = mask[0]
                     mask = mask.unsqueeze(0)# [64,64]
-                    print(f'shape of mask : {mask.shape}')
                     masks.append(mask)
                 mask_latent = torch.cat(masks, dim=0)
                 mask_latent = mask_latent.mean(dim=0, dtype=torch.float32)
@@ -172,9 +170,11 @@ def recon_loop(args, latent_dict, start_latent, context, inference_times, schedu
                 z_noise_pred, y_noise_pred = noise_pred.chunk(2)
 
                 mask_latent = mask_latent.unsqueeze(0).unsqueeze(0)
-                mask_latent = mask_latent.expand(z_noise_pred.shape)
+                mask_latent = mask_latent.expand(z_noise_pred.shape).to(z_noise_pred.device)
+
                 back_latent = latent_dict[prev_time]
                 obj_latent = prev_step(y_noise_pred, int(t), x_latent, scheduler)
+
                 y_latent = obj_latent * mask_latent + back_latent * (1 - mask_latent)
             else :
                 y_latent = prev_step(noise_pred, t, x_latent, scheduler)
