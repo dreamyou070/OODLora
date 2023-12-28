e6f5f174d1cba379b409923aa725fe0e056b52d3
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-28 15:39:45 +0900

    Update null_text.py

--- lora/null_inversion/null_text.py
+++ lora/null_inversion/null_text.py
@@ -532,7 +532,7 @@ def main(args) :
 
     scheduler = DDIMScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", clip_sample=False, set_alpha_to_one=False)
 
-    ldm_stable = StableDiffusionPipeline.from_pretrained(args.pretrained_model,
+    ldm_stable = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4",
                                                          scheduler=scheduler).to(device)
     try:
         ldm_stable.disable_xformers_memory_efficient_attention()
