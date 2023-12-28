ca76a49e1b9dcec053b81e8e49fc3e16c328fcad
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-28 17:15:00 +0900

    value checking

--- lora/lora_myinverting.sh
+++ lora/lora_myinverting.sh
@@ -7,7 +7,7 @@ conda activate venv_lora
 ml purge
 ml load cuda/11.0
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 53189 lora_myinverting.py \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 53889 lora_myinverting.py \
   --process_title parksooyeon \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned-emaonly.ckpt \
   --network_module networks.lora --network_dim 64 --network_alpha 4 \
@@ -23,5 +23,5 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --mask_thredhold 0 \
   --num_ddim_steps 50 \
   --final_noising_time  300 \
-  --pixel_mask_res 32 \
+  --pixel_mask_res 8 \
   --other_token_preserving
