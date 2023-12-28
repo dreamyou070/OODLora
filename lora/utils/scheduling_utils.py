f2dae45e8f6f7e27a6b49d44953a5eb5c6eab67d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-28 12:54:46 +0900

    Update scheduling_utils.py

--- lora/utils/scheduling_utils.py
+++ lora/utils/scheduling_utils.py
@@ -190,6 +190,8 @@ def recon_loop(args, latent_dict, start_latent, context, inference_times, schedu
                 obj_latent = prev_step(y_noise_pred, int(t), x_latent, scheduler)
                 #back_position = (1 - mask_latent.to(obj_latent.device)).sum()
                 #print(f'back_position : {back_position}')
+                back_position = 1 - mask_latent
+                print(f'back_position : {back_position.sum()}')
                 y_latent = obj_latent * mask_latent.to(obj_latent.device) + back_latent * (1 - mask_latent.to(obj_latent.device))
                 #y_latent = prev_step(y_noise_pred, int(t), x_latent, scheduler)
             else :
