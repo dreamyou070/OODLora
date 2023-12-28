f381a39473fa0ecee5b054bdc2a082645f2b25b9
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-28 20:02:49 +0900

    code trimming

--- lora/BestModel/sy.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-x = np.array([[1, 2, 3], [4, 5, 6]])
-x_hat = np.array([[1, 5, 2], [8, 7, 1]])
-mse = np.square(x - x_hat) ** 0.5
-binary = np.where(mse > 0.5, 1, 0)
-print(binary)
--- lora/lora_myinverting.sh
+++ lora/lora_myinverting.sh
@@ -7,7 +7,7 @@ conda activate venv_lora
 ml purge
 ml load cuda/11.0
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 53339 lora_myinverting.py \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 56439 lora_myinverting.py \
   --process_title parksooyeon \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned-emaonly.ckpt \
   --network_module networks.lora --network_dim 64 --network_alpha 4 \
@@ -19,10 +19,10 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --output_dir ../result/MVTec3D-AD_experiment/bagel/lora_training/6_2_lora_diff_trining_all_res_attnmap_attn_loss/inference \
   --resolution 512,512 \
   --seed 42 \
-  --cross_map_res [32] \
+  --cross_map_res [64] \
   --mask_thredhold 0 \
   --num_ddim_steps 50 \
   --final_noising_time  300 \
-  --pixel_mask_res 32 \
+  --pixel_mask_res 64 \
   --pixel_thred 0 \
   --other_token_preserving
