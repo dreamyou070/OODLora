c4364861bf76ae8a98e1ded1f0e28295894d4e23
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-28 09:46:23 +0900

    Update scheduling_utils.py

--- lora/utils/scheduling_utils.py
+++ lora/utils/scheduling_utils.py
@@ -165,7 +165,7 @@ def recon_loop(args, latent_dict, start_latent, context, inference_times, schedu
                     mask = mask[0] # [8,1024]
                     head, pix_num = mask.shape
                     mask = torch.reshape(mask, (head, int(pix_num ** 0.5), int(pix_num ** 0.5)))
-                    image = np.array(mask.numpy().astype(np.uint8))
+                    image = np.array(mask.cpu().numpy().astype(np.uint8))
                     map = np.array(Image.fromarray(image).resize((64, 64)))
                     np_map = np.where(map > 20, 1, 0)
                     mask = torch.from_numpy(np_map)#.unsqueeze(0).unsqueeze(0).float()
