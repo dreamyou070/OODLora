a9d73b7a00006eca6acd079888836b10807c0b47
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-29 16:00:20 +0900

    Update inference.py

--- lora/inference.py
+++ lora/inference.py
@@ -256,7 +256,8 @@ def main(args) :
                     org_vae_latent  = image2latent(image_gt_np, vae, device=device, weight_dtype=weight_dtype)
                     mask_vae_latent = image2latent(mask_np,     vae, device=device, weight_dtype=weight_dtype)
 
-                with torch.enable_grad():
+                with torch.no_grad():
+                #with torch.enable_grad():
                     inf_time = inference_times.tolist()
                     inf_time.reverse()  # [0,20,40,60,80,100 , ... 980]
                     org_latent_dict, time_steps, pil_images = ddim_loop(args,
