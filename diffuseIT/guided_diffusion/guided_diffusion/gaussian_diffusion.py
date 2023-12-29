a99b43fe7da7e837368b18ab1fd3efcef18c3178
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-29 11:15:44 +0900

    Update gaussian_diffusion.py

--- diffuseIT/guided_diffusion/guided_diffusion/gaussian_diffusion.py
+++ diffuseIT/guided_diffusion/guided_diffusion/gaussian_diffusion.py
@@ -551,6 +551,7 @@ class GaussianDiffusion:
                                   postprocess_fn=None,    # None
                                   randomize_class=False,  # True
                                   find_init = False):
+        print('Why not here?')
 
         print(f' (1) device')
         if device is None:
