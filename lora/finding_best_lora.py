e8e07f3cb6cdb7e47d1d5b6dc6e7d28ec3e4a314
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-29 19:15:38 +0900

    .

--- lora/finding_best_lora.py
+++ lora/finding_best_lora.py
@@ -111,7 +111,7 @@ def register_attention_control(unet: nn.Module, controller: AttentionStore,
 
 def main(args):
     parent = os.path.split(args.network_weights)[0]
-    args.output_dir = os.path.join(parent, 'inference')
+    args.output_dir = os.path.join(parent, 'anormality_score')
 
     print(f' \n step 1. setting')
     if args.process_title:
@@ -228,7 +228,7 @@ def main(args):
     os.makedirs(test_output_dir, exist_ok=True)
     lines = []
     for class_name in classes:
-        if 'good' in class_name:
+        if 'good' not in class_name:
             class_base_folder = os.path.join(test_output_dir, class_name)
             os.makedirs(class_base_folder, exist_ok=True)
 
@@ -267,10 +267,10 @@ def main(args):
                     score_list = controller.normal_score_list
                     controller.reset()
                     score = sum(score_list)/len(score_list)
-                    line = f'{test_image} : {score}'
+                    line = f'{class_name} : {test_image} : {score}'
                     lines.append(line)
 
-    output_text = os.path.join(output_dir, 'normality_score.txt')
+    output_text = os.path.join(output_dir, 'anormality_score.txt')
     with open(output_text, 'w') as f:
         for line in lines:
             f.write(line + '\n')
--- lora/finding_best_lora.sh
+++ lora/finding_best_lora.sh
@@ -7,13 +7,13 @@ conda activate venv_lora
 ml purge
 ml load cuda/11.0
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 54339 finding_best_lora.py \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 50539 finding_best_lora.py \
   --process_title parksooyeon --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned-emaonly.ckpt \
   --network_module networks.lora --network_dim 64 --network_alpha 4 --prompt 'good' --sample_sampler ddim \
   --concept_image_folder ../../../MyData/anomaly_detection/MVTec3D-AD/bagel \
-  --network_weights ../result/MVTec3D-AD_experiment/bagel/lora_training/9_lora_diff_trining_16_res_attnmap_attn_loss/epoch-000001.safetensors \
+  --network_weights ../result/MVTec3D-AD_experiment/bagel/lora_training/7_lora_diff_trining_64_res_attnmap_attn_loss/epoch-000005.safetensors \
   --resolution 512,512 --seed 42 \
-  --cross_map_res [16] --pixel_mask_res 16 \
+  --cross_map_res [32] --pixel_mask_res 32 \
   --mask_thredhold 0 \
   --num_ddim_steps 1000 \
   --final_noising_time  100 \
