0c43345983aa39f749923469347cf170c2518da8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-30 18:10:29 +0900

    Update inference.py

--- lora/inference.py
+++ lora/inference.py
@@ -70,7 +70,8 @@ def register_attention_control(unet: nn.Module, controller: AttentionStore,  mas
 
                         max_txt_idx = torch.max(attention_probs_back[:, :, 1:], dim=-1).indices  # remove cls token
                         """ is i can trust original img, token should be 0 ( without cls token ) """
-                        position_map = torch.where(max_txt_idx == 0, 1, 0)  # trust of background
+                        #position_map = torch.where(max_txt_idx == 0, 1, 0)  # trust of background
+                        position_map = torch.where(max_txt_idx == 1, 0, 1)  # trust of object
                         map_list.append(position_map)
 
                         map_dict[common_name] = []
