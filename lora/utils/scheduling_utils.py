7ff473d74dd8823a82fc6991bf1a65aa9bf0b464
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-30 17:45:00 +0900

    Update scheduling_utils.py

--- lora/utils/scheduling_utils.py
+++ lora/utils/scheduling_utils.py
@@ -95,6 +95,11 @@ def ddim_loop(args, latent, context, inference_times, scheduler, unet, vae, fina
     latent_dict[0] = latent
     pil_images = []
     flip_times = inference_times
+    # inf_time : [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220,
+    # 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360, 370, 380, 390, 400, 410, 420, 430, 440, 450,
+    # 460, 470, 480, 490, 500, 510, 520, 530, 540, 550, 560, 570, 580, 590, 600, 610, 620, 630, 640, 650, 660, 670, 680,
+    # 690, 700, 710, 720, 730, 740, 750, 760, 770, 780, 790, 800, 810, 820, 830, 840, 850, 860, 870, 880, 890, 900, 910,
+    # 920, 930, 940, 950, 960, 970, 980, 990]
     for i, t in enumerate(flip_times[:-1]):
         next_time = flip_times[i + 1]
         if next_time <= final_time :
@@ -111,6 +116,9 @@ def ddim_loop(args, latent, context, inference_times, scheduler, unet, vae, fina
             time_steps.append(final_time)
             latent_dict[int(final_time)] = latent
             break
+    if final_time not in latent_dict.keys():
+        latent_dict[int(final_time)] = latent
+        
     return latent_dict, time_steps, pil_images
 
 
