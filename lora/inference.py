c42aa0dd2f9d57ba5eb0d2af7a6c7f396c649331
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-30 10:50:10 +0900

    Update inference.py

--- lora/inference.py
+++ lora/inference.py
@@ -135,6 +135,7 @@ def register_attention_control(unet: nn.Module, controller: AttentionStore,  mas
 def main(args) :
 
     parent = os.path.split(args.network_weights)[0]
+    print(f'parent : {parent}')
     super_parent, folder = os.path.split(parent)[0]
     args.output_dir = os.path.join(parent, f'{folder}/inference')
 
