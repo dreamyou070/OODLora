610c830a688ae119d563237ccaef76b0b323cb6e
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2023-12-30 19:58:57 +0900

    make mask for bagel

--- SAM/main.py
+++ SAM/main.py
@@ -16,28 +16,31 @@ def main(args):
     base_folder = args.base_folder
     cats = os.listdir(base_folder)
     for cat in cats:
-        cat_dir = os.path.join(base_folder, f'{cat}/train_ex/bad')
-        save_cat_dir = os.path.join(base_folder, f'{cat}/train_ex/bad_sam')
-        os.makedirs(save_cat_dir, exist_ok=True)
-        folders = os.listdir(cat_dir)
-        for folder in folders:
+        if cat == 'bagel' :
+            cat_dir = os.path.join(base_folder, f'{cat}/train_ex/bad')
+            save_cat_dir = os.path.join(base_folder, f'{cat}/train_ex/bad_sam')
+            os.makedirs(save_cat_dir, exist_ok=True)
+            folders = os.listdir(cat_dir)
+            for folder in folders:
 
-            save_folder_dir = os.path.join(save_cat_dir, folder)
-            os.makedirs(save_folder_dir, exist_ok=True)
+                save_folder_dir = os.path.join(save_cat_dir, folder)
+                os.makedirs(save_folder_dir, exist_ok=True)
 
-            folder_dir = os.path.join(cat_dir, folder)
-            images = os.listdir(folder_dir)
-            for image in images:
-                img_dir = os.path.join(folder_dir, image)
-                np_img = np.array(Image.open(img_dir))
-                predictor.set_image(np_img)
-                input_point = np.array([[0,0]])
-                input_label = np.array([0])
-                masks, scores, logits = predictor.predict(point_coords=input_point,point_labels=input_label,multimask_output=True,)
-                for i, (mask, score) in enumerate(zip(masks, scores)):
-                    np_mask = 255 * (mask * 1)
-                    Image.fromarray(np_mask.astype(np.uint8)).save(os.path.join(save_folder_dir, f'{image[:-4]}_{i}.png'))
-                break
+                folder_dir = os.path.join(cat_dir, folder)
+                images = os.listdir(folder_dir)
+                for image in images:
+                    img_dir = os.path.join(folder_dir, image)
+                    np_img = np.array(Image.open(img_dir))
+                    predictor.set_image(np_img)
+                    input_point = np.array([[0,0]])
+                    input_label = np.array([0])
+                    masks, scores, logits = predictor.predict(point_coords=input_point,point_labels=input_label,multimask_output=True,)
+                    for i, (mask, score) in enumerate(zip(masks, scores)):
+                        if i == 1 :
+                            np_mask = (mask * 1)
+                            np_mask = np.where(np_mask == 1, 0, 1) * 255
+                            Image.fromarray(np_mask.astype(np.uint8)).save(os.path.join(save_folder_dir, image))
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--base_folder', type=str, default=r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD')
