0230a97d095290e64852590f549a6480e44c47b1
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-01 11:00:40 +0900

    Update move_img.py

--- token_compose/train/move_img.py
+++ token_compose/train/move_img.py
@@ -6,7 +6,7 @@ input_json_path = "data/coco_gsam_img/train/metadata.jsonl"
 
 src_dir = "train2017"
 tgt_dir = "coco_gsam_img/train"
-
+os.makedirs(tgt_dir, exist_ok=True)
 input_json_data = []
 with open(input_json_path, "r") as f:
     for line in f:
@@ -18,4 +18,4 @@ for json_data in tqdm(input_json_data):
     tgt_path = os.path.join(tgt_dir, file_name)
 
     command = f"cp {src_path} {tgt_path}"
-    os.system(command)
+    os.system(command) # command = "cp train2017/000000000009.jpg coco_gsam_img/train/000000000009.jpg"
