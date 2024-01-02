9f50f2262ae04c6cd60c23d380f25d640a0d138f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-02 18:19:51 +0900

    Update inference.py

--- lora/inference.py
+++ lora/inference.py
@@ -47,6 +47,7 @@ def register_attention_control(unet: nn.Module, controller: AttentionStore,  mas
             attention_probs = attention_scores.softmax(dim=-1)
             attention_probs = attention_probs.to(value.dtype)
             if is_cross_attention and trg_indexs_list is not None:
+                print(f'registering attn map')
                 trg_probs = attention_probs[:,:,1]
                 controller.store(trg_probs,layer_name)
             hidden_states = torch.bmm(attention_probs, value)
