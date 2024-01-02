f9d1d7591b639767e39c85aea099d91d16641cd0
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-02 16:07:29 +0900

    .

--- lora/sy.py
+++ lora/sy.py
@@ -1,12 +1,9 @@
 import torch
 
 
-a = torch.tensor([[1,2,3,4],
-              [4,5,6,7]])
-a_sum = torch.sum(a, dim=0)/a.shape[0]
-# 5, 7, 9, 11
-print(a_sum)
+
 
 ca_map_obj_1 = torch.randn((8,8,8,77))
+print(ca_map_obj_1.shape)
 a = ca_map_obj_1[:,:,:,1:5].softmax(dim=-1)
 print(a.shape)
