823e9835ef91c6e6127604faf5c30972b63b2299
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-02 20:11:57 +0900

    cls checking

--- lora/inference.py
+++ lora/inference.py
@@ -51,7 +51,7 @@ def register_attention_control(unet: nn.Module, controller: AttentionStore,
             attention_probs = attention_scores.softmax(dim=-1)
             attention_probs = attention_probs.to(value.dtype)
             if is_cross_attention:
-                trg_map = attention_probs[:, :, 1]
+                trg_map = attention_probs[:, :, 0]
                 controller.store(trg_map, layer_name)
 
             hidden_states = torch.bmm(attention_probs, value)
--- lora/inference.sh
+++ lora/inference.sh
@@ -11,5 +11,5 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --process_title parksooyeon --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --network_module networks.lora --network_dim 64 --network_alpha 4 --prompt 'hole' --sample_sampler ddim \
   --resolution '512,512' --concept_image_folder ../../../MyData/anomaly_detection/MVTec3D-AD/bagel --resolution 512,512 --seed 42 \
-  --network_weights ../result/MVTec3D-AD_experiment/bagel/lora_training/8_anormal_sample_training_not_use_attn_loss_more_hole_good_captio_use/models/epoch-000010.safetensors \
+  --network_weights ../result/MVTec3D-AD_experiment/bagel/lora_training/8_anormal_sample_training_not_use_attn_loss_more_hole_good_captio_use/models/epoch-000008.safetensors \
   --num_ddim_steps 50 --final_noising_time 980 --pixel_mask_res 16 --pixel_thred 0.3 --cross_map_res [64,32,16,8] --org_latent_attn_map_check
