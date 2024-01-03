765e85fc49f89f310d8f8f70c17d81ad76e35134
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-03 22:08:02 +0900

    ad recon file

--- lora/sy.py
+++ lora/sy.py
@@ -1,7 +1,7 @@
 import torch
 
 
-a = torch.randn(8, 32, 3)
-attn_list = [a,a]
-aa = torch.cat(attn_list, dim=0)
-print(aa.shape)
+trigger = torch.randn(32,32)
+anormal_map = torch.flatten(trigger).unsqueeze(0)
+anormal_map = anormal_map.repeat(8, 1)
+print(anormal_map.shape)
