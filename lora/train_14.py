8b243753c09ae34d79e6acf56212eb1122d27fab
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-03 15:24:05 +0900

    Update train_14.py

--- lora/train_14.py
+++ lora/train_14.py
@@ -681,8 +681,8 @@ class NetworkTrainer:
                                 anormal_mask = batch["anormal_masks"][0][res].unsqueeze(0)  # [1,1,res,res], foreground = 1
                                 mask = anormal_mask.squeeze()  # res,res
                                 mask = torch.stack([mask.flatten() for i in range(8)], dim=0)#.unsqueeze(-1)  # 8, res*res, 1
-                                print(f'score_map (8, res*res) : {score_map.shape}')
-                                print(f'mask (8, res*res, 1) : {mask.shape}')
+                                #print(f'score_map (8, res*res) : {score_map.shape}')
+                                #print(f'mask (8, res*res, 1) : {mask.shape}')
                                 activation = (score_map * mask).sum(dim=-1)
                                 total_score = (score_map).sum(dim=-1)
                                 activation_loss = (1 - (activation / total_score)) ** 2  # 8, res*res
