bdb68be43820aed4a7cbc05e58567534c1072d52
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-04 10:24:13 +0900

    Update inference.sh

--- lora/inference.sh
+++ lora/inference.sh
@@ -11,5 +11,5 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --process_title parksooyeon --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --network_module networks.lora --network_dim 64 --network_alpha 4 --prompt 'good' --sample_sampler ddim \
   --resolution '512,512' --concept_image_folder ../../../MyData/anomaly_detection/MVTec3D-AD/bagel --resolution 512,512 --seed 42 \
-  --network_weights ../result/MVTec3D-AD_experiment/bagel/lora_training/18_normal_sample_training_res_64_32_change_to_max_with_down/models \
+  --network_weights ../result/MVTec3D-AD_experiment/bagel/lora_training/17_normal_sample_training_res_64_change_to_max/models \
   --num_ddim_steps 50 --final_noising_time 980 --pixel_mask_res 32 --pixel_thred 0.3 --cross_map_res [64,32,16,8] --org_latent_attn_map_check
