ce8aa0bef531d1cee6021fe69dc212993cd9d0aa
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-04 18:56:36 +0900

    lora retraining

--- lora/ad_recon.sh
+++ lora/ad_recon.sh
@@ -1,6 +1,6 @@
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 53289 ad_recon.py \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 52009 ad_recon.py \
   --process_title parksooyeon --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --network_module networks.lora --network_dim 64 --network_alpha 4 --prompt 'good' --sample_sampler ddim \
   --resolution '512,512' --concept_image_folder ../../../MyData/anomaly_detection/MVTec3D-AD/bagel --resolution 512,512 --seed 42 \
   --network_weights ../result/MVTec3D-AD_experiment/bagel/lora_training/res_64_up/models \
-  --num_ddim_steps 50 --final_noising_time 980 --cross_map_res [64] --pixel_copy                        --mask_thredhold 0.005  --pixel_thredhold 0.5
+  --num_ddim_steps 200 --final_noising_time 980 --cross_map_res [64] --pixel_copy
