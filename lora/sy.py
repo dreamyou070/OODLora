10b27962d64c7e7f19aaa46149f4014a5fe66969
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-05 18:41:17 +0900

    Update sy.py

--- lora/sy.py
+++ lora/sy.py
@@ -11,10 +11,10 @@ for line in content :
     print(f'class_name: {class_name}, img_name: {img_name}')
     score_list = line_list[2:]
     for score_elem in score_list :
-        print(score)
+        print(score_elem)
         name = score_elem.split('=')[0]
         score = score_elem.split('=')[1]
-    
+
     break
 
 # combined | 001.png | res_64_down_attn_0=839 | res_64_down_attn_1=1528 | res_32_down_attn_0=3121 | res_32_down_attn_1=3650 | res_32_up_attn_0=2963 | res_32_up_attn_1=3659 | res_32_up_attn_2=3959 | res_64_up_attn_0=14341 | res_64_up_attn_1=12120 | res_64_up_attn_2=8650 | down_64=1329 | down_32=3819 | up_32=3810 | up_64=11452
