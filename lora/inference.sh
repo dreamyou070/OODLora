009780bcde735af07b001b0e1f8e5927a420a6f4
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-06 08:36:44 +0900

    check code again

--- lora/inference.sh
+++ lora/inference.sh
@@ -5,9 +5,9 @@ echo $SLURM_NODEID
 source ~/.bashrc
 conda activate venv_lora
 ml purge
-ml load cuda/11.0
-
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 51616 inference.py \
+ml load cuda/11.00
+# srun -p suma_a6000 -q big_qos --gres=gpu:2 --pty bash -i
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 56464 inference.py \
   --process_title parksooyeon \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --network_module networks.lora \
@@ -18,7 +18,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --resolution '512,512' \
   --concept_image_folder ../../../MyData/anomaly_detection/MVTec3D-AD/bagel \
   --seed 42 \
-  --network_weights ../result/MVTec3D-AD_experiment/bagel/lora_training/res_16_up_down_text_embedding/models \
+  --network_weights ../result/MVTec3D-AD_experiment/bagel/lora_training/res_64_up_down_text_embedding/models \
   --num_ddim_steps 50 \
-  --cross_map_res [16]
-  --
+  --cross_map_res [16] \
+  --parser.add_argument "['down', 'up']"
--- lora/inference_layerwise_text_embedding.sh
+++ lora/inference_layerwise_text_embedding.sh
@@ -1,6 +1,16 @@
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 50989 inference_layerwise_text_embedding.py \
-  --process_title parksooyeon --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
-  --network_module networks.lora --network_dim 64 --network_alpha 4 --prompt 'good' --sample_sampler ddim \
-  --resolution '512,512' --concept_image_folder ../../../MyData/anomaly_detection/MVTec3D-AD/bagel --resolution 512,512 --seed 42 \
-  --network_weights ../result/MVTec3D-AD_experiment/bagel/lora_training/res_64_32_up_down_text_embedding/models \
-  --num_ddim_steps 50 --final_noising_time 980 --pixel_mask_res 32 --pixel_thred 0.3 --cross_map_res [64,32] --org_latent_attn_map_check --cross_map_position "['up','down']"
+  --process_title parksooyeon \
+  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
+  --network_module networks.lora \
+  --network_dim 64 \
+  --network_alpha 4 \
+  --prompt 'good' \
+  --sample_sampler ddim \
+  --resolution '512,512' \
+  --concept_image_folder ../../../MyData/anomaly_detection/MVTec3D-AD/bagel \
+  --seed 42 \
+  --network_weights ../result/MVTec3D-AD_experiment/bagel/lora_training/res_8_mid_text_embedding/models \
+  --num_ddim_steps 50 \
+  --final_noising_time 980 \
+  --cross_map_res [8] \
+  --trg_position "['mid']"
