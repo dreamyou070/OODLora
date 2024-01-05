600086e8ac81cfa4c3bedc8c5c0d5e3d4712fd44
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-05 18:37:54 +0900

    Update sy.py

--- lora/sy.py
+++ lora/sy.py
@@ -1,6 +1,6 @@
 import os
 
-record_dir = r'../result/MVTec3D-AD_experiment/bagel/lora_training/res_64_32_up_down_text_embedding/per_res_normalized_cross_attention_map/score_record'
+record_dir = r'../result/MVTec3D-AD_experiment/bagel/lora_training/res_64_32_up_down_text_embedding/per_res_normalized_cross_attention_map/score_record/score_epoch_1.txt'
 with open(record_dir, 'r') as f :
     content = f.readlines()
 print(content)
