65f5cd24c918f053c78b56f753451c1621b4ccfc
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-06 17:00:53 +0900

    Update ad_recon_layerwise_text_embedding.py

--- lora/ad_recon_layerwise_text_embedding.py
+++ lora/ad_recon_layerwise_text_embedding.py
@@ -449,7 +449,7 @@ def main(args) :
                                                     pil_img_512.save(
                                                         os.path.join(trg_img_output_dir, f'{name}_pixel_mask{ext}'))
                                                 # ----------------------------------------------------------------------
-                                                pixel_mask = pixel_mask.where(pixel_mask > 0.5, 1, 0)
+                                                pixel_mask = torch.where(pixel_mask > 0.5, 1, 0)
                                                 if pixel_mask.dim() == 2:
                                                     pixel_mask = pixel_mask.unsqueeze(0).unsqueeze(0)
                                                 if pixel_mask.dim() == 3:
