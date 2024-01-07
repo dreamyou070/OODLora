689674349183af7da343460c8cb0b47a7868b1f4
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-07 15:51:29 +0900

    Update test_potato.py

--- SAM/test_potato.py
+++ SAM/test_potato.py
@@ -39,8 +39,8 @@ def main(args):
                 predictor.set_image(np_img)
 
                 h, w, c = np_img.shape
-                trg_h_1, trg_w_1 = h / 2, w / 3
-                trg_h_2, trg_w_2 = h * (1 / 2), w * (2 / 3)
+                trg_h_1, trg_w_1 = h / 2, w * (5 / 11)
+                trg_h_2, trg_w_2 = h * (1 / 2), w * (6 / 11)
                 input_point = np.array([[trg_h_1, trg_w_1], [trg_h_2, trg_w_2]])
                 input_label = np.array([1, 1])
                 masks, scores, logits = predictor.predict(point_coords=input_point, point_labels=input_label,
@@ -49,7 +49,7 @@ def main(args):
                 for i, (mask, score) in enumerate(zip(masks, scores)):
                     if i == 1:
                         np_mask = (mask * 1)
-                        np_mask = np.where(np_mask == 1, 0, 1) * 255
+                        np_mask = np.where(np_mask == 1, 1, 0) * 255  # if true,  be black
                         sam_result_pil = Image.fromarray(np_mask.astype(np.uint8))
                         sam_result_pil.save(os.path.join(sam_train_dir, image))
             # -------------------------------------------------------------------------------------------------------
@@ -64,8 +64,8 @@ def main(args):
 
                 predictor.set_image(np_img)
                 h, w, c = np_img.shape
-                trg_h_1, trg_w_1 = h / 2, w / 3
-                trg_h_2, trg_w_2 = h * (1 / 2), w * (2 / 3)
+                trg_h_1, trg_w_1 = h / 2, w * (5 / 11)
+                trg_h_2, trg_w_2 = h * (1 / 2), w * (6 / 11)
                 input_point = np.array([[trg_h_1, trg_w_1], [trg_h_2, trg_w_2]])
                 input_label = np.array([1, 1])
                 masks, scores, logits = predictor.predict(point_coords=input_point, point_labels=input_label,
@@ -74,7 +74,7 @@ def main(args):
                 for i, (mask, score) in enumerate(zip(masks, scores)):
                     if i == 1:
                         np_mask = (mask * 1)
-                        np_mask = np.where(np_mask == 1, 0, 1) * 255
+                        np_mask = np.where(np_mask == 1, 1, 0) * 255  # if true,  be black
                         sam_result_pil = Image.fromarray(np_mask.astype(np.uint8))
                         sam_result_pil.save(os.path.join(sam_test_dir, image))
 
@@ -89,8 +89,8 @@ def main(args):
                 np_img = np.array(Image.open(img_dir))
                 predictor.set_image(np_img)
                 h, w, c = np_img.shape
-                trg_h_1, trg_w_1 = h / 2, w / 3
-                trg_h_2, trg_w_2 = h * (1 / 2), w * (2 / 3)
+                trg_h_1, trg_w_1 = h / 2, w * (5 / 11)
+                trg_h_2, trg_w_2 = h * (1 / 2), w * (6 / 11)
                 input_point = np.array([[trg_h_1, trg_w_1], [trg_h_2, trg_w_2]])
                 input_label = np.array([1, 1])
                 masks, scores, logits = predictor.predict(point_coords=input_point, point_labels=input_label,
@@ -99,7 +99,7 @@ def main(args):
                 for i, (mask, score) in enumerate(zip(masks, scores)):
                     if i == 1:
                         np_mask = (mask * 1)
-                        np_mask = np.where(np_mask == 1, 0, 1) * 255
+                        np_mask = np.where(np_mask == 1, 1, 0) * 255  # if true,  be black
                         sam_result_pil = Image.fromarray(np_mask.astype(np.uint8))
                         sam_result_pil.save(os.path.join(sam_validation_dir, image))
 
