3dc2a6052f599f8b272d33b3dda40b5a45eff6c8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-07 16:53:27 +0900

    test peach

--- lora/train.sh
+++ lora/train.sh
@@ -5,7 +5,7 @@
 # srun -p suma_a6000 -q big_qos --job-name=lora_train_5 --gres=gpu:2 --time=48:00:00 --pty bash -i
 # conda activate venv_lora
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port 50545 train.py \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port 50606 train.py \
   --process_title parksooyeon \
   --log_with wandb \
   --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc \
@@ -25,10 +25,10 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_c
   --seed 42 \
   --class_caption 'good' \
   --start_epoch 0 \
-  --wandb_init_name dowel \
-  --train_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD/dowel/train_ex/rgb \
-  --valid_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD/dowel/test_ex/rgb \
-  --output_dir ../result/MVTec3D-AD_experiment/dowel/lora_training/res_64_up
+  --wandb_init_name foam \
+  --train_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD/foam/train_ex/rgb \
+  --valid_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD/foam/test_ex/rgb \
+  --output_dir ../result/MVTec3D-AD_experiment/foam/lora_training/res_64_up
 
 
   --network_weights ../result/MVTec3D-AD_experiment/foam/lora_training/res_64_up/models/epoch-000004.safetensors \
