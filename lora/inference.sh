bf949c1546c261a2fb416e1e5685c03d2f29c71c
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-09 12:30:57 +0900

    Update inference.sh

--- lora/inference.sh
+++ lora/inference.sh
@@ -8,7 +8,7 @@
 # cd ./Lora/OODLora/lora/
 # conda activate venv_lora
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 51111 inference.py \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 50111 inference.py \
   --process_title parksooyeon \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --network_module networks.lora \
@@ -19,11 +19,11 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --resolution '512,512' \
   --seed 42 \
   --num_ddim_steps 50 \
-  --cross_map_res [64,32] \
+  --cross_map_res [64,16] \
   --trg_position "['up','down']" \
   --concept_image_folder ../../../MyData/anomaly_detection/MVTec3D-AD/cable_gland \
-  --network_weights ../result/MVTec3D-AD_experiment/cable_gland/lora_training/res_64_up_32_up_down_normal_10_anormal_80_bent_120/models \
-  --start_epoch 0
+  --network_weights ../result/MVTec3D-AD_experiment/cable_gland/lora_training/res_64_up_16_up_down_normal_10_anormal_80_bent_120/models \
+  --start_epoch 13
 
 # -------------------------------------------------------------------------------------------------------------------------------------------- #
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 51661 inference_cookie.py \
