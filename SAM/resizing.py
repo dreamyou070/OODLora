5c07f11cfe4e250fc32ef5f4c3ef3026bed24b15
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-10 17:53:20 +0900

    Update resizing.py

--- SAM/resizing.py
+++ SAM/resizing.py
@@ -5,13 +5,6 @@ import numpy as np
 
 def main(args):
 
-    print(f'step 1. prepare model')
-
-    model_type = "vit_h"
-    path_to_checkpoint= r'/home/dreamyou070/pretrained_stable_diffusion/sam_vit_h_4b8939.pth'
-    sam = sam_model_registry[model_type](checkpoint=path_to_checkpoint)
-    predictor = SamPredictor(sam)
-
     print(f'step 2. prepare images')
     base_folder = args.base_folder
     cats = os.listdir(base_folder)
@@ -32,7 +25,7 @@ def main(args):
                     img_rgb_dir = os.path.join(folder_rgb_dir, image)
                     Image.open(img_gt_dir).resize((512,512)).convert('L').save(img_gt_dir)
                     Image.open(img_rgb_dir).resize((512,512)).save(img_rgb_dir)
-                    
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--base_folder', type=str, default=r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD')
