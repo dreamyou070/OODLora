e304d0d14533d48fe52d01e711ba40cc5ba9f7e0
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-11 13:33:44 +0900

    Update train.sh

--- lora/train.sh
+++ lora/train.sh
@@ -8,17 +8,17 @@
 # conda activate venv_lora
 
 class_name="carrot"
-data_source='train_ex'
-save_folder="1_7_res_64_down_32_up_from_normal_eighth_epoch"
-start_folder="0_7_res_64_down_32_up_normal"
-trg_lora_model="epoch-000008.safetensors"
+data_source='train_normal'
+save_folder="0_10_res_64_up_down_32_up_normal"
+#start_folder="0_7_res_64_down_32_up_normal"
+#trg_lora_model="epoch-000008.safetensors"
 start_epoch=0
-port_number=55888
+port_number=55810
 
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${save_folder}"
-start_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${start_folder}"
-network_weights="${start_dir}/models/${trg_lora_model}"
+#start_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${start_folder}"
+#network_weights="${start_dir}/models/${trg_lora_model}"
 
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port $port_number train.py \
@@ -37,7 +37,5 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_c
   --start_epoch $start_epoch \
   --output_dir "$output_dir" \
   --cross_map_res [64,32] \
-  --detail_64_down \
   --trg_position "['up']" \
-  --trg_part "['attn_0','attn_1','attn_2']" \
-  --network_weights "$network_weights"
+  --trg_part "['attn_0','attn_1','attn_2']"
