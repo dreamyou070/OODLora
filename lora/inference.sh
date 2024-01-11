b17daa2d9a1410239088b55901bd067f16f5b6d3
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-11 10:40:12 +0900

    Update inference.sh

--- lora/inference.sh
+++ lora/inference.sh
@@ -4,34 +4,25 @@
 # srun -p suma_a6000 -q big_qos --gres=gpu:1 --time=2-0 --pty bash -i
 # srun -p suma_a6000 -q big_qos --gres=gpu:2 --time=2-0 --pty bash -i
 # srun -p suma_rtx4090 -q big_qos --gres=gpu:2 --time=2-0 --pty bash -i
-
-
 # cd ./Lora/OODLora/lora/
 # conda activate venv_lora
+
+class_name="carrot"
+start_epoch=0
+port_number=51185
+save_folder="0_5_res_64_down_16_up_normal"
+
+concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${save_folder}"
+network_weights="${output_dir}/models"
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 59452 inference.py \
   --process_title parksooyeon \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --network_module networks.lora \
   --network_dim 64 --network_alpha 4 --prompt 'good' --sample_sampler ddim --resolution '512,512' --seed 42 \
-  --num_ddim_steps 50 \
-  --cross_map_res [64,32] \
+  --cross_map_res [64,16] \
   --trg_position "['up']" \
-  --concept_image_folder ../../../MyData/anomaly_detection/MVTec3D-AD/carrot \
-  --network_weights ../result/MVTec3D-AD_experiment/carrot/lora_training/0_4_res_64_up_32_up_normal/models \
-  --start_epoch 0
-
-# -------------------------------------------------------------------------------------------------------------------------------------------- #
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 51661 inference_cookie.py \
-  --process_title parksooyeon \
-  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
-  --network_module networks.lora \
-  --network_dim 64 \
-  --network_alpha 4 \
-  --prompt 'good' \
-  --sample_sampler ddim \
-  --resolution '512,512' \
-  --seed 42 \
-  --num_ddim_steps 50 \
-  --concept_image_folder ../../../MyData/anomaly_detection/MVTec3D-AD/cookie \
-  --network_weights ../result/MVTec3D-AD_experiment/cookie/lora_training/res_64_up_16_up_down_up_normal_10_anormal_80/models
+  --concept_image_folder "${concept_image_folder}" \
+  --network_weights "${network_weights}" \
+  --start_epoch ${start_epoch}
