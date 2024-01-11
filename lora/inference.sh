2de83b4fa7b6e8d3aebabf9a918acc02e01df99e
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-11 10:42:04 +0900

    Update inference.sh

--- lora/inference.sh
+++ lora/inference.sh
@@ -22,7 +22,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --network_module networks.lora \
   --network_dim 64 --network_alpha 4 --prompt 'good' --sample_sampler ddim --resolution '512,512' --seed 42 \
   --cross_map_res [64,16] \
-  --trg_position "['up']" \
+  --trg_position "['up','down']" \
   --concept_image_folder "${concept_image_folder}" \
   --network_weights "${network_weights}" \
   --start_epoch ${start_epoch}
