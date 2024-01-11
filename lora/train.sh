f72c5748079d2ce66a09fb5984f69995768484bf
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-11 10:29:01 +0900

    Update train.sh

--- lora/train.sh
+++ lora/train.sh
@@ -8,14 +8,14 @@
 # conda activate venv_lora
 
 class_name="carrot"
-#trg_lora_model="epoch-000003.safetensors"
-start_epoch=0
-port_number=51178
-save_folder="0_8_res_64_down_32_up_down_normal"
+trg_lora_model="epoch-000003.safetensors"
+start_epoch=3
+port_number=51171
+save_folder="0_1_res_64_up_16_up_normal"
 
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/train_normal/rgb"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${save_folder}"
-#network_weights="${output_dir}/models/${trg_lora_model}"
+network_weights="${output_dir}/models/${trg_lora_model}"
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port $port_number train.py \
   --process_title parksooyeon \
@@ -32,8 +32,9 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_c
   --train_data_dir "$train_data_dir" \
   --start_epoch $start_epoch \
   --output_dir "$output_dir" \
-  --cross_map_res [64,32] \
-  --detail_64_down \
-  --trg_position "['up','down']" \
-  --trg_part "['attn_2','attn_1','attn_0']"
+  --cross_map_res [64,16] \
+  --detail_64_up \
+  --trg_position "['up']" \
+  --trg_part "['attn_2','attn_1','attn_0']" \
+  --network_weights "$network_weights" \
 
