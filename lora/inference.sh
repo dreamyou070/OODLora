336bc71793e833cb48a7907121ca94349f935a58
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-11 14:29:33 +0900

    inference again

--- lora/inference.sh
+++ lora/inference.sh
@@ -7,10 +7,10 @@
 # srun -p suma_rtx4090 -q big_qos --gres=gpu:2 --time=2-0 --pty bash -i
 # cd ./Lora/OODLora/lora/
 # conda activate venv_lora
-class_name="carrot"
-start_epoch=14
-port_number=54710
-save_folder="0_8_res_64_down_32_up_down_normal"
+class_name="cookie"
+start_epoch=1
+port_number=54711
+save_folder="2_1_res_64_up_attn_2_part"
 
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${save_folder}"
@@ -21,8 +21,8 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --network_module networks.lora \
   --network_dim 64 --network_alpha 4 --prompt 'good' --sample_sampler ddim --resolution '512,512' --seed 42 \
-  --cross_map_res [64,32] \
-  --trg_position "['up','down']" \
+  --cross_map_res [64] \
+  --trg_position "['up']" \
   --concept_image_folder "${concept_image_folder}" \
   --network_weights "${network_weights}" \
   --start_epoch ${start_epoch}
