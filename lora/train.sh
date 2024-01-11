24aadde0e0d483c859dacb5f4fb466dfd5c05adc
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-11 18:05:33 +0900

    Update train.sh

--- lora/train.sh
+++ lora/train.sh
@@ -10,11 +10,11 @@
 
 class_name="cookie"
 data_source='train_ex'
-start_folder="1_res_64_up_16_up_double_mask"
-save_folder="1_res_64_up_16_up_double_mask"
-trg_lora_model="epoch-000002.safetensors"
+start_folder="0_res_64_up_16_up_only_normal"
+save_folder="2_1_res_64_up_attn_2_part_normal_double_mask"
+trg_lora_model="last.safetensors"
 start_epoch=0
-port_number=55816
+port_number=55836
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${save_folder}"
 start_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${start_folder}"
@@ -36,6 +36,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_c
   --train_data_dir "$train_data_dir" \
   --start_epoch $start_epoch \
   --output_dir "$output_dir" \
-  --cross_map_res [64,16] \
+  --cross_map_res [64] \
+  --detail_64_up \
   --trg_position "['up']" \
-  --trg_part "['attn_0','attn_1','attn_2']"
+  --trg_part "['attn_2']"
