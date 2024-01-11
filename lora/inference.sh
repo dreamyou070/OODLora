a987b1e01bd36c58da34722b7ff82881d334b9f5
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-11 10:00:08 +0900

    make train shell file

--- lora/inference.sh
+++ lora/inference.sh
@@ -3,22 +3,22 @@
 # qkrtndus0701?!
 # srun -p suma_a6000 -q big_qos --gres=gpu:1 --time=2-0 --pty bash -i
 # srun -p suma_a6000 -q big_qos --gres=gpu:2 --time=2-0 --pty bash -i
-# srun -p suma_rtx4090 -q big_qos --gres=gpu:1 --time=2-0 --pty bash -i
+# srun -p suma_rtx4090 -q big_qos --gres=gpu:2 --time=2-0 --pty bash -i
 
 
 # cd ./Lora/OODLora/lora/
 # conda activate venv_lora
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 59442 inference.py \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port 59452 inference.py \
   --process_title parksooyeon \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --network_module networks.lora \
   --network_dim 64 --network_alpha 4 --prompt 'good' --sample_sampler ddim --resolution '512,512' --seed 42 \
   --num_ddim_steps 50 \
   --cross_map_res [64,32] \
-  --trg_position "['up','down']" \
+  --trg_position "['up']" \
   --concept_image_folder ../../../MyData/anomaly_detection/MVTec3D-AD/carrot \
-  --network_weights ../result/MVTec3D-AD_experiment/carrot/lora_training/12_res_64_up_down_res_32_up_down/models \
+  --network_weights ../result/MVTec3D-AD_experiment/carrot/lora_training/0_4_res_64_up_32_up_normal/models \
   --start_epoch 0
 
 # -------------------------------------------------------------------------------------------------------------------------------------------- #
--- lora/train.sh
+++ lora/train.sh
@@ -1,3 +1,4 @@
+#!bin/bash
 # dreamyou070
 # qkrtndus0701?!
 # srun -p suma_a6000 -q big_qos --gres=gpu:2 --time=2-0 --pty bash -i
@@ -6,9 +7,29 @@
 # cd ./Lora/OODLora/lora/
 # conda activate venv_lora
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port 52040 train.py \
-  --process_title parksooyeon --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors --network_module networks.lora --network_dim 64 --network_alpha 4 --train_batch_size 1 \
-  --optimizer_type AdamW --lr_scheduler cosine_with_restarts --lr_warmup_steps 144 --learning_rate 0.0003 --unet_lr 0.0001 --text_encoder_lr 0.00005 --resolution '512,512' --save_every_n_epochs 1 --sample_every_n_epochs 1 --sample_prompts ../../../MyData/anomaly_detection/inference.txt --max_train_steps 1000000 --use_attn_loss --task_loss_weight 1.0 --seed 42 --class_caption 'good' --start_epoch 0 \
-  --wandb_init_name carrot --start_epoch 0 --train_data_dir ../../../MyData/anomaly_detection/MVTec3D-AD/carrot/train_normal/rgb \
-  --output_dir ../result/MVTec3D-AD_experiment/carrot/lora_training/13_res_64_up_16_up_only_normal \
-  --cross_map_res [64,16] --detail_64_up --trg_position "['up']" --warmup_epochs 6
+class_name="carrot"
+trg_lora_mode = "epoch-000003.safetensors"
+start_epoch=3
+
+train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/train_normal/rgb"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/0_3_res_64_up_16_up_down_normal"
+network_weights="${output_dir}/models/${trg_lora_mode}"
+
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port 51119 train.py \
+  --process_title parksooyeon \
+  --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
+  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
+  --network_module networks.lora \
+  --network_dim 64 --network_alpha 4 --train_batch_size 1 \
+  --optimizer_type AdamW --lr_scheduler cosine_with_restarts \
+  --lr_warmup_steps 144 --learning_rate 0.0003 --unet_lr 0.0001 --text_encoder_lr 0.00005 --resolution '512,512' --save_every_n_epochs 1 \
+  --sample_every_n_epochs 1 \
+  --sample_prompts ../../../MyData/anomaly_detection/inference.txt \
+  --max_train_steps 70000 --use_attn_loss --task_loss_weight 1.0 --seed 42 --class_caption 'good' --start_epoch 0 \
+  --wandb_init_name "$class_name" \
+  --train_data_dir "$train_data_dir" \
+  --start_epoch $start_epoch \
+  --output_dir "$output_dir" \
+  --network_weights "$network_weights" \
+  --cross_map_res [64,16] --detail_64_down --trg_position "['up','down']" --trg_part "['attn_2','attn_1','attn_0']"
+
