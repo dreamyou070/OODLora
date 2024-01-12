e8dda46d53b7428ddfb9ce7ac3a0c740e3b1f170
ParkSooYeon <dreamyou070@yonsei.ac.kr>
2024-01-12 23:49:53 +0900

    inference

--- lora/inference.sh
+++ lora/inference.sh
@@ -8,11 +8,11 @@
 # cd ./Lora/OODLora/lora/
 # conda activate venv_lora
 
-class_name="foam"
+class_name="cookie"
 start_epoch=0
-port_number=58011
+port_number=58111
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/normal/res_64_down_32_up"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/res_64_up_16_up_truncate_3_cls_training_normal_weight_2_third_code"
 network_weights="${output_dir}/models"
 
 
@@ -21,8 +21,8 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --network_module networks.lora \
   --network_dim 64 --network_alpha 4 --prompt 'good' --sample_sampler ddim --resolution '512,512' --seed 42 \
-  --cross_map_res [64,32] \
-  --detail_64_down \
+  --cross_map_res [64,16] \
+  --detail_64_up \
   --trg_position "['up']" \
   --concept_image_folder "${concept_image_folder}" \
   --network_weights "${network_weights}" \
