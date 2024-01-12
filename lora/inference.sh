0267425b2e8e08910d748fdff493cdd652b8ac48
ParkSooYeon <dreamyou070@yonsei.ac.kr>
2024-01-12 21:47:01 +0900

    inference

--- lora/inference.sh
+++ lora/inference.sh
@@ -7,15 +7,14 @@
 # srun -p suma_rtx4090 -q big_qos --gres=gpu:2 --time=2-0 --pty bash -i
 # cd ./Lora/OODLora/lora/
 # conda activate venv_lora
-class_name="cookie"
-start_epoch=0
-port_number=50233
 
+class_name="foam"
+start_epoch=0
+port_number=50211
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/res_64_up_16_up_truncate_3_cls_training_normal_weight_2"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/normal/res_64_up_16_up"
 network_weights="${output_dir}/models"
 
-port_number=50253
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port ${port_number} inference.py \
   --process_title parksooyeon \
