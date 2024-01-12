bb9c1de9347b3f48aef104d4ead8a2c5a823f660
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-12 16:50:58 +0900

    Update train.sh

--- lora/train.sh
+++ lora/train.sh
@@ -10,8 +10,8 @@
 
 # network_weights": "../result/MVTec3D-AD_experiment/cookie/lora_training/0_res_64_up_16_up_only_normal/models/epoch-000003.safetensors
 # 0_res_64_up_16_up_only_normal
-class_name="cookie"
-data_source='train_ex'
+class_name="carrot"
+data_source='train_normal'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 
 #start_folder="0_10_res_64_up_down_32_up_normal_truncate_pad_3_part_all_cls_training"
@@ -19,10 +19,10 @@ train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${dat
 #start_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${start_folder}"
 #network_weights="${start_dir}/models/${trg_lora_model}"
 
-save_folder="0_res_64_up_16_up_pad_3_cls_training_with_background_anormal_weight_5"
+save_folder="0_res_64_up_down_32_up_normal"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${save_folder}"
 
-port_number=50325
+port_number=50301
 start_epoch=0
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config --main_process_port $port_number train.py \
@@ -40,12 +40,10 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --train_data_dir "$train_data_dir" \
   --start_epoch $start_epoch \
   --output_dir "$output_dir" \
-  --cross_map_res [64,16] \
-  --detail_64_up \
+  --cross_map_res [64,32] \
   --trg_position "['up']" \
   --truncate_pad \
   --truncate_length 3 \
   --trg_part '["attn_2","attn_1","attn_0"]' \
-  --anormal_weight 5.0 \
   --anormal_sample_normal_loss \
   --cls_training
