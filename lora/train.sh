53a9b7cca3775f74505d203cba2aba295d0559fc
ParkSooYeon <dreamyou070@yonsei.ac.kr>
2024-01-12 20:29:40 +0900

    Update train.sh

--- lora/train.sh
+++ lora/train.sh
@@ -1,13 +1,13 @@
 #!bin/bash
 
-class_name="cookie"
-data_source='train_ex'
+class_name="foam"
+data_source='train_normal'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 
-save_folder="res_64_up_16_up_truncate_3_cls_training_normal_weight_2"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/${save_folder}"
+save_folder="res_64_up_16_up_down"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/normal/${save_folder}"
 
-port_number=50332
+port_number=50337
 start_epoch=0
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config --main_process_port $port_number train.py \
@@ -27,10 +27,10 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --output_dir "$output_dir" \
   --cross_map_res [64,16] \
   --detail_64_up \
-  --trg_position "['up']" \
+  --trg_position "['up','down']" \
   --truncate_pad \
   --truncate_length 3 \
   --trg_part '["attn_2","attn_1","attn_0"]' \
   --anormal_sample_normal_loss \
-  --normal_weight 2.0 \
+  --detail_64_up \
   --cls_training
