d3f76b17a24848bff40f8644ed517debebd30083
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-12 13:24:45 +0900

    Update inference.sh

--- lora/inference.sh
+++ lora/inference.sh
@@ -8,11 +8,11 @@
 # cd ./Lora/OODLora/lora/
 # conda activate venv_lora
 class_name="cookie"
-start_epoch=2
-port_number=50033
+start_epoch=0
+port_number=50013
 
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/0_res_64_up_16_up_only_normal_pad_3_cls_training"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/1_res_64_up_16_up_part_all_pad_3_cls_training_with_anormal_not_anormal_cls"
 network_weights="${output_dir}/models"
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port ${port_number} inference.py \
