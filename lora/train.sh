486a3a0ba70fb963969376a360ce316a62a151aa
ParkSooYeon <dreamyou070@yonsei.ac.kr>
2024-01-12 22:08:15 +0900

    .

--- lora/train.sh
+++ lora/train.sh
@@ -1,13 +1,13 @@
 #!bin/bash
 
 class_name="foam"
-data_source='train_ex'
+data_source='train_normal'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 
-save_folder="res_64_up_32_up_down"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/${save_folder}"
+save_folder="res_64_up_32_up"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/normal/${save_folder}"
 
-port_number=53339
+port_number=53341
 start_epoch=0
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config --main_process_port $port_number train.py \
@@ -27,7 +27,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --output_dir "$output_dir" \
   --cross_map_res [64,32] \
   --detail_64_up \
-  --trg_position "['up','down']" \
+  --trg_position "['up']" \
   --truncate_pad  --truncate_length 3 \
   --trg_part '["attn_2","attn_1","attn_0"]' \
   --anormal_sample_normal_loss \
