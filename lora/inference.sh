06ffcefadfde0647a2c6311d8629af4992116ab2
ParkSooYeon <dreamyou070@yonsei.ac.kr>
2024-01-12 21:19:16 +0900

    Update inference.sh

--- lora/inference.sh
+++ lora/inference.sh
@@ -12,7 +12,7 @@ start_epoch=0
 port_number=50233
 
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/res_64_up_32_up_truncate_3_cls_training_normal_weight_2"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/res_64_up_16_up_truncate_3_cls_training_normal_weight_2"
 network_weights="${output_dir}/models"
 
 port_number=50253
@@ -22,7 +22,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --network_module networks.lora \
   --network_dim 64 --network_alpha 4 --prompt 'good' --sample_sampler ddim --resolution '512,512' --seed 42 \
-  --cross_map_res [64,32] \
+  --cross_map_res [64,16] \
   --trg_position "['up']" \
   --concept_image_folder "${concept_image_folder}" \
   --network_weights "${network_weights}" \
