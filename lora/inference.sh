170a455c3d9f27cb77868996e737500eabcd2cf4
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-12 09:05:47 +0900

    Update inference.sh

--- lora/inference.sh
+++ lora/inference.sh
@@ -13,7 +13,7 @@ port_number=51111
 save_folder = "0_10_res_64_up_down_32_up_normal_truncate_pad_3_part_all_cls_training"
 
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${save_folder}"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/0_10_res_64_up_down_32_up_normal_truncate_pad_3_part_all_cls_training"
 network_weights="${output_dir}/models"
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port ${port_number} inference.py \
