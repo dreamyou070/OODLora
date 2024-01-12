47e27f22c4b833c5e860c1dcb49e6252f0635e79
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-12 11:30:07 +0900

    Update inference.sh

--- lora/inference.sh
+++ lora/inference.sh
@@ -9,7 +9,7 @@
 # conda activate venv_lora
 class_name="cookie"
 start_epoch=0
-port_number=50008
+port_number=50018
 
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/0_res_64_up_16_up_only_normal"
