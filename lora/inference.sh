d9d652350a366062f669d728b161c018f58ce45d
ParkSooYeon <dreamyou070@yonsei.ac.kr>
2024-01-13 01:17:23 +0900

    inference again

--- lora/inference.sh
+++ lora/inference.sh
@@ -10,9 +10,9 @@
 
 class_name="foam"
 start_epoch=0
-port_number=52931
+port_number=52123
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/normal/res_64_up_down_32_up_down"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/normal/res_64_down_32_up"
 network_weights="${output_dir}/models"
 
 
