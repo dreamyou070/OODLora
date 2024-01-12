df133529dc3645b25fdbeeeab02e75fca2dfdb99
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-13 08:25:22 +0900

    Update inference.sh

--- lora/inference.sh
+++ lora/inference.sh
@@ -9,10 +9,10 @@
 # conda activate venv_lora
 
 class_name="cookie"
-start_epoch=0
-port_number=50004
+start_epoch=2
+port_number=50005
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/res_64_up_16_up_truncate_3_cls_training_normal_weight_6_third_code"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/res_64_up_16_up_truncate_3_cls_training_normal_weight_4_third_code"
 network_weights="${output_dir}/models"
 
 
