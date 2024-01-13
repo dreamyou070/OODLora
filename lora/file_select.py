3263f04f6c563e2c2ed97fbf877bff4f9ffe31a7
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-13 20:00:04 +0900

    file rearrange code

--- lora/file_select.py
+++ lora/file_select.py
@@ -6,23 +6,23 @@ for folder in folders:
     folder_dir = os.path.join(base_folder, folder)
     files = os.listdir(folder_dir)
     for file in files :
-        if file == 'res_64_up_16_up' :
-            if 'length_2' in file :
-                file_dir_ = os.path.join(folder_dir,f'{file}' )
-                f_dir = os.path.join(file_dir_,'trg_res_check_cross_attention_map')
-                f_dir_ = os.path.join(f_dir, 'test_set')
+        #if file == 'res_64_up_16_up' :
+        if 'length_2' in file :
+            file_dir_ = os.path.join(folder_dir,f'{file}' )
+            f_dir = os.path.join(file_dir_,'trg_res_check_cross_attention_map')
+            f_dir_ = os.path.join(f_dir, 'test_set')
 
-                loras = os.listdir(f_dir_)
-                for lora in loras :
-                    lora_dir = os.path.join(f_dir_, lora)
-                    cats = os.listdir(lora_dir)
-                    for cat in cats :
-                        cat_dir = os.path.join(lora_dir, cat)
-                        sub_cats = os.listdir(cat_dir)
-                        for sub_cat in sub_cats :
-                            sub_cat_dir = os.path.join(cat_dir, sub_cat)
-                            images = os.listdir(sub_cat_dir)
-                            for img in images :
-                                img_dir = os.path.join(sub_cat_dir, img)
-                                if 'down' in img :
-                                    os.remove(img_dir)
+            loras = os.listdir(f_dir_)
+            for lora in loras :
+                lora_dir = os.path.join(f_dir_, lora)
+                cats = os.listdir(lora_dir)
+                for cat in cats :
+                    cat_dir = os.path.join(lora_dir, cat)
+                    sub_cats = os.listdir(cat_dir)
+                    for sub_cat in sub_cats :
+                        sub_cat_dir = os.path.join(cat_dir, sub_cat)
+                        images = os.listdir(sub_cat_dir)
+                        for img in images :
+                            img_dir = os.path.join(sub_cat_dir, img)
+                            if 'down' in img :
+                                os.remove(img_dir)
