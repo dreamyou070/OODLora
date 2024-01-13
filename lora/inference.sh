30610fea5f037af25a849b8eead1ea03685c3639
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-13 11:45:50 +0900

    Update inference.sh

--- lora/inference.sh
+++ lora/inference.sh
@@ -12,7 +12,7 @@ class_name="carrot"
 start_epoch=0
 port_number=50025
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/res_64_up_down_32_up_down"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/res_64_up_down_32_up_down_text_len_2"
 network_weights="${output_dir}/models"
 
 
