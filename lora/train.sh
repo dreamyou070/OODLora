d74c3141eb324f9864d985b8326ab3706f16d1d0
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-13 14:03:08 +0900

    Update train.sh

--- lora/train.sh
+++ lora/train.sh
@@ -4,13 +4,13 @@ class_name="foam"
 data_source='train_ex'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 
-save_folder="res_64_up_32_up_down_train_text_3_no_background_loss"
+save_folder="res_64_up_32_up_down_train_text_77_no_background_loss"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/${save_folder}"
-port_number=50083
+port_number=57783
 
 start_epoch=0
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port $port_number train.py \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config --main_process_port $port_number train.py \
   --process_title parksooyeon \
   --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
@@ -30,4 +30,4 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_c
   --trg_position "['up','down']" \
   --trg_part '["attn_2","attn_1","attn_0"]' \
   --anormal_sample_normal_loss \
-  --truncate_pad --truncate_length 3 --cls_training
+  --truncate_pad --truncate_length 77 --cls_training
