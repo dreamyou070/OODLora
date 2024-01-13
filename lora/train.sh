eb0004854d829df9534347335fd8e641817d195e
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-13 09:56:29 +0900

    Update train.sh

--- lora/train.sh
+++ lora/train.sh
@@ -4,13 +4,13 @@ class_name="cookie"
 data_source='train_normal'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 
-save_folder="res_64_up_down_16_up_down"
+save_folder="res_64_down_16_up_down"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/normal/${save_folder}"
-port_number=50012
+port_number=50013
 
 start_epoch=0
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config --main_process_port $port_number train.py \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port $port_number train.py \
   --process_title parksooyeon \
   --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
@@ -26,6 +26,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --start_epoch $start_epoch \
   --output_dir "$output_dir" \
   --cross_map_res [64,16] \
+  --detail_64_down \
   --trg_position "['up','down']" \
   --truncate_pad \
   --truncate_length 3 \
