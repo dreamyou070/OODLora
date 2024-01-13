414f5bacb2dbb0b81c23b4a82e7eb4601efd0240
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-13 20:42:07 +0900

    Update inference.sh

--- lora/inference.sh
+++ lora/inference.sh
@@ -12,7 +12,7 @@ class_name="cookie"
 start_epoch=0
 port_number=50082
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/res_64_down_16_up_down_text_3"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/res_64_up_16_up_text_3"
 network_weights="${output_dir}/models"
 
 
@@ -22,7 +22,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --network_module networks.lora \
   --network_dim 64 --network_alpha 4 --prompt 'good' --sample_sampler ddim --resolution '512,512' --seed 42 \
   --cross_map_res [64] \
-  --trg_position "['down']" \
+  --trg_position "['up']" \
   --concept_image_folder "${concept_image_folder}" \
   --network_weights "${network_weights}" \
   --truncate_length 2 \
