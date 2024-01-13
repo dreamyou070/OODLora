3c4b619445500fc9001466d9072aacfaa1ee9474
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-13 23:04:56 +0900

    Update inference.sh

--- lora/inference.sh
+++ lora/inference.sh
@@ -8,11 +8,11 @@
 # cd ./Lora/OODLora/lora/
 # conda activate venv_lora
 
-class_name="cookie"
+class_name="foam"
 start_epoch=0
-port_number=50011
+port_number=50000
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/res_64_up_16_up_text_3_background_loss"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/64_down_16_down"
 network_weights="${output_dir}/models"
 
 
@@ -22,7 +22,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --network_module networks.lora \
   --network_dim 64 --network_alpha 4 --prompt 'good' --sample_sampler ddim --resolution '512,512' --seed 42 \
   --cross_map_res [64] \
-  --trg_position "['up']" \
+  --trg_position "['down']" \
   --concept_image_folder "${concept_image_folder}" \
   --network_weights "${network_weights}" \
   --truncate_length 2 \
