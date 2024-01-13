17a3db1cd88c0270d6b5672961c32492facfb3fc
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-13 19:32:26 +0900

    Update inference.sh

--- lora/inference.sh
+++ lora/inference.sh
@@ -10,9 +10,9 @@
 
 class_name="cookie"
 start_epoch=0
-port_number=50000
+port_number=50001
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/res_64_up_down_32_up_down_text_77"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/res_64_up_down_32_up_down_text_3"
 network_weights="${output_dir}/models"
 
 
