07954ad09c6155e6c9d001980e97a944f9ff2def
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-13 09:45:19 +0900

    Update train.sh

--- lora/train.sh
+++ lora/train.sh
@@ -4,9 +4,9 @@ class_name="cookie"
 data_source='train_normal'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 
-save_folder="res_64_down_16_up_down"
+save_folder="res_64_down_16_up"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/normal/${save_folder}"
-port_number=50011
+port_number=50012
 
 start_epoch=2
 
@@ -27,7 +27,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_c
   --output_dir "$output_dir" \
   --cross_map_res [64,16] \
   --detail_64_down \
-  --trg_position "['up','down']" \
+  --trg_position "['up']" \
   --truncate_pad \
   --truncate_length 3 \
   --trg_part '["attn_2","attn_1","attn_0"]' \
