0fb9583573cf1bdcc56a06bbe843514a51d6bac3
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-13 23:40:29 +0900

    Update train.sh

--- lora/train.sh
+++ lora/train.sh
@@ -4,9 +4,9 @@ class_name="foam"
 data_source='train_normal'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 
-save_folder="res_64_down_res_16_up_down_test_3_backgroundloss"
+save_folder="res_64_up_res_16_down_test_3_backgroundloss"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/${save_folder}"
-port_number=50302
+port_number=50303
 
 start_epoch=0
 
@@ -26,7 +26,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --start_epoch $start_epoch \
   --output_dir "$output_dir" \
   --cross_map_res [64,16] \
-  --detail_64_down \
-  --trg_position "['up','down']" \
+  --detail_64_up \
+  --trg_position "['down']" \
   --trg_part '["attn_2","attn_1","attn_0"]' \
   --anormal_sample_normal_loss --background_loss --truncate_pad --truncate_length 3 --cls_training
