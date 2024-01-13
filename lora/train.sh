f5931f1d04a5951e440185a8b609a89f657a8699
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-13 10:13:47 +0900

    train without background

--- lora/train.sh
+++ lora/train.sh
@@ -1,16 +1,16 @@
 #!bin/bash
 
-class_name="cookie"
-data_source='train_normal'
+class_name="foam"
+data_source='train_ex'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 
-save_folder="res_64_down_16_up"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/normal/${save_folder}"
-port_number=50013
+save_folder="res_64_up_32_up_down_without_background_loss"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/${save_folder}"
+port_number=50021
 
 start_epoch=0
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config --main_process_port $port_number train.py \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config --main_process_port $port_number train.py \
   --process_title parksooyeon \
   --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
@@ -25,9 +25,9 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --train_data_dir "$train_data_dir" \
   --start_epoch $start_epoch \
   --output_dir "$output_dir" \
-  --cross_map_res [64,16] \
-  --detail_64_down \
-  --trg_position "['up']" \
+  --cross_map_res [64,32] \
+  --detail_64_up \
+  --trg_position "['up','down']" \
   --truncate_pad \
   --truncate_length 3 \
   --trg_part '["attn_2","attn_1","attn_0"]' \
