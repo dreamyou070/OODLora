a8337fcb1bee3247d07c83f49151e7904d660d48
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-14 13:33:44 +0900

    Update rope_check.py

--- SAM/rope_check.py
+++ SAM/rope_check.py
@@ -23,17 +23,25 @@ def main(args):
             train_rgb_dir = os.path.join(train_ex_dir, 'rgb')
             train_gt_dir = os.path.join(train_ex_dir, 'gt')
             train_pixel_mask_dir = os.path.join(train_ex_dir, 'mask')
-
+            re_train_pixel_mask_dir = os.path.join(train_ex_dir, 'mask_re')
+            os.makedirs(re_train_pixel_mask_dir, exist_ok=True)
 
             folders = os.listdir(train_pixel_mask_dir)
             for folder in folders:
                 rgb_folder_dir = os.path.join(train_pixel_mask_dir, folder)
+                re_rgb_folder_dir = os.path.join(re_train_pixel_mask_dir, folder)
+                os.makedirs(re_rgb_folder_dir, exist_ok=True)
                 images = os.listdir(rgb_folder_dir)
                 for image in images:
                     rgb_img_dir = os.path.join(rgb_folder_dir, image)
                     np_img = np.array(Image.open(rgb_img_dir))
-                    h, w, c = np_img.shape
-                    print(f'h : {h} w : {w} c : {c}')
+                    h, w = np_img.shape
+                    min_h , max_h = int(h/3), int(h*2/3)
+                    for h_index in range(h) :
+                        if h_index < min_h or h_index > max_h :
+                            np_img[h_index, :] = 0
+                    new_mask = Image.fromarray(np_img)
+                    new_mask.save(os.path.join(re_rgb_folder_dir, image))
 
 
 if __name__ == "__main__":
