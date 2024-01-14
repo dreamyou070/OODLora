bdbfd962cf1bc43f1591f05b4cd5c6499e020fb4
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-14 23:58:38 +0900

    Update train.sh

--- lora/train.sh
+++ lora/train.sh
@@ -10,7 +10,7 @@ output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/
 port_number=54919
 start_epoch=0
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config --main_process_port $port_number train.py \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config --main_process_port $port_number train.py \
   --process_title parksooyeon \
   --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
