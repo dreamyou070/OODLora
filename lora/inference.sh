9be28b226fd352fecfb439f4abb8678b7c209913
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-14 14:35:05 +0900

    Update inference.sh

--- lora/inference.sh
+++ lora/inference.sh
@@ -10,9 +10,9 @@
 
 class_name="peach"
 start_epoch=0
-port_number=50300
+port_number=50301
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/res_64_up_down_res_16_up_down_text_3_backgroundloss"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/res_64_up_down_res_32_up_down_text_3_backgroundloss"
 network_weights="${output_dir}/models"
 
 
