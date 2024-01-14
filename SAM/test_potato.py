19787eb9aac15bf933de38e49c9d152634e41e71
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-14 15:06:58 +0900

    Update test_potato.py

--- SAM/test_potato.py
+++ SAM/test_potato.py
@@ -15,10 +15,19 @@ def main(args):
     print(f'step 2. prepare images')
     base_folder = args.base_folder
     trg_img_dir = os.path.join(base_folder, args.sub_dir)
-
     np_img = np.array(Image.open(trg_img_dir))
-    predictor.set_image(np_img)
     h, w, c = np_img.shape
+    h_min, h_max = 0, int(h * 4/10)
+    w_min, w_max = 0, int(w * 1/5)
+    for h_ in range(h_min, h_max, 1):
+        for w_ in range(w_min, w_max, 1):
+            np_img[h_, w_, :] = 0
+    new_mask = Image.fromarray(np_img)
+    new_mask.save('test.png')
+    """
+    
+    predictor.set_image(np_img)
+    
     trg_h_1, trg_w_1 = h / 2, w * (4 / 11)
     trg_h_2, trg_w_2 = h * (1 / 2), w * (7 / 11)
     input_point = np.array([[trg_h_1, trg_w_1], [trg_h_2, trg_w_2]])
@@ -32,10 +41,11 @@ def main(args):
             np_mask = np.where(np_mask == 1, 1, 0) * 255  # if true,  be black
             sam_result_pil = Image.fromarray(np_mask.astype(np.uint8))
             sam_result_pil.save('test.png')
+    """
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--base_folder', type=str, default=r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD')
-    parser.add_argument('--sub_dir', type=str, default='potato/train_ex/rgb/30_good/val_001.png')
+    parser.add_argument('--sub_dir', type=str, default='rope/train_ex/rgb/80_open/open_008.png')
     args = parser.parse_args()
     main(args)
