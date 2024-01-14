adea3040849caa948864a04a4d4e6346f405764b
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-14 13:38:39 +0900

    Update rope_check.py

--- SAM/rope_check.py
+++ SAM/rope_check.py
@@ -18,7 +18,7 @@ def main(args):
     for cat in cats:
         if cat == args.trg_cat:
             cat_dir = os.path.join(base_folder, f'{cat}')
-            train_ex_dir = os.path.join(cat_dir, 'train_ex')
+            train_ex_dir = os.path.join(cat_dir, 'test_ex')
 
             train_rgb_dir = os.path.join(train_ex_dir, 'rgb')
             train_gt_dir = os.path.join(train_ex_dir, 'gt')
@@ -28,20 +28,21 @@ def main(args):
 
             folders = os.listdir(train_pixel_mask_dir)
             for folder in folders:
-                rgb_folder_dir = os.path.join(train_pixel_mask_dir, folder)
-                re_rgb_folder_dir = os.path.join(re_train_pixel_mask_dir, folder)
-                os.makedirs(re_rgb_folder_dir, exist_ok=True)
-                images = os.listdir(rgb_folder_dir)
-                for image in images:
-                    rgb_img_dir = os.path.join(rgb_folder_dir, image)
-                    np_img = np.array(Image.open(rgb_img_dir))
-                    h, w = np_img.shape
-                    min_h , max_h = int(h/3), int(h*2/3)
-                    for h_index in range(h) :
-                        if h_index < min_h or h_index > max_h :
-                            np_img[h_index, :] = 0
-                    new_mask = Image.fromarray(np_img)
-                    new_mask.save(os.path.join(re_rgb_folder_dir, image))
+                if 'good' in folder:
+                    rgb_folder_dir = os.path.join(train_pixel_mask_dir, folder)
+                    re_rgb_folder_dir = os.path.join(re_train_pixel_mask_dir, folder)
+                    os.makedirs(re_rgb_folder_dir, exist_ok=True)
+                    images = os.listdir(rgb_folder_dir)
+                    for image in images:
+                        rgb_img_dir = os.path.join(rgb_folder_dir, image)
+                        np_img = np.array(Image.open(rgb_img_dir))
+                        h, w = np_img.shape
+                        min_h , max_h = int(h/3), int(h*2/3)
+                        for h_index in range(h) :
+                            if h_index < min_h or h_index > max_h :
+                                np_img[h_index, :] = 0
+                        new_mask = Image.fromarray(np_img)
+                        new_mask.save(os.path.join(re_rgb_folder_dir, image))
 
 
 if __name__ == "__main__":
