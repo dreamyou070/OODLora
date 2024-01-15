753e79ac2e213485407b2f7484d7173a0fef8fc0
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-15 20:23:06 +0900

    Update train_normal.sh

--- lora/train_normal.sh
+++ lora/train_normal.sh
@@ -4,12 +4,12 @@ class_name="carrot"
 data_source='train_normal'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 normal_folder='normal'
-save_folder="1_2_res_64_up_down_32_up_down_back_cls_training"
+save_folder="2_2_res_64_up_down_32_up_down_back_no_cls_training"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
-port_number=54944
+port_number=54945
 start_epoch=0
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config --main_process_port $port_number train_normal.py \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config --main_process_port $port_number train_normal.py \
   --process_title parksooyeon \
   --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
@@ -26,4 +26,4 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --output_dir "$output_dir" \
   --cross_map_res [64,32] --detail_64_up --detail_64_down \
   --trg_position "['up','down']" \
-  --trg_part '["attn_2","attn_1","attn_0"]' --cls_training
+  --trg_part '["attn_2","attn_1","attn_0"]'
