77edfaccfe5501015b3552878d453649fb4f9b7a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-15 20:40:30 +0900

    Update train_normal.sh

--- lora/train_normal.sh
+++ lora/train_normal.sh
@@ -4,9 +4,9 @@ class_name="cookie"
 data_source='train_normal'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 normal_folder='normal'
-save_folder="1_2_res_64_up_16_up_back_cls_training"
+save_folder="2_2_res_64_up_16_up_back_no_cls_training"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
-port_number=54946
+port_number=54947
 start_epoch=0
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config --main_process_port $port_number train_normal.py \
@@ -26,4 +26,4 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --output_dir "$output_dir" \
   --cross_map_res [64,16] --detail_64_up \
   --trg_position "['up']" \
-  --trg_part '["attn_2","attn_1","attn_0"]' --cls_training
+  --trg_part '["attn_2","attn_1","attn_0"]'
