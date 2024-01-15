c9b2f412f478bfe85381028d8bcb5e782009775e
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-15 15:49:20 +0900

    Update train_anormal.sh

--- lora/train_anormal.sh
+++ lora/train_anormal.sh
@@ -1,12 +1,12 @@
 #!bin/bash
 
 class_name="carrot"
-data_source='train_ex'
+data_source='train_normal'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
-normal_folder='anormal'
+normal_folder='normal'
 save_folder="2_res_64_up_down_32_up_down_no_back_no_cls_training"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
-port_number=54934
+port_number=54932
 start_epoch=0
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config --main_process_port $port_number train_anormal.py \
