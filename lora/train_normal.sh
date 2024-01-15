400393171ea62fa4d0fea0f194f2037fbe79c207
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-16 00:50:24 +0900

    Update train_normal.sh

--- lora/train_normal.sh
+++ lora/train_normal.sh
@@ -26,4 +26,4 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --output_dir "$output_dir" \
   --cross_map_res [64,32] --detail_64_up --trg_position "['up']" --cls_training \
   --trg_part '["attn_2","attn_1","attn_0"]' \
-  --truncate_pad --pad_length 3
+  --truncate_pad --truncate_length 3
