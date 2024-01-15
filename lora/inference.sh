232499e3d9a91c3bccaf1d807c57183f542f1471
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-15 20:47:00 +0900

    Update inference.sh

--- lora/inference.sh
+++ lora/inference.sh
@@ -8,12 +8,12 @@
 # cd ./Lora/OODLora/lora/
 # conda activate venv_lora
 
-class_name="carrot"
+class_name="cookie"
 start_epoch=0
-port_number=50316
+port_number=50333
 
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/normal/1_1_res_64_up_down_32_up_down_no_back_cls_training"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/normal/1_1_res_64_up_16_up_no_back_cls_training"
 network_weights="${output_dir}/models"
 
 
@@ -23,8 +23,8 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --network_module networks.lora \
   --network_dim 64 --network_alpha 4 \
   --prompt 'good' --sample_sampler ddim --resolution '512,512' --seed 42 \
-  --cross_map_res [64,32] \
-  --trg_position "['up','down']" \
+  --cross_map_res [64,16] \
+  --trg_position "['up']" \
   --concept_image_folder "${concept_image_folder}" \
   --network_weights "${network_weights}" \
   --truncate_length 2 \
