9dd89fbe8d0c878080d8a83d481ae0ff7ac392bc
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-16 12:08:09 +0900

    Update train_normal.sh

--- lora/train_normal.sh
+++ lora/train_normal.sh
@@ -1,14 +1,14 @@
 #!bin/bash
 
-class_name="foam"
+class_name="carrot"
 data_source='train_normal'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 normal_folder='normal'
-save_folder="1_2_res_64_up_16_up_down_no_back_cls_training"
+save_folder="1_1_res_64_up_32_up_no_back_cls_training"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
-network_weights="${output_dir}/models/epoch-000045.safetensors"
-port_number=54942
-start_epoch=45
+network_weights="${output_dir}/models/epoch-000066.safetensors"
+port_number=54966
+start_epoch=66
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config --main_process_port $port_number train_normal.py \
   --process_title parksooyeon \
@@ -25,7 +25,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --train_data_dir "$train_data_dir" \
   --start_epoch $start_epoch \
   --output_dir "$output_dir" \
-  --cross_map_res [64,16] --detail_64_up --trg_position "['up','down']" --cls_training \
+  --cross_map_res [64,32] --detail_64_up --trg_position "['up']" --cls_training \
   --trg_part '["attn_2","attn_1","attn_0"]' \
   --network_weights "$network_weights" \
   --truncate_pad --truncate_length 3
