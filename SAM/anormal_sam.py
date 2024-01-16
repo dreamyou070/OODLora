985f98452ac65f285437959a41ccbf2409ca1a4d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-17 08:34:43 +0900

    anormal sam

--- SAM/anormal_sam.py
+++ SAM/anormal_sam.py
@@ -38,32 +38,17 @@ def main(args):
                         np_img = np.array(Image.open(rgb_img_dir))
                         predictor.set_image(np_img)
                         h, w, c = np_img.shape
-                        trg_h_0, trg_w_0 = h / 2, w * (1 / 11)
-                        trg_h_1, trg_w_1 = h / 2, w * (2 / 11)
-                        trg_h_15, trg_w_15 = h / 2, w * (3 / 11)
-                        trg_h_2, trg_w_2 = h * (1 / 2), w * (4 / 11)
-                        trg_h_3, trg_w_3 = h * (1 / 2), w * (6 / 11)
-                        trg_h_35, trg_w_35 = h * (1 / 2), w * (7 / 11)
-                        trg_h_4, trg_w_4 = h * (1 / 2), w * (8 / 11)
-                        trg_h_45, trg_w_45 = h * (1 / 2), w * (9 / 11)
-                        trg_h_5, trg_w_5 = h * (1 / 2), w * (10 / 11)
-                        input_point = np.array([[trg_h_0, trg_w_0],
-                                                [trg_h_1, trg_w_1],
-                                                [trg_h_15, trg_w_15],
-                                                [trg_h_2, trg_w_2],
-                                                [trg_h_3, trg_w_3],
-                                                [trg_h_35, trg_w_35],
-                                                [trg_h_4, trg_w_4],
-                                                [trg_h_45, trg_w_45],
-                                                [trg_h_5, trg_w_5]])
-                        input_label = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1])
+                        trg_h_0, trg_w_01 = h / 4, w * (6 / 12)
+                        trg_h_1 = h / 2
+                        input_point = np.array([[trg_h_1, trg_w_01]])
+                        input_label = np.array([1])
+
                         masks, scores, logits = predictor.predict(point_coords=input_point, point_labels=input_label,
                                                                   multimask_output=True, )
-
                         for i, (mask, score) in enumerate(zip(masks, scores)):
                             if i == 2:
                                 np_mask = (mask * 1)
-                                np_mask = np.where(np_mask == 1, 0, 1) * 255  # if true,  be black
+                                np_mask = np.where(np_mask == 1, 1, 0) * 255
                                 sam_result_pil = Image.fromarray(np_mask.astype(np.uint8))
                                 sam_result_pil.save(os.path.join(mask_folder_dir, image))
 
@@ -71,6 +56,6 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--base_folder', type=str, default=r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD')
-    parser.add_argument('--trg_cat', type=str, default='rope')
+    parser.add_argument('--trg_cat', type=str, default='tire')
     args = parser.parse_args()
     main(args)
--- lora/sy.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-
-import math
-import numpy as np
-
-attn_score = torch.randn((10,3,4))
-
-print(my.shape)
