91d7755375f629bc36ed600bf896aa9d1eb90f9e
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-16 21:48:02 +0900

    Update train_anormal.sh

--- lora/train_anormal.sh
+++ lora/train_anormal.sh
@@ -1,12 +1,12 @@
 #!bin/bash
 
-class_name="cookie"
+class_name="carrot"
 data_source='train_ex'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 normal_folder='anormal'
-save_folder="1_1_res_64_up_16_up_cls_training_from_42_epoch_normal_without_background"
+save_folder="1_4_res_64_up_16_up_cls_training"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
-network_weights="../result/MVTec3D-AD_experiment/cookie/lora_training/normal/1_1_res_64_up_16_up_no_back_cls_training/models/epoch-000037.safetensors"
+network_weights="../result/MVTec3D-AD_experiment/cookie/lora_training/normal/1_4_res_64_up_16_up_cls_training/models/epoch-000002.safetensors"
 
 port_number=54176
 start_epoch=0
