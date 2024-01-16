8ead17b3b2019501d8408aa053ee917b24a839e7
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-16 23:43:12 +0900

    Update train_anormal.sh

--- lora/train_anormal.sh
+++ lora/train_anormal.sh
@@ -4,7 +4,7 @@ class_name="cookie"
 data_source='train_ex'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 normal_folder='anormal'
-save_folder="1_1_res_64_up_16_up_from_5epoch"
+save_folder="1_3_res_64_up_16_up_from_5epoch"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
 network_weights="../result/MVTec3D-AD_experiment/${class_name}/lora_training/normal/3_20240116_res_64_up_16_up_cls_training/models/epoch-000005.safetensors"
 
@@ -28,4 +28,4 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_c
   --output_dir "$output_dir" \
   --cross_map_res [64,16] --detail_64_up --trg_position "['up']" \
   --trg_part '["attn_2","attn_1","attn_0"]' --truncate_pad --truncate_length 3 --cls_training \
-  --network_weights "$network_weights" --normal_with_background \
+  --network_weights "$network_weights"
