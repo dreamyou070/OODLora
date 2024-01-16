edf13fce92df20af66631cd9cc6336502db75253
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-16 21:39:55 +0900

    Update inference.sh

--- lora/inference.sh
+++ lora/inference.sh
@@ -8,12 +8,12 @@
 # cd ./Lora/OODLora/lora/
 # conda activate venv_lora
 
-class_name="foam"
-start_epoch=174
+class_name="carrot"
+start_epoch=0
 port_number=50515
 
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/normal/1_1_res_64_up_16_up_no_back_cls_training"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/normal/1_4_res_64_up_16_up_cls_training"
 network_weights="${output_dir}/models"
 
 
