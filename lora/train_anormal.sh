a802fb2e679ccd8f343681266bda80e72c11bc9a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-17 21:03:52 +0900

    .

--- lora/train_anormal.sh
+++ lora/train_anormal.sh
@@ -28,4 +28,5 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --output_dir "$output_dir" \
   --cross_map_res [64,16] --detail_64_up --trg_position "['up']" \
   --trg_part '["attn_2","attn_1","attn_0"]' --truncate_pad --truncate_length 3 --cls_training \
-  --normal_with_background --network_weights "$network_weights"
+  --normal_with_background \
+  --network_weights "$network_weights"
