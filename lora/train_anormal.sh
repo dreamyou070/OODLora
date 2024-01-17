a01c558d29c7960bdbca616d52e113223ed204a6
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-17 16:03:56 +0900

    anormal sam tire

--- lora/train_anormal.sh
+++ lora/train_anormal.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-class_name="dowel"
+class_name="tire"
 data_source='train_ex'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 normal_folder='anormal'
