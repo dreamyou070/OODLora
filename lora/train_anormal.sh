b682c884f26580f4d2de4cd7b8c14593bd4a22a5
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-18 08:52:34 +0900

    Update train_anormal.sh

--- lora/train_anormal.sh
+++ lora/train_anormal.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-class_name="carrot"
+class_name="peach"
 data_source='train_ex'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 normal_folder='anormal'
