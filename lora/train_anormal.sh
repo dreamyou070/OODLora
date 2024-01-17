93d79b99e74e1fe139f6ed428442866d24683bd8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-17 15:05:47 +0900

    Update train_anormal.sh

--- lora/train_anormal.sh
+++ lora/train_anormal.sh
@@ -1,13 +1,13 @@
 #!bin/bash
 
-class_name="cable_gland"
+class_name="dowel"
 data_source='train_ex'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 normal_folder='anormal'
 save_folder="2_1_res_64_up_16_up"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
 network_weights="${output_dir}/models/epoch-000043.safetensors"
-port_number=54455
+port_number=54433
 start_epoch=0
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config --main_process_port $port_number train_anormal.py \
