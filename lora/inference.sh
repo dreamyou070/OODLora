319464a4abadc306916284042219d2ca38f71db2
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-17 11:01:12 +0900

    Update inference.sh

--- lora/inference.sh
+++ lora/inference.sh
@@ -10,10 +10,10 @@
 
 class_name="foam"
 start_epoch=0
-port_number=50542
+port_number=50543
 
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/2_2_res_64_up_16_up"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/2_3_res_64_up_16_up"
 network_weights="${output_dir}/models"
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port ${port_number} inference.py \
