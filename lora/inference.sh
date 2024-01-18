e7011e2d527636b7fd3efd385b674da30fa7df9b
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-18 20:23:08 +0900

    Update inference.sh

--- lora/inference.sh
+++ lora/inference.sh
@@ -8,12 +8,12 @@
 # cd ./Lora/OODLora/lora/
 # conda activate venv_lora
 
-class_name='foam'
+class_name='peach'
 start_epoch=0
-port_number=50123
+port_number=50133
 
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/2_1_res_64_up_16_up_good_10_anomal_80_cut_100"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/2_1_res_64_up_16_up_good_2_anormal_10_cut_30"
 network_weights="${output_dir}/models"
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port ${port_number} inference.py \
