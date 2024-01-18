526914080a27533a1d1d3c92610f5de0584fd2a1
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-19 00:39:01 +0900

    inference

--- lora/inference.sh
+++ lora/inference.sh
@@ -8,12 +8,13 @@
 # cd ./Lora/OODLora/lora/
 # conda activate venv_lora
 
-class_name='cable_gland'
+class_name='potato'
 start_epoch=0
 port_number=50101
 
 concept_image_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
-output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/2_1_res_64_up_16_up_good_3_anomal_5"
+class_name="potatocable_gland"
+output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/"
 network_weights="${output_dir}/models"
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port ${port_number} inference.py \
