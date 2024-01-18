0231b693cc31b33f05f041b09b07b72f4e9395a7
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-18 20:38:47 +0900

    Update train_anormal.sh

--- lora/train_anormal.sh
+++ lora/train_anormal.sh
@@ -1,17 +1,17 @@
 #!bin/bash
 
-class_name="carrot"
+class_name="bagel"
 data_source='train_ex'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 normal_folder='anormal'
-save_folder="2_3_res_16_up_1_good_8_anormal"
+save_folder="2_1_res_8"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
-start_epoch=7
-network_weights="${output_dir}/models/epoch-000007.safetensors"
-port_number=54777
+start_epoch=0
+#network_weights="${output_dir}/models/epoch-000007.safetensors"
+port_number=50000
 
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config --main_process_port $port_number train_anormal.py \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config --main_process_port $port_number train_anormal.py \
   --process_title parksooyeon \
   --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
@@ -26,6 +26,6 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --train_data_dir "$train_data_dir" \
   --start_epoch $start_epoch \
   --output_dir "$output_dir" \
-  --cross_map_res [16] --detail_64_up --trg_position "['up']" \
+  --cross_map_res [8] --detail_64_up --trg_position "['mid']" \
   --trg_part '["attn_2","attn_1","attn_0"]' --truncate_pad --truncate_length 3 --cls_training \
-  --normal_with_background --network_weights "$network_weights"
+  --normal_with_background
