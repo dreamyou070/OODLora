b1217e3063115433506a48564e569237e54b32bd
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-18 09:05:49 +0900

    Update train_anormal.sh

--- lora/train_anormal.sh
+++ lora/train_anormal.sh
@@ -1,14 +1,14 @@
 #!bin/bash
 
-class_name="peach"
+class_name="foam"
 data_source='train_ex'
 train_data_dir="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}/${data_source}/rgb"
 normal_folder='anormal'
 save_folder="2_1_res_64_up_16_up"
 output_dir="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
-network_weights="${output_dir}/models/epoch-000015.safetensors"
-port_number=54151
-start_epoch=29
+network_weights="${output_dir}/models/epoch-000022.safetensors"
+port_number=54122
+start_epoch=22
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config --main_process_port $port_number train_anormal.py \
   --process_title parksooyeon \
