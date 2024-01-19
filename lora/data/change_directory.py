b7f2f5fe95d5d5df64a305202401927d39c09198
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-19 23:10:52 +0900

    Update change_directory.py

--- lora/data/change_directory.py
+++ lora/data/change_directory.py
@@ -82,7 +82,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--base_folder', type=str, default=r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D')
+    parser.add_argument('--base_folder', type=str, default=r'../../../../MyData/anomaly_detection/MVTec')
     parser.add_argument('--trg_cat', type=str, default='bottle')
     args = parser.parse_args()
     main(args)
