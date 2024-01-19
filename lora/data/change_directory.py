99b9b214f1f13af304d72d6adecb085b11936592
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-19 23:09:02 +0900

    Update change_directory.py

--- lora/data/change_directory.py
+++ lora/data/change_directory.py
@@ -16,121 +16,73 @@ def main(args):
             cat_dir = os.path.join(base_folder, f'{cat}')
 
             train_dir = os.path.join(cat_dir, 'train')
-            test_dir = os.path.join(cat_dir, 'test')
-            validation_dir = os.path.join(cat_dir, 'validation')
+            test_rgb_dir = os.path.join(cat_dir, 'test')
+            test_gt_dir = os.path.join(cat_dir, 'ground_truth')
+
 
             train_ex_dir = os.path.join(cat_dir, 'train_ex')
-
-            train_ex_rgb_dir = os.path.join(train_ex_dir, 'mask')
+            train_ex_rgb_dir = os.path.join(train_ex_dir, 'rgb')
             train_ex_gt_dir = os.path.join(train_ex_dir, 'gt')
             os.makedirs(train_ex_rgb_dir, exist_ok=True)
             os.makedirs(train_ex_gt_dir, exist_ok=True)
 
             test_ex_dir = os.path.join(cat_dir, 'test_ex')
 
-            test_ex_rgb_dir = os.path.join(test_ex_dir, 'mask')
+            test_ex_rgb_dir = os.path.join(test_ex_dir, 'rgb')
             test_ex_gt_dir = os.path.join(test_ex_dir, 'gt')
             os.makedirs(test_ex_rgb_dir, exist_ok=True)
             os.makedirs(test_ex_gt_dir, exist_ok=True)
 
-            # -------------------------------------------------------------------------------------------------------
-            # (1) train
-            good_train_dir = os.path.join(train_dir, 'good/mask')
-            sam_train_dir = os.path.join(train_dir, f'good/gt')
+            categoriess = os.listdir(test_rgb_dir)
+            for categor in categoriess:
 
-            train_ex_good_rgb_dir = os.path.join(train_ex_dir, f'mask/10_good')
-            train_ex_good_gt_dir = os.path.join(train_ex_dir, f'gt/10_good')
-            os.makedirs(train_ex_good_rgb_dir, exist_ok=True)
-            os.makedirs(train_ex_good_gt_dir, exist_ok=True)
+                org_test_rgb_dir = os.path.join(test_rgb_dir, categor)
+                org_test_gt_dir =  os.path.join(test_gt_dir, categor)
 
-            images = os.listdir(good_train_dir)
+                new_test_rgb_dir = os.path.join(test_ex_rgb_dir, categor)
+                new_test_gt_dir = os.path.join(test_ex_gt_dir, categor)
+                os.makedirs(new_test_rgb_dir, exist_ok=True)
+                os.makedirs(new_test_gt_dir, exist_ok=True)
 
-            for image in images:
-                org_rgb_image_path = os.path.join(good_train_dir, image)
-                org_gt_image_path = os.path.join(sam_train_dir, image)
-                new_rgb_image_path = os.path.join(train_ex_good_rgb_dir, f'train_{image}')
-                new_gt_image_path = os.path.join(train_ex_good_gt_dir, f'train_{image}')
-                Image.open(org_rgb_image_path).resize((512,512)).save(new_rgb_image_path)
-                gt = Image.open(org_gt_image_path).resize((512,512)).convert('L')
-                gt.save(new_gt_image_path)
+                new_train_rgb_dir = os.path.join(train_ex_rgb_dir, categor)
+                new_train_gt_dir = os.path.join(train_ex_gt_dir, categor)
+                os.makedirs(new_train_rgb_dir, exist_ok=True)
+                os.makedirs(new_train_gt_dir, exist_ok=True)
 
-            # -------------------------------------------------------------------------------------------------------
-            # (2) validation
-            good_validation_dir = os.path.join(validation_dir, 'good/mask')
-            sam_validation_dir = os.path.join(validation_dir, f'good/gt')
-            val_images = os.listdir(good_validation_dir)
-            for image in val_images:
-                org_rgb_image_path = os.path.join(good_validation_dir, image)
-                org_gt_image_path = os.path.join(sam_validation_dir, image)
-                new_rgb_image_path = os.path.join(train_ex_good_rgb_dir, f'val_{image}')
-                new_gt_image_path = os.path.join(train_ex_good_gt_dir, f'val_{image}')
-                Image.open(org_rgb_image_path).resize((512,512)).save(new_rgb_image_path)
-                gt = Image.open(org_gt_image_path).resize((512,512)).convert('L')
-                gt.save(new_gt_image_path)
+                if 'good' not in categor:
+                    test_images = os.listdir(org_test_rgb_dir)
+                    total_num = len(test_images)
+                    test_num = int(total_num * 0.2)
+                    for i, t_img in enumerate(test_images) :
+                        org_rgb_dir = os.path.join(org_test_rgb_dir, t_img)
+                        org_gt_dir = os.path.join(org_test_gt_dir, t_img)
 
-            # -------------------------------------------------------------------------------------------------------
-            categories = os.listdir(test_dir)
-            for category in categories:
-                if 'good' not in category:
-                    category_dir = os.path.join(test_dir, category)
-                    train_cat_rgb_dir = os.path.join(train_ex_rgb_dir, f'80_{category}')
-                    train_cat_gt_dir = os.path.join(train_ex_gt_dir, f'80_{category}')
-                    os.makedirs(train_cat_rgb_dir, exist_ok=True)
-                    os.makedirs(train_cat_gt_dir, exist_ok=True)
-                    test_cat_rgb_dir = os.path.join(test_ex_rgb_dir, category)
-                    test_cat_gt_dir = os.path.join(test_ex_gt_dir, category)
-                    os.makedirs(test_cat_rgb_dir, exist_ok=True)
-                    os.makedirs(test_cat_gt_dir, exist_ok=True)
-
-                    cat_rgb_dir = os.path.join(category_dir, 'mask')
-                    cat_gt_dir = os.path.join(category_dir, 'gt')
-                    cat_images = os.listdir(cat_rgb_dir)
-                    num_images = len(cat_images)
-
-                    train_index = random.sample(range(num_images), int(num_images * trg_ratio))
-                    for i, img in enumerate(cat_images) :
-                        org_rgb_image_path = os.path.join(cat_rgb_dir, img)
-                        org_gt_image_path = os.path.join(cat_gt_dir, img)
-                        org_pil_rgb_image = Image.open(org_rgb_image_path).resize((512,512))
-                        org_pil_gt_image = Image.open(org_gt_image_path).convert('L').resize((512,512))
-
-                        if i in train_index:
-                            new_rgb_image_path = os.path.join(train_cat_rgb_dir, f'{category}_{img}')
-                            new_gt_image_path = os.path.join(train_cat_gt_dir, f'{category}_{img}')
-
+                        if i < test_num :
+                            new_rgb_dir = os.path.join(new_test_rgb_dir, t_img)
+                            new_gt_dir = os.path.join(new_test_gt_dir, t_img)
                         else :
-                            new_rgb_image_path = os.path.join(test_cat_rgb_dir, f'{category}_{img}')
-                            new_gt_image_path = os.path.join(test_cat_gt_dir, f'{category}_{img}')
-                        org_pil_rgb_image.save(new_rgb_image_path)
-                        org_pil_gt_image.save(new_gt_image_path)
+                            new_rgb_dir = os.path.join(new_train_rgb_dir, t_img)
+                            new_gt_dir = os.path.join(new_train_gt_dir, t_img)
+                        Image.open(org_rgb_dir).resize((512,512)).save(new_rgb_dir)
+                        gt = Image.open(org_gt_dir).resize((512,512)).convert('L')
+                        gt.save(new_gt_dir)
 
-                if 'good' in category :
+                else :
+                    for i, t_img in enumerate(test_images):
+                        org_rgb_dir = os.path.join(org_test_rgb_dir, t_img)
+                        org_gt_dir = os.path.join(org_test_gt_dir, t_img)
+                        new_rgb_dir = os.path.join(new_train_rgb_dir, t_img)
+                        new_gt_dir = os.path.join(new_train_gt_dir, t_img)
+                        Image.open(org_rgb_dir).resize((512, 512)).save(new_rgb_dir)
+                        gt = Image.open(org_gt_dir).resize((512, 512)).convert('L')
+                        gt.save(new_gt_dir)
 
-                    category_dir = os.path.join(test_dir, category)
 
-                    cat_rgb_dir = os.path.join(category_dir, 'mask')
-                    cat_gt_dir = os.path.join(category_dir, 'gt')
-                    cat_images = os.listdir(cat_rgb_dir)
-
-                    test_cat_rgb_dir = os.path.join(test_ex_rgb_dir, category)
-                    test_cat_gt_dir = os.path.join(test_ex_gt_dir, category)
-                    os.makedirs(test_cat_rgb_dir, exist_ok=True)
-                    os.makedirs(test_cat_gt_dir, exist_ok=True)
-
-                    for cat_image in cat_images :
-                        org_rgb_image_path = os.path.join(cat_rgb_dir, cat_image)
-                        org_gt_image_path = os.path.join(cat_gt_dir, cat_image)
-                        new_rgb_image_path = os.path.join(test_cat_rgb_dir, cat_image)
-                        new_gt_image_path = os.path.join(test_cat_gt_dir, cat_image)
-
-                        Image.open(org_rgb_image_path).resize((512,512)).save(new_rgb_image_path)
-                        gt = Image.open(org_gt_image_path).resize((512,512)).convert('L')
-                        gt.save(new_gt_image_path)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--base_folder', type=str, default=r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD')
-    parser.add_argument('--trg_cat', type=str, default='peach')
+    parser.add_argument('--base_folder', type=str, default=r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D')
+    parser.add_argument('--trg_cat', type=str, default='bottle')
     args = parser.parse_args()
     main(args)
