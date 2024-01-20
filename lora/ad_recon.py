2b896abd82d1a027c1c7e6df3fe66105be89eb91
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-20 15:23:16 +0900

    Update ad_recon.py

--- lora/ad_recon.py
+++ lora/ad_recon.py
@@ -388,17 +388,17 @@ def main(args) :
                                                 img_dir = os.path.join(trg_img_output_dir,f'{name}_recon_{t}{ext}')
                                                 image.save(img_dir)
                                         else :
-                                            if t == 999 :
-                                                image = pipeline.latents_to_image(latents)[0]
-                                                img_dir = os.path.join(trg_img_output_dir, f'{name}_test_final_recon_{t}{ext}')
-                                                image.save(img_dir)
+                                            image = pipeline.latents_to_image(latents)[0]
+                                            img_dir = os.path.join(trg_img_output_dir, f'{name}_test_final_recon_{t}{ext}')
+                                            image.save(img_dir)
                                         controller.reset()
                                     reconstruction = image = pipeline.latents_to_image(latents)[0]
                         # ----------------------------[4] generate anomaly maps ------------------------------ #
                         input = org_input_image
                         input_np = np.array(input)
                         reconstruction_np = np.array(reconstruction)
-                        anomaly_maps = np.where(input_np != reconstruction_np, 1, 0)
+                        np_diff = np.abs(input_np - reconstruction_np)/255
+                        anomaly_maps = np.where(np_diff > 0.4, 1, 0)
                         classification_result = np.sum(anomaly_maps)
                         if classification_result > 0:
                             label = 1
