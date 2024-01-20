285e8077d574a6577c44d201389964da96214349
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-20 22:26:29 +0900

    Update ad_recon.sh

--- script/ad_recon.sh
+++ script/ad_recon.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-class_name="bagel"
-folder_name="2_2_res_64_up_attn2_t_2"
-lora_folder="epoch-000011.safetensors"
+class_name="cable_gland"
+folder_name="2_2_res_64_up_attn2_good_2_anormal_8"
+lora_folder="epoch-000014.safetensors"
 network_weight_folder="./OODLora/result/MVTec3D-AD_experiment/${class_name}/lora_training/anormal/${folder_name}/models"
 img_folder="../../../MyData/anomaly_detection/MVTec3D-AD/${class_name}"
 
