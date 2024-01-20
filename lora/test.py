3771e3ee353f175566f5229747fdf1c28ca78847
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-21 02:06:25 +0900

    Update test.py

--- lora/test.py
+++ lora/test.py
@@ -1,6 +1,11 @@
 import os
 from PIL import Image
-base_file = r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD/cookie/test/crack/gt/000.png'
-h,w = Image.open(base_file).size
+import numpy as np
 
-print(h)
+base_folder = r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD/cookie/test/good/rgb'
+gt_folder = r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD/cookie/test/good/gt'
+images = os.listdir(base_folder)
+for image in images:
+    base = np.zeros((500,500))
+    mask = Image.fromarray(base.astype(np.uint8))
+    mask.save(os.path.join(gt_folder, image))
