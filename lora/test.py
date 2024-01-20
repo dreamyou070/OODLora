aabafa3e2d1b8f55f7be1e839ee0b247a5e42bad
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-21 02:04:15 +0900

    Update test.py

--- lora/test.py
+++ lora/test.py
@@ -1,5 +1,5 @@
 import os
-from PIl import Image
+from PIL import Image
 base_file = r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD/cookie/test/crack/gt/000.png'
 h,w = Image.open(base_file).size
 
