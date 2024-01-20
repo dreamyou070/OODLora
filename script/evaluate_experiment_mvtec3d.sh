0a67e9a7ba204463d8fbfeac8a6aaea76d9efa7a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-20 22:17:44 +0900

    Update evaluate_experiment_mvtec3d.sh

--- script/evaluate_experiment_mvtec3d.sh
+++ script/evaluate_experiment_mvtec3d.sh
@@ -3,8 +3,8 @@
 class_name="bagel"
 dataset_cat="MVTec3D-AD"
 dataset_dir="../../../MyData/anomaly_detection/${dataset_cat}"
-anomaly_maps_dir="../result/${dataset_cat}/${class_name}/lora_training/anormal/2_2_res_64_up_attn2_t_2/recon_infer/2024-01-20_22-07-56"
-output_dir="../result/${dataset_cat}/${class_name}/lora_training/anormal/2_2_res_64_up_attn2_t_2/recon_infer/2024-01-20_22-07-56/metrics"
+anomaly_maps_dir="../result/${dataset_cat}/${class_name}_experiment/lora_training/anormal/2_2_res_64_up_attn2_t_2/recon_infer/2024-01-20_22-07-56"
+output_dir="../result/${dataset_cat}/${class_name}_experiment/lora_training/anormal/2_2_res_64_up_attn2_t_2/recon_infer/2024-01-20_22-07-56/metrics"
 
 python ../lora/evaluation/evaluation_code_MVTec3D-AD/evaluate_experiment.py \
      --dataset_base_dir "${dataset_dir}" \
