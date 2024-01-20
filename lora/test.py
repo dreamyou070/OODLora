53ea35f6bd939b6540aec4226d14aff96e7fabfb
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-21 02:21:39 +0900

    Update test.py

--- lora/test.py
+++ lora/test.py
@@ -2,11 +2,11 @@ import os
 from PIL import Image
 import numpy as np
 
-base_img_dir = '/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD/cable_gland/test/cut/gt/000.png'
+base_img_dir = '/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD/dowel/test/bent/gt/000.png'
 h,w = Image.open(base_img_dir).size
 
-base_folder = r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD/cable_gland/test/good/rgb'
-gt_folder = r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD/cable_gland/test/good/gt'
+base_folder = r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD/dowel/test/good/rgb'
+gt_folder = r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD/dowel/test/good/gt'
 images = os.listdir(base_folder)
 for image in images:
     base = np.zeros((h,w))
