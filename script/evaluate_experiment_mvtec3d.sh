4218217f05039358e3b9e2020fd5e3293baac401
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-21 09:54:51 +0900

    Update evaluate_experiment_mvtec3d.sh

--- script/evaluate_experiment_mvtec3d.sh
+++ script/evaluate_experiment_mvtec3d.sh
@@ -5,7 +5,7 @@ dataset_cat="MVTec3D-AD"
 dataset_dir="../../../MyData/anomaly_detection/${dataset_cat}"
 #anomaly_maps_dir="../result/${dataset_cat}_experiment/${class_name}/lora_training/anormal/2_2_res_64_up_attn2_t_2/recon_infer/2024-01-20_22-07-56"
 #output_dir="../result/${dataset_cat}_experiment/${class_name}/lora_training/anormal/2_2_res_64_up_attn2_t_2/recon_infer/2024-01-20_22-07-56/metrics"
-base_dir = "../result/${dataset_cat}_experiment/${class_name}/lora_training/anormal/2_2_res_64_up_attn2_t_2_data_38/reconstruction"
+base_dir="../result/${dataset_cat}_experiment/${class_name}/lora_training/anormal/2_2_res_64_up_attn2_t_2_data_38/reconstruction"
 anomaly_maps_dir="step_4_guidance_scale_8.5_start_from_origin_False_start_from_final_True_"
 output_dir="metrics"
 
