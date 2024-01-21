bd6cd92766689ae53cf38614f4cdb62e1b781499
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-21 22:01:40 +0900

    Update get_all_metrics.sh

--- script/get_all_metrics.sh
+++ script/get_all_metrics.sh
@@ -2,7 +2,7 @@
 
 class_name="tire"
 condition_folder='step_4_guidance_scale_8.5_start_from_origin_False_start_from_final_True_'
-second_folder_name='sub_folder="2_2_res_64_up_attn2_t_2_data_12'
+second_folder_name="2_2_res_64_up_attn2_t_2_data_12"
 
 python ../lora/evaluation/get_all_metrics.py \
   --class_name ${class_name} \
