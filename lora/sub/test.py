88e03a39f3f4215c90cc22e24dfb87441af17fb9
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-21 15:13:17 +0900

    Update test.py

--- lora/sub/test.py
+++ lora/sub/test.py
@@ -2,7 +2,7 @@ import os
 from PIL import Image
 import numpy as np
 
-class_name = 'rope'
+class_name = 'tire'
 base_img_dir = f'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD/{class_name}/test/cut/gt/000.png'
 w,h = Image.open(base_img_dir).size
 
