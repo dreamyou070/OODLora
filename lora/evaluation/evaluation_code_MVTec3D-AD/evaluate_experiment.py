247bd77938c780ca3cc3a0b9f5d8a8050a09faa3
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-21 10:46:28 +0900

    Update evaluate_experiment.py

--- lora/evaluation/evaluation_code_MVTec3D-AD/evaluate_experiment.py
+++ lora/evaluation/evaluation_code_MVTec3D-AD/evaluate_experiment.py
@@ -187,7 +187,7 @@ def main():
 
     lora_folders = os.listdir(args.base_dir)
     for lora_folder in lora_folders:
-        lora_dir = os.path.join(args.base_folder, lora_folder)
+        lora_dir = os.path.join(args.base_dir, lora_folder)
         args.anomaly_maps_dir = os.path.join(lora_dir, base_anomaly_maps_dir)
         args.output_dir = os.path.join(args.anomaly_maps_dir, base_save_dir)
 
