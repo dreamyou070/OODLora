9aac95b12b6b7294c897f9e7f877dfe88e2b681d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-21 12:18:53 +0900

    Create get_all_metrics.sh

---
+++ script/get_all_metrics.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+class_name="cable_gland"
+condition_folder='step_4_guidance_scale_8.5_start_from_origin_False_start_from_final_True_'
+second_folder_name='2_2_res_64_up_attn2_t_2_20240121'
+
+port_number=50002
+
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
+  --main_process_port ${port_number} ../lora/evaluation/get_all_metrics.py \
+  --class_name ${class_name} \
+  --condition_folder ${condition_folder} \
+  --second_folder_name ${second_folder_name}
