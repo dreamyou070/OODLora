98c660e47b9cc8c3cc2389bbc48a7dfa9fe984f6
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-22 20:31:13 +0900

    Update rec_infer.sh

--- script/rec_infer.sh
+++ script/rec_infer.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 class_name="bagel"
-folder_name="res_64_up_attn2_t_2_attn210_sub_module_only_activating"
+folder_name="res_64_up_attn2_t_2_attn2"
 data_folder="MVTec3D-AD"
 normality_folder='anormal'
 network_weight_folder="../result/${data_folder}_experiment/${class_name}/lora_training/${normality_folder}/${folder_name}/models"
