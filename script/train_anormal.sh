5a44665b3e7fce4db7b078f4d748e3f04487e11f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-22 14:52:35 +0900

    Update train_anormal.sh

--- script/train_anormal.sh
+++ script/train_anormal.sh
@@ -5,7 +5,7 @@ data_source='train_ex'
 data_folder='MVTec3D-AD'
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
 normal_folder='anormal'
-save_folder="res_64_up_attn2_t_2_attn2"
+save_folder="res_64_up_attn2_t_2_attn210"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
 
 start_epoch=0
@@ -29,4 +29,4 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --start_epoch $start_epoch \
   --output_dir "$output_dir" --truncate_pad --truncate_length 2  \
   --cross_map_res [64] --detail_64_up --trg_position "['up']" --normal_with_background \
-  --trg_part '["attn_2"]' --cls_training # --network_weights "$network_weights"
+  --trg_part '["attn_2","attn_1","attn_0"]' --cls_training --all_same_learning # --network_weights "$network_weights"
