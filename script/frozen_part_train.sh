7993c24d9e00931e5931c8c2ef318cd9f7ba4d14
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-22 16:45:10 +0900

    Update frozen_part_train.sh

--- script/frozen_part_train.sh
+++ script/frozen_part_train.sh
@@ -13,7 +13,7 @@ start_epoch=0
 port_number=50001
 
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
   --main_process_port $port_number ../lora/frozen_part_train.py \
   --process_title parksooyeon \
   --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
