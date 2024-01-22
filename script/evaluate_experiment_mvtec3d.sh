eeefda68ff7b11bdf5fd75efe8135084129855c7
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-22 20:29:18 +0900

    Update evaluate_experiment_mvtec3d.sh

--- script/evaluate_experiment_mvtec3d.sh
+++ script/evaluate_experiment_mvtec3d.sh
@@ -3,7 +3,7 @@
 class_name="bagel"
 dataset_cat="MVTec3D-AD"
 dataset_dir="../../../MyData/anomaly_detection/${dataset_cat}"
-sub_folder="res_64_up_attn2_t_2_attn2_attn1_attn0"
+sub_folder="res_64_up_attn2_t_2_attn2"
 base_dir="../result/${dataset_cat}_experiment/${class_name}/lora_training/anormal/${sub_folder}/reconstruction"
 anomaly_maps_dir="step_4_guidance_scale_8.5_start_from_origin_False_start_from_final_True_"
 output_dir="metrics"
