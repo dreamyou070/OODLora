f46a997db7efd7c3863de50ea2efa1a0179fba0d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-22 15:35:36 +0900

    Update train_normal.sh

--- script/train_normal.sh
+++ script/train_normal.sh
@@ -5,7 +5,7 @@ data_source='train_normal'
 data_folder='MVTec3D-AD'
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
 normal_folder='normal'
-save_folder="res_64_up_attn2_t_2_attn2"
+save_folder="res_64_up_attn2_t_2_attn2_normal_only"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
 
 start_epoch=0
