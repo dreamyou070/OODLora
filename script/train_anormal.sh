130f3fff252bc8e6e2b77d832377379c36bd5592
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-22 14:50:24 +0900

    Update train_anormal.sh

--- script/train_anormal.sh
+++ script/train_anormal.sh
@@ -5,14 +5,14 @@ data_source='train_ex'
 data_folder='MVTec3D-AD'
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
 normal_folder='anormal'
-save_folder="res_64_up_attn2_t_2_attn2_attn1_attn0_submodule_only_normal"
+save_folder="res_64_up_attn2_t_2_attn2"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
-network_weights="${output_dir}/models/epoch-000013.safetensors"
+
 start_epoch=0
-port_number=52222
+port_number=50001
 
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
   --main_process_port $port_number ../lora/train_anormal.py \
   --process_title parksooyeon \
   --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
@@ -29,4 +29,4 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --start_epoch $start_epoch \
   --output_dir "$output_dir" --truncate_pad --truncate_length 2  \
   --cross_map_res [64] --detail_64_up --trg_position "['up']" --normal_with_background \
-  --trg_part '["attn_2","attn_1","attn_0"]' --cls_training # --network_weights "$network_weights"
+  --trg_part '["attn_2"]' --cls_training # --network_weights "$network_weights"
