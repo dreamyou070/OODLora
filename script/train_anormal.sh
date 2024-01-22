cfb39965daa03efe6e881b7608b9dba8c67753b7
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-22 13:39:52 +0900

    Update train_anormal.sh

--- script/train_anormal.sh
+++ script/train_anormal.sh
@@ -9,7 +9,7 @@ save_folder="res_64_up_attn2_t_2_attn2_attn1_attn0"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
 network_weights="${output_dir}/models/epoch-000013.safetensors"
 start_epoch=0
-port_number=56578
+port_number=56668
 
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
