36e333f7ba5935371ab5ffdc75f741b19a18d036
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-22 17:33:45 +0900

    Update rec_infer.sh

--- script/rec_infer.sh
+++ script/rec_infer.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 class_name="bagel"
-folder_name="res_64_up_attn2_t_2_attn2_normal"
+folder_name="res_64_up_attn2_t_2_attn2_from_normal_frozen_only_small_update"
 data_folder="MVTec3D-AD"
 network_weight_folder="../result/${data_folder}_experiment/${class_name}/lora_training/normal/${folder_name}/models"
 img_folder="../../../MyData/anomaly_detection/${data_folder}/${class_name}"
@@ -31,5 +31,4 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --start_from_final \
   --only_zero_save \
   --use_pixel_mask \
-  --class_name ${class_name} \
-  --only_normal_infer
+  --class_name ${class_name}
