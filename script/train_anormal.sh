171e5cc561b54f8a11cd1bb388c1a541572ee110
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-22 14:56:28 +0900

    Update train_anormal.sh

--- script/train_anormal.sh
+++ script/train_anormal.sh
@@ -5,11 +5,11 @@ data_source='train_ex'
 data_folder='MVTec3D-AD'
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
 normal_folder='anormal'
-save_folder="res_64_up_attn2_t_2_attn210_sub_module_only_activating"
+save_folder="res_64_up_attn2_t_2_attn210"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
 
 start_epoch=0
-port_number=50003
+port_number=50002
 
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
@@ -29,4 +29,4 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --start_epoch $start_epoch \
   --output_dir "$output_dir" --truncate_pad --truncate_length 2  \
   --cross_map_res [64] --detail_64_up --trg_position "['up']" --normal_with_background \
-  --trg_part '["attn_2","attn_1","attn_0"]' --cls_training # --all_same_learning  --network_weights "$network_weights"
+  --trg_part '["attn_2","attn_1","attn_0"]' --cls_training --all_same_learning #  --network_weights "$network_weights"
