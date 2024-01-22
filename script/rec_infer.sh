c7139bf5ec8f5b0c151ffdf5e655e3e3318668a3
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-22 14:16:41 +0900

    Update rec_infer.sh

--- script/rec_infer.sh
+++ script/rec_infer.sh
@@ -2,7 +2,7 @@
 
 class_name="bagel"
 folder_name="res_64_up_attn2_t_2"
-data_folder="MVTec3D-AD_org"
+data_folder="MVTec3D-AD-org"
 network_weight_folder="../result/${data_folder}_experiment/${class_name}/lora_training/anormal/${folder_name}/models"
 img_folder="../../../MyData/anomaly_detection/${data_folder}/${class_name}"
 
