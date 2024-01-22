f0bc382093395d37a3c91188a6443c632410a3b7
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-22 10:51:36 +0900

    Update change_repeat.py

--- lora/data/change_repeat.py
+++ lora/data/change_repeat.py
@@ -35,12 +35,14 @@ def main(args):
                 os.rename(org_rgb_folder, new_rgb_folder)
                 os.rename(org_gt_folder, new_gt_folder)
 
+# 20 + 120 = 140
+# 30 + 120 = 150
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--base_folder', type=str,
                         default=r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD-org')
-    parser.add_argument('--trg_cat', type=str, default='bagel')
-    parser.add_argument('--new_ok_repeat', type=int, default=20)
+    parser.add_argument('--trg_cat', type=str, default='cable_gland')
+    parser.add_argument('--new_ok_repeat', type=int, default=30)
     parser.add_argument('--new_nok_repeat', type=int, default=120)
     args = parser.parse_args()
     main(args)
