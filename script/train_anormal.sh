4dc6cec286e0f984c4ab4a99b750308bdd2ccbdf
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-24 08:24:36 +0900

    Update train_anormal.sh

--- script/train_anormal.sh
+++ script/train_anormal.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-class_name="carrot"
+class_name="dowel"
 data_source='train_ex'
 data_folder='MVTec3D-AD'
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
@@ -9,7 +9,7 @@ save_folder="res_64_up_attn2_t_2_attn2"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
 network_weights="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}/models/epoch-000004.safetensors"
 start_epoch=5
-port_number=52134
+port_number=52144
 
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
