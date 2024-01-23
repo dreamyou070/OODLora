347dfa67fe9374398bd86bf1f9cd72a3730c3d30
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-23 11:41:33 +0900

    Update evaluate_experiment_mvtec.sh

--- script/evaluate_experiment_mvtec.sh
+++ script/evaluate_experiment_mvtec.sh
@@ -3,7 +3,7 @@
 class_name="bagle"
 dataset_cat="MVTec3D-AD"
 dataset_dir="../../../MyData/anomaly_detection/${dataset_cat}"
-sub_folder="res_64_up_attn2_t_2_attn2_attn1_attn0"
+sub_folder="res_64_up_attn12_from_normal_self_cross_attn"
 base_dir="../result/${dataset_cat}_experiment/${class_name}/lora_training/anormal/${sub_folder}/reconstruction"
 anomaly_maps_dir="step_4_guidance_scale_8.5_start_from_origin_False_start_from_final_True_"
 output_dir="metrics"
@@ -15,4 +15,3 @@ python ../lora/evaluation/evaluation_code_MVTec/evaluate_experiment_2.py \
      --output_dir "${output_dir}" \
      --evaluated_objects "${class_name}" \
      --pro_integration_limit 0.3
-
