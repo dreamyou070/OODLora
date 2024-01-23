5f56d1b99ce22692c48755ed12fb4870ead8ef5f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-23 13:43:55 +0900

    Update rec_infer.sh

--- script/rec_infer.sh
+++ script/rec_infer.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 class_name="bagel"
-folder_name="res_64_up_attn012_t_2_only_normal"
+folder_name="2_2_res_64_up_attn2_t_2_20240121"
 data_folder="MVTec3D-AD"
 normality_folder='normal'
 network_weight_folder="../result/${data_folder}_experiment/${class_name}/lora_training/${normality_folder}/${folder_name}/models"
@@ -20,8 +20,8 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --network_dim 64 \
   --network_alpha 4 \
   --network_weights ${network_weight_folder}  \
-  --cross_map_res [64] \
-  --trg_position "['up']" \
+  --cross_map_res [64,32,16,8] \
+  --trg_position "['up','down','mid]" \
   --trg_part "['attn_2','attn_1','attn_0']" \
   --num_ddim_steps 4 \
   --inner_iter 10 \
