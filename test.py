fe7511a230039c9856fe0dff8f63181a1689d315
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-23 15:35:23 +0900

    trim to clamp

--- test.py
+++ test.py
@@ -3,4 +3,5 @@ import torch
 object_position = torch.tensor([0.9, 1.2, 0.3])
 
 
-object_position = torch.triu(object_position, min = 0, max = 1)
+object_position = torch.clamp(object_position, min = 0, max = 1)
+print(object_position)
