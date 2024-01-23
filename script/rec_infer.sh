e14b2d54bdd0bb15433a1079e894f654467baeca
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-23 21:47:27 +0900

    Update rec_infer.sh

--- script/rec_infer.sh
+++ script/rec_infer.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-class_name="cable_gland"
-folder_name="1_res_64_up_attn2_t_2"
+class_name="tire"
+folder_name="1_res_64_up_16_up_good_6_anomal_80"
 data_folder="MVTec3D-AD"
 normality_folder='anormal'
 network_weight_folder="../result/${data_folder}_experiment/${class_name}/lora_training/${normality_folder}/${folder_name}/models"
 img_folder="../../../MyData/anomaly_detection/${data_folder}/${class_name}"
 
-port_number=56993
+port_number=56101
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
   --main_process_port ${port_number} ../lora/rec_infer.py \
