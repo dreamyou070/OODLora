3bfc259d092e83f39d455f8b253ce86707698672
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-23 22:50:11 +0900

    rope training

--- script/train_anormal.sh
+++ script/train_anormal.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-class_name="bagel"
+class_name="tire"
 data_source='train_ex'
 data_folder='MVTec3D-AD'
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
@@ -9,10 +9,10 @@ save_folder="res_64_up_attn2_t_2_attn2"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
 
 start_epoch=0
-port_number=50001
+port_number=50010
 
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
   --main_process_port $port_number ../lora/train_anormal.py \
   --process_title parksooyeon \
   --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
