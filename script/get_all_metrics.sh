24c7aee7060421b18c5c7588eba3e80d3195486b
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-23 13:37:35 +0900

    Update get_all_metrics.sh

--- script/get_all_metrics.sh
+++ script/get_all_metrics.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-class_name="tire"
+class_name="bagel"
 condition_folder='step_4_guidance_scale_8.5_start_from_origin_False_start_from_final_True_'
-second_folder_name="2_2_res_64_up_attn2_t_2_data_12"
+second_folder_name="res_64_up_attn12_from_normal_self_cross_attn"
 
 python ../lora/evaluation/get_all_metrics.py \
   --class_name ${class_name} \
