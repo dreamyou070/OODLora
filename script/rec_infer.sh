efd96e444ebeeefe3f58f8f2ee6e961f4a9581ec
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-24 14:06:51 +0900

    Update rec_infer.sh

--- script/rec_infer.sh
+++ script/rec_infer.sh
@@ -7,7 +7,7 @@ normality_folder='anormal'
 network_weight_folder="../result/${data_folder}_experiment/${class_name}/lora_training/${normality_folder}/${folder_name}/models"
 img_folder="../../../MyData/anomaly_detection/${data_folder}/${class_name}"
 
-port_number=50500
+port_number=50200
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
   --main_process_port ${port_number} ../lora/rec_infer.py \
@@ -32,6 +32,6 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --only_zero_save \
   --use_pixel_mask \
   --class_name ${class_name} \
-  --free_time 500 \
+  --free_time 200 \
   --anormal_thred 0.5 \
   --latent_diff_thred 0.5
