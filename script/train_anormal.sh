cd69dd411dc1612f9e894b2c990793f614d39583
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-24 10:42:25 +0900

    potato retraing

--- script/train_anormal.sh
+++ script/train_anormal.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
-class_name="tire"
+class_name="potato"
 data_source='train_ex'
 data_folder='MVTec3D-AD'
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
 normal_folder='anormal'
 save_folder="res_64_up_attn2_t_2_attn2_data_60"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
-network_weights="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}/models/epoch-000005.safetensors"
+network_weights="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/res_64_up_attn2_t_2_attn2/models/epoch-000001.safetensors"
 start_epoch=0
-port_number=55510
+port_number=55588
 
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
   --main_process_port $port_number ../lora/train_anormal.py \
   --process_title parksooyeon \
   --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
@@ -29,4 +29,4 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_c
   --start_epoch $start_epoch \
   --output_dir "$output_dir" --truncate_pad --truncate_length 2  \
   --cross_map_res [64] --detail_64_up --trg_position "['up']" --normal_with_background \
-  --trg_part '["attn_2"]' --cls_training --all_same_learning # --network_weights "$network_weights"
+  --trg_part '["attn_2"]' --cls_training --all_same_learning --network_weights "$network_weights"
