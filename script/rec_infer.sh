47f863bad4b878bdedf4f6a7a7309bfcfd170401
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-24 10:20:30 +0900

    Update rec_infer.sh

--- script/rec_infer.sh
+++ script/rec_infer.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-class_name="cable_gland"
-folder_name="res_64_up_attn2_t_2_attn2"
+class_name="tire"
+folder_name="res_64_up_attn2_t_2_attn2_data_60"
 data_folder="MVTec3D-AD"
 normality_folder='anormal'
 network_weight_folder="../result/${data_folder}_experiment/${class_name}/lora_training/${normality_folder}/${folder_name}/models"
 img_folder="../../../MyData/anomaly_detection/${data_folder}/${class_name}"
 
-port_number=50011
+port_number=50020
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
   --main_process_port ${port_number} ../lora/rec_infer.py \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --sample_sampler ddim \
