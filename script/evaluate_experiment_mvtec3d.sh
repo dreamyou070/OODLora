a524ebd7630d3d26d00a0aa637af47dc422d24a8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-25 13:50:50 +0900

    Update evaluate_experiment_mvtec3d.sh

--- script/evaluate_experiment_mvtec3d.sh
+++ script/evaluate_experiment_mvtec3d.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-class_name="foam"
+class_name="potato"
 dataset_cat="MVTec3D-AD"
 dataset_dir="../../../MyData/anomaly_detection/${dataset_cat}"
 sub_folder="res_64_up_attn2_t_2_attn2"
 base_dir="../result/${dataset_cat}_experiment/${class_name}/lora_training/anormal/${sub_folder}/reconstruction"
-anomaly_maps_dir="with_object_detect_epoch_4_step_4_guidance_8.5_start_from_final_True_"
+anomaly_maps_dir="anormal_thred_0.5_guidance_scale_8.5_num_ddim_steps_50"
+
 output_dir="metrics"
 
 python ../lora/evaluation/evaluation_code_MVTec3D-AD/evaluate_experiment.py \
