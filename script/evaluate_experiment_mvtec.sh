270e37b440a92613ff33b3a6cff7a95143873692
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-25 13:50:03 +0900

    Update evaluate_experiment_mvtec.sh

--- script/evaluate_experiment_mvtec.sh
+++ script/evaluate_experiment_mvtec.sh
@@ -6,6 +6,7 @@ dataset_dir="../../../MyData/anomaly_detection/${dataset_cat}"
 sub_folder="res_64_up_attn2_t_2_attn2"
 base_dir="../result/${dataset_cat}_experiment/${class_name}/lora_training/anormal/${sub_folder}/reconstruction"
 anomaly_maps_dir="anormal_thred_0.5_guidance_scale_8.5_num_ddim_steps_50"
+
 output_dir="metrics"
 
 python ../lora/evaluation/evaluation_code_MVTec/evaluate_experiment_2.py \
