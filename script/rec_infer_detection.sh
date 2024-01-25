d2fdf5ecb6434832c702809a71cf60fb68d1a2eb
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-25 12:07:59 +0900

    Update rec_infer_detection.sh

--- script/rec_infer_detection.sh
+++ script/rec_infer_detection.sh
@@ -5,7 +5,7 @@ folder_name="res_64_up_attn2_t_2_attn2"
 data_folder="MVTec3D-AD"
 normality_folder='anormal'
 network_weight_folder="../result/${data_folder}_experiment/${class_name}/lora_training/${normality_folder}/${folder_name}/models"
-detection_network_weights="../result/${data_folder}_experiment/${class_name}/lora_training/normal/res_64_up_attn012_t_2_only_normal/models/epoch-00004.safetensors"
+detection_network_weights="../result/${data_folder}_experiment/${class_name}/lora_training/normal/res_64_up_attn012_t_2_only_normal/models/epoch-000004.safetensors"
 img_folder="../../../MyData/anomaly_detection/${data_folder}/${class_name}"
 
 port_number=50118
