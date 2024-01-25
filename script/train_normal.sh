f6c2f6ce69b0f6bb26a698921987bfdc8ccb6298
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-25 11:20:59 +0900

    Update train_normal.sh

--- script/train_normal.sh
+++ script/train_normal.sh
@@ -9,9 +9,9 @@ save_folder="res_64_up_attn012_t_2_only_normal"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
 
 start_epoch=0
-port_number=50039
+port_number=50019
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config \
   --main_process_port $port_number ../lora/train_normal.py \
   --process_title parksooyeon \
   --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
