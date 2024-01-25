f5e38ca49b8f87d9bd59d040dd42afe4030d0616
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-25 20:20:48 +0900

    Update train_anormal.sh

--- script/train_anormal.sh
+++ script/train_anormal.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-class_name="carrot"
+class_name="cookie"
 data_source='train_ex'
 data_folder='MVTec3D-AD'
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
