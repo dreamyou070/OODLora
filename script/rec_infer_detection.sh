8fdc8ba4c60c4da4aca260dc7e50615d9fb506bc
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-25 14:25:53 +0900

    Update rec_infer_detection.sh

--- script/rec_infer_detection.sh
+++ script/rec_infer_detection.sh
@@ -8,7 +8,7 @@ network_weight_folder="../result/${data_folder}_experiment/${class_name}/lora_tr
 detection_network_weights="../result/${data_folder}_experiment/${class_name}/lora_training/normal/res_64_up_attn012_t_2_only_normal/models/epoch-000004.safetensors"
 img_folder="../../../MyData/anomaly_detection/${data_folder}/${class_name}"
 
-port_number=50755
+port_number=50711
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
   --main_process_port ${port_number} ../lora/rec_infer_detection.py \
