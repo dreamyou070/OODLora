449458bd16254f611240e62be60d66b44c5ed863
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-25 12:09:39 +0900

    Update common_utils.py

--- lora/utils/common_utils.py
+++ lora/utils/common_utils.py
@@ -9,9 +9,9 @@ def get_lora_epoch(model_dir) :
         model_epoch = 10000
     return model_epoch
 
-def save_latent(latent, base_dir, save_name, h, w):
+def save_latent(latent, save_dir, h, w):
     latent_np = latent.squeeze().detach().cpu().numpy().astype(np.uint8)
     latent_np = latent_np * 255
     pil_img = Image.fromarray(latent_np).resize((h, w))
-    pil_img.save(os.path.join(base_dir, save_name))
+    pil_img.save(save_dir)
     return pil_img
