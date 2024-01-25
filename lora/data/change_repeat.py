2edc2a9c46b9f93e4a7487fca888e05499f50292
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-25 17:32:09 +0900

    Update change_repeat.py

--- lora/data/change_repeat.py
+++ lora/data/change_repeat.py
@@ -48,8 +48,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--base_folder', type=str,
                         default=r'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD')
-    parser.add_argument('--trg_cat', type=str, default='cookie')
-    parser.add_argument('--new_ok_repeat', type=int, default=40)
+    parser.add_argument('--trg_cat', type=str, default='potato')
+    parser.add_argument('--new_ok_repeat', type=int, default=20)
     parser.add_argument('--new_nok_repeat', type=int, default=40)
     args = parser.parse_args()
     main(args)
