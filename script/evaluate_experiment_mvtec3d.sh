d1b89ee513115921d2599cac7f727c869b064ec3
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-26 09:51:09 +0900

    Update evaluate_experiment_mvtec3d.sh

--- script/evaluate_experiment_mvtec3d.sh
+++ script/evaluate_experiment_mvtec3d.sh
@@ -5,7 +5,7 @@ dataset_cat="MVTec3D-AD"
 dataset_dir="../../../MyData/anomaly_detection/${dataset_cat}"
 sub_folder="res_64_up_attn2_t_2_attn2"
 base_dir="../result/${dataset_cat}_experiment/${class_name}/lora_training/anormal/${sub_folder}/reconstruction"
-anomaly_maps_dir="detec_epoch_3_anomal_thredhold_0.5_step_4_guidance_15.0"
+anomaly_maps_dir="detec_epoch_4_anomal_thredhold_0.5_step_4_guidance_15.0"
 
 output_dir="metrics"
 
