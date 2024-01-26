12b8211b7efda1977e4dd960fa770c8b01d3ba16
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-26 17:34:24 +0900

    new reconstruction

--- script/rec_infer_detection.sh
+++ script/rec_infer_detection.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
-class_name="rope"
+class_name="bagel"
 folder_name="res_64_up_attn2_t_2_attn2"
-data_name="MVTec"
+data_name="MVTec3D-AD"
 normality_folder='anormal'
 
 network_weight_folder="../result/${data_name}_experiment/${class_name}/lora_training/${normality_folder}/${folder_name}/models"
 detection_network_weights="../result/${data_name}_experiment/${class_name}/lora_training/normal/res_64_up_attn012_t_2_only_normal/models/epoch-000003.safetensors"
 img_folder="../../../MyData/anomaly_detection/${data_folder}/${class_name}"
 
-port_number=54001
+port_number=54010
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
-  --main_process_port ${port_number} ../lora/rec_infer_latent_diff.py \
+  --main_process_port ${port_number} ../lora/rec_infer_latent_diff2.py \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --sample_sampler ddim \
   --seed 42 \
@@ -29,7 +29,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --num_ddim_steps 4 \
   --prompt 'good' \
   --negative_prompt "low quality, worst quality, bad anatomy, bad composition, poor, low effort" \
-  --guidance_scale 8.5 \
+  --guidance_scale 1 \
   --truncate_length 2 \
   --start_from_final \
   --only_zero_save \
