dc05210bbb8c166c317e88fe899b85cb135587f4
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-26 09:45:24 +0900

    Update get_all_metrics.sh

--- script/get_all_metrics.sh
+++ script/get_all_metrics.sh
@@ -2,7 +2,7 @@
 
 class_name="peach"
 condition_folder='detec_epoch_4_anomal_thredhold_0.5_step_4_guidance_15.0'
-second_folder_name="res_64_up_attn2_t_2_attn2_data_40"
+second_folder_name="res_64_up_attn2_t_2_attn2_0"
 
 python ../lora/evaluation/get_all_metrics.py \
   --class_name ${class_name} \
