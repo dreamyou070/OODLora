89a39582e2f93ed1d261e660d26740ad946174b6
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-26 09:26:38 +0900

    Update get_all_metrics.sh

--- script/get_all_metrics.sh
+++ script/get_all_metrics.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-class_name="carrot"
-condition_folder='detec_epoch_3_anomal_thredhold_0.5_step_4_guidance_8.5'
-second_folder_name="res_64_up_attn2_t_2_attn2_data_12"
+class_name="dowel"
+condition_folder='detec_epoch_3_anomal_thredhold_0.5_step_4_guidance_15.0'
+second_folder_name="res_64_up_attn2_t_2_attn2"
 
 python ../lora/evaluation/get_all_metrics.py \
   --class_name ${class_name} \
