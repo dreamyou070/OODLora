92ca896f1b26ba6a7611cd6acdb5960de168b7ac
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-26 11:49:48 +0900

    Update rec_infer_latent_diff2.py

--- lora/rec_infer_latent_diff2.py
+++ lora/rec_infer_latent_diff2.py
@@ -304,14 +304,14 @@ def main(args):
                                                        guidance_scale=args.guidance_scale,
                                                        negative_prompt=args.negative_prompt,
                                                        back_dict=back_dict,
-                                                       #mask=recon_mask)
-                                                       mask = None)
-                                image = pipeline.latents_to_image(latents)[0].resize((org_h, org_w))
-                                img_dir = os.path.join(class_base_folder, f'original{ext}')
-                                image.save(img_dir)
+                                                       mask=recon_mask)
+                                                       #mask = None)
                                 #image = pipeline.latents_to_image(latents)[0].resize((org_h, org_w))
-                                #img_dir = os.path.join(class_base_folder, f'{name}_recon{ext}')
+                                #img_dir = os.path.join(class_base_folder, f'original{ext}')
                                 #image.save(img_dir)
+                                image = pipeline.latents_to_image(latents)[0].resize((org_h, org_w))
+                                img_dir = os.path.join(class_base_folder, f'{name}_recon{ext}')
+                                image.save(img_dir)
                                 # -------------------------------------- [4] anomaly map -------------------------------------- #
                                 network.restore()
                                 network.load_weights(weight_dir)
