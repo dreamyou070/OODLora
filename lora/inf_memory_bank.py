936640c10091fec2b5644166d4b85c557dee574f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-28 00:39:27 +0900

    Update inf_memory_bank.py

--- lora/inf_memory_bank.py
+++ lora/inf_memory_bank.py
@@ -251,8 +251,14 @@ def main(args):
                                     b_dist = mahalanobis(sample, b_center, b_cov)
                                     total_dist = n_dist + b_dist
                                     n_dist = torch.tensor((n_dist / total_dist))
-                                    n_dist_list.append(n_dist)
-                                n_dist_vector = torch.stack(n_dist_list, dim=0).unsqueeze(0)
+                                    d_dist = torch.tensor((d_dist / total_dist))
+                                    if n_dist > d_dist :
+                                        t_dist_list.append(n_dist)
+                                    else :
+                                        t_dist_list.append(d_dist)
+                                    #n_dist_list.append(n_dist)
+
+                                n_dist_vector = torch.stack(t_dist_list, dim=0).unsqueeze(0)
                                 n_dist_map = n_dist_vector.reshape(res,res)
                                 recon_mask = n_dist_map.to(device)
                                 #total_diff = n_diff + b_diff
