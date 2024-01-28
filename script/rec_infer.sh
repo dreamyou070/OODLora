4625c04e8c3b54654dbbd41abd6c907809225cc4
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-28 19:24:40 +0900

    Update rec_infer.sh

--- script/rec_infer.sh
+++ script/rec_infer.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 class_name="bagel"
-folder_name="res_16_32_up_t_2_attn_1_20240128_with_gaussian_noise"
+folder_name="res_16_32_64_up_t_2_20240128_with_gaussian_noise"
 data_folder="MVTec3D-AD"
 normality_folder='normal'
 network_weight_folder="../result/${data_folder}_experiment/${class_name}/lora_training/${normality_folder}/${folder_name}/models"
@@ -20,9 +20,9 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --network_dim 64 \
   --network_alpha 4 \
   --network_weights ${network_weight_folder}  \
-  --cross_map_res "[16,32]" \
+  --cross_map_res "[16,32,64]" \
   --trg_position "['up']" \
-  --trg_part "['attn_1']" \
+  --trg_part "['attn_0','attn_1','attn_2']" \
   --num_ddim_steps 50 \
   --prompt 'good' \
   --negative_prompt "low quality, worst quality, bad anatomy, bad composition, poor, low effort" \
