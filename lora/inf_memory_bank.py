0ffe163acb03e3bda6c3ddd82712d1a21e8cd95b
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-28 09:47:59 +0900

    Update inf_memory_bank.py

--- lora/inf_memory_bank.py
+++ lora/inf_memory_bank.py
@@ -128,7 +128,7 @@ def main(args):
 
     print(f'\n step 3. save directory and save config')
     parent = os.path.split(args.network_weights)[0]
-    center_folders = os.path.join(parent, f'centers_20240128_{args.cross_map_res[0]}_{args.trg_part[0]}')
+    center_folders = os.path.join(parent, f'centers_20240128_{args.cross_map_res[0]}_{args.trg_part[0]}_pca_dim_320/centers')
     print(f'center folder: {center_folders}')
 
     network_weights = os.listdir(args.network_weights)
@@ -177,7 +177,7 @@ def main(args):
             test_lora_dir = os.path.join(args.output_dir, f'lora_{model_epoch}')
             os.makedirs(test_lora_dir, exist_ok=True)
             ## (3.1) base dir
-            condition_save_dir = os.path.join(test_lora_dir, f'memory_bank_{args.cross_map_res[0]}_{args.trg_part[0]}')
+            condition_save_dir = os.path.join(test_lora_dir, f'memory_bank_{args.cross_map_res[0]}_{args.trg_part[0]}_reconstruction_test')
             print(f'condition_save_dir : {condition_save_dir}')
             os.makedirs(condition_save_dir, exist_ok=True)
             ## (3.2) evaluate dir
