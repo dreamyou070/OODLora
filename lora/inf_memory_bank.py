7cc1798d8142e98fb31b6bf778d2f908596bddb5
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-28 10:05:59 +0900

    Update inf_memory_bank.py

--- lora/inf_memory_bank.py
+++ lora/inf_memory_bank.py
@@ -292,7 +292,7 @@ def main(args):
                                     #    t_dist_list.append(n_dist)
                                     #else :
                                     #    t_dist_list.append(b_dist)
-                                    t_dist_list.append(n_dist)
+                                    t_dist_list.append(b_dist)
 
                                 n_dist_vector = torch.stack(t_dist_list, dim=0).unsqueeze(0)
                                 n_dist_map = n_dist_vector.reshape(res,res)
