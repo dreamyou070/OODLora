1b0dee555247c64d5f0ee4a69f53e5266c1d8c09
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-29 16:01:47 +0900

    train anormal model

--- script/train_anormal_with_noise.sh
+++ script/train_anormal_with_noise.sh
@@ -5,11 +5,11 @@ data_source='train_normal'
 data_folder='MVTec3D-AD'
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
 normal_folder='normal'
-save_folder="res_32_up_attn_1_20240128_with_random_position_gaussian_not_cls_training_all_text_only_object_anomal_p_0.5"
+save_folder="res_32_up_attn_1_20240128_with_random_position_shuffle_not_cls_training_all_text_only_object_anomal_p_0.5"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
 #network_weights="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/res_64_up_attn2_t_2_attn2/models/epoch-000007.safetensors"
 start_epoch=0
-port_number=59003
+port_number=59002
 # NCCL_P2P_DISABLE=1
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
@@ -32,4 +32,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
   --detail_64_up \
   --trg_position "['up']" \
   --normal_with_background \
-  --trg_part '["attn_1"]'
+  --trg_part '["attn_1"]' --shuffle
