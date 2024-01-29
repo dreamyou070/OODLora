11ba4457e0f94cfc15dcbe836a06fbecc49db747
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-30 07:32:58 +0900

    Update recon_infer_test.sh

--- script/recon_infer_test.sh
+++ script/recon_infer_test.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 class_name="bagel"
-folder_name="res_32_up_attn_1_cls_training_all_text_init_gaussian_noise"
+folder_name="res_32_up_only_normal_20240128_small_data"
 data_folder="MVTec3D-AD"
 normality_folder='normal'
 network_weight_folder="../result/${data_folder}_experiment/${class_name}/lora_training/${normality_folder}/${folder_name}/models"
 img_folder="../../../MyData/anomaly_detection/${data_folder}/${class_name}"
 
-port_number=50803
+port_number=50802
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
   --main_process_port ${port_number} ../lora/recon_infer_test.py \
