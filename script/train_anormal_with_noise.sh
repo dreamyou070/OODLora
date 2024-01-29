2eed8ab768f3761ececbe738b9dba5eed0c9c560
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-29 11:06:56 +0900

    Update train_anormal_with_noise.sh

--- script/train_anormal_with_noise.sh
+++ script/train_anormal_with_noise.sh
@@ -10,7 +10,7 @@ output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${no
 #network_weights="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/res_64_up_attn2_t_2_attn2/models/epoch-000007.safetensors"
 start_epoch=0
 port_number=59002
-
+# NCCL_P2P_DISABLE=1
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
   --main_process_port $port_number ../lora/train_anormal_with_noise.py \
