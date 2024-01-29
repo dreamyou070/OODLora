2cd76fd95018940b05a692c79ead6e1ab2bd5344
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-30 00:16:15 +0900

    Update train_normal.sh

--- script/train_normal.sh
+++ script/train_normal.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
 class_name="cable_gland"
-data_source='train_normal'
+data_source='train_normal_small'
 data_folder='MVTec3D-AD'
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
 normal_folder='normal'
-save_folder="res_16_32_64_up_only_normal_20240128"
+save_folder="res_32_up_only_normal_20240128_small_data"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
 
 start_epoch=0
 port_number=50019
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
   --main_process_port $port_number ../lora/train_normal.py \
   --process_title parksooyeon \
   --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
@@ -27,5 +27,5 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --train_data_dir "$train_data_dir" \
   --start_epoch $start_epoch \
   --output_dir "$output_dir" --truncate_pad --truncate_length 2  \
-  --cross_map_res "[64, 32, 16]" --detail_64_up --trg_position "['up']" \
-  --trg_part '["attn_2","attn_1","attn_0"]' --cls_training  # --normal_with_background #  --network_weights "$network_weights"
+  --cross_map_res "[32]" --detail_64_up --trg_position "['up']" \
+  --trg_part '["attn_1"]' --cls_training  # --normal_with_background #  --network_weights "$network_weights"
