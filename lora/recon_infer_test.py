f7f4fa421001124b65567e31b790f40f341865cb
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-29 23:54:53 +0900

    Update recon_infer_test.py

--- lora/recon_infer_test.py
+++ lora/recon_infer_test.py
@@ -325,9 +325,9 @@ def main(args) :
                         anomaly_score_pil = Image.fromarray((255 - (anomaly_score * 255)).astype(np.uint8))
                         anomaly_score_pil = anomaly_score_pil.resize((org_h, org_w))
                         anomaly_mask_save_dir = os.path.join(class_base_folder, f'{name}_attn0{ext}')
-                        
+                        anomaly_score_pil.save(anomaly_mask_save_dir)
+
                         #tiff_anomaly_mask_save_dir = os.path.join(evaluate_class_dir, f'{name}_attn0.tiff')
-                        #anomaly_score_pil.save(anomaly_mask_save_dir)
                         #anomaly_score_pil.save(tiff_anomaly_mask_save_dir)
 
 
