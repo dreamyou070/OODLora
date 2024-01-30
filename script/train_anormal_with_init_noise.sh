64c2a47e15a6b8ce300bc8fe57a6dfab292ab14f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-30 12:42:38 +0900

    Update train_anormal_with_init_noise.sh

--- script/train_anormal_with_init_noise.sh
+++ script/train_anormal_with_init_noise.sh
@@ -5,7 +5,7 @@ data_source='train_normal'
 data_folder='MVTec3D-AD'
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
 normal_folder='normal'
-save_folder="res_64_up_attn2_t_2_attn2_normal_init_gaussian_noise_unet_frozen_activating_deactivating"
+save_folder="res_64_up_attn2_t_2_attn2_normal_init_gaussian_noise_unet_frozen_act_deact"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
 network_weights="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/res_64_up_attn2_t_2_attn2_normal/models/epoch-000008.safetensors"
 
