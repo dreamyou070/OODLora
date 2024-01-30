fbc98c67aa262d3da7504de3a2332a11d03295d7
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-30 13:21:12 +0900

    Update train_normal.sh

--- script/train_normal.sh
+++ script/train_normal.sh
@@ -5,11 +5,11 @@ data_source='train_normal'
 data_folder='MVTec3D-AD'
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
 normal_folder='normal'
-save_folder="res_64_up_attn2_t_2_dim_64"
+save_folder="res_64_up_attn2_t_2_dim_20"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
 
 start_epoch=0
-port_number=50064
+port_number=50001
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
   --main_process_port $port_number ../lora/train_normal.py \
@@ -17,7 +17,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --network_module networks.lora \
-  --network_dim 64 --network_alpha 4 --train_batch_size 1 \
+  --network_dim 20 --network_alpha 4 --train_batch_size 1 \
   --optimizer_type AdamW --lr_scheduler cosine_with_restarts \
   --lr_warmup_steps 144 --learning_rate 0.0003 --unet_lr 0.0001 --text_encoder_lr 0.00005 --resolution '512,512' --save_every_n_epochs 1 \
   --sample_every_n_epochs 1 \
