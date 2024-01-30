a65eea6565904a993deded143c255fa4fca9bdf5
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-30 14:06:42 +0900

    Update train_anormal_with_init_noise.sh

--- script/train_anormal_with_init_noise.sh
+++ script/train_anormal_with_init_noise.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
-class_name="bagel"
+class_name="cable_gland"
 data_source='train_normal'
 data_folder='MVTec3D-AD'
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
 normal_folder='normal'
-save_folder="res_64_up_attn2_total_text_attn2_normal_init_gaussian_noise_unet_frozen_none_act_deact"
+save_folder="res_64_up_attn012_t_2_only_normal_total_text_attn2_normal_init_gaussian_noise_unet_frozen_none_act_deact"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
-network_weights="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/res_64_up_attn2_t_2_attn2_normal/models/epoch-000008.safetensors"
+network_weights="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/res_64_up_attn012_t_2_only_normal/models/epoch-000005.safetensors"
 
 start_epoch=0
-port_number=51300
+port_number=51301
 #
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
   --main_process_port $port_number ../lora/train_anormal_with_init_noise.py \
