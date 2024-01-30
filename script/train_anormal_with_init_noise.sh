39bf32957ab246f57379638a94c7f39307c3f9c6
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-30 12:06:18 +0900

    Update train_anormal_with_init_noise.sh

--- script/train_anormal_with_init_noise.sh
+++ script/train_anormal_with_init_noise.sh
@@ -12,7 +12,7 @@ output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${no
 start_epoch=0
 port_number=51335
 #
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
   --main_process_port $port_number ../lora/train_anormal_with_init_noise.py \
   --process_title parksooyeon \
   --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
