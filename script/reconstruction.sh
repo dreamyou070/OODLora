1eee62ac04798a95e354b36e844339256158980f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-01 08:00:15 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
 class_name="bagel"
-folder_name="res_64_down_attn1_text_attnloss_weight_0.001"
+folder_name="res_64_32_16_down_all_text_attnloss_weight_0.001"
 data_name="MVTec3D-AD"
 normality_folder='normal'
 
 network_weight_folder="../result/${data_name}_experiment/${class_name}/lora_training/${normality_folder}/${folder_name}/models"
-detection_network_weights="../result/${data_name}_experiment/${class_name}/lora_training/normal/${folder_name}/models/epoch-000001.safetensors"
+detection_network_weights="../result/${data_name}_experiment/${class_name}/lora_training/normal/${folder_name}/models/epoch-000011.safetensors"
 img_folder="../../../MyData/anomaly_detection/${data_name}/${class_name}"
 
-port_number=50001
+port_number=50002
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port ${port_number} ../lora/reconstruction.py \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
@@ -37,7 +37,9 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --anormal_thred 0.5 \
   --trg_layer 'down_blocks_0_attentions_1_transformer_blocks_0_attn2' \
   --only_zero_save \
-  --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2']"
+  --trg_layer_list "['down_blocks_0_attentions_0_transformer_blocks_0_attn2', 'down_blocks_0_attentions_1_transformer_blocks_0_attn2',
+                     'down_blocks_1_attentions_0_transformer_blocks_0_attn2', 'down_blocks_1_attentions_1_transformer_blocks_0_attn2',
+                     'down_blocks_2_attentions_0_transformer_blocks_0_attn2', 'down_blocks_2_attentions_1_transformer_blocks_0_attn2']"
   #, 'down_blocks_0_attentions_1_transformer_blocks_0_attn2',
   #                  'down_blocks_1_attentions_0_transformer_blocks_0_attn2', 'down_blocks_1_attentions_1_transformer_blocks_0_attn2',
   #                  'down_blocks_2_attentions_0_transformer_blocks_0_attn2', 'down_blocks_2_attentions_1_transformer_blocks_0_attn2',]"
