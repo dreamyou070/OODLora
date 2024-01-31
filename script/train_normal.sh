2add057b9592f3acf73bc56320cbefc6cdae9851
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-31 20:55:59 +0900

    Update train_normal.sh

--- script/train_normal.sh
+++ script/train_normal.sh
@@ -6,14 +6,14 @@ class_name="bagel"
 data_source='train_normal'
 data_folder='MVTec3D-AD'
 normal_folder='normal'
-save_folder="res_64_32_16_8_all_t_2_dim_64"
+save_folder="res_64_32_16_down_all_t_2_dim_64"
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
 
 start_epoch=0
-port_number=50033
+port_number=52222
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config \
   --main_process_port $port_number ../lora/train_normal.py \
   --process_title parksooyeon \
   --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
@@ -25,15 +25,14 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --sample_every_n_epochs 1 \
   --sample_prompts ../../../MyData/anomaly_detection/inference.txt \
   --max_train_steps 10000 --attn_loss --task_loss_weight 1.0 --seed 42 --class_caption 'good' \
-  --max_train_epochs 20 \
+  --max_train_epochs 500 \
   --train_data_dir ${train_data_dir} \
   --start_epoch $start_epoch \
   --output_dir "$output_dir" \
   --truncate_pad --truncate_length 2  \
-  --cross_map_res "[64,32,16,8]" \
+  --cross_map_res "[64,32,16]" \
   --detail_64_down \
-  --detail_64_up \
-  --trg_position "['down','up','mid']" \
+  --trg_position "['down','up']" \
   --trg_part '["attn_0","attn_1","attn_2"]' \
   --do_task_loss \
   --cls_training
