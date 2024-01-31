a40bff1cc006091ae728872a17914df13e841cf0
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-31 22:13:09 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 class_name="bagel"
-folder_name="res_64_32_16_all_t_2_dim_64"
+folder_name="res_64_32_16_down_t_2_dim_64"
 data_name="MVTec3D-AD"
 normality_folder='normal'
 
@@ -38,8 +38,9 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --trg_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn2' \
   --trg_layer_list "['down_blocks_0_attentions_0_transformer_blocks_0_attn2', 'down_blocks_0_attentions_1_transformer_blocks_0_attn2',
                      'down_blocks_1_attentions_0_transformer_blocks_0_attn2','down_blocks_1_attentions_1_transformer_blocks_0_attn2',
-                     'down_blocks_2_attentions_0_transformer_blocks_0_attn2','down_blocks_2_attentions_1_transformer_blocks_0_attn2',
-    'up_blocks_1_attentions_0_transformer_blocks_0_attn2','up_blocks_1_attentions_1_transformer_blocks_0_attn2','up_blocks_1_attentions_2_transformer_blocks_0_attn2',
-    'up_blocks_2_attentions_0_transformer_blocks_0_attn2','up_blocks_2_attentions_1_transformer_blocks_0_attn2','up_blocks_2_attentions_2_transformer_blocks_0_attn2',
-    'up_blocks_3_attentions_0_transformer_blocks_0_attn2','up_blocks_3_attentions_1_transformer_blocks_0_attn2','up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
+                     'down_blocks_2_attentions_0_transformer_blocks_0_attn2','down_blocks_2_attentions_1_transformer_blocks_0_attn2',]" \
   --only_zero_save
+    #'up_blocks_1_attentions_0_transformer_blocks_0_attn2','up_blocks_1_attentions_1_transformer_blocks_0_attn2','up_blocks_1_attentions_2_transformer_blocks_0_attn2',
+    #'up_blocks_2_attentions_0_transformer_blocks_0_attn2','up_blocks_2_attentions_1_transformer_blocks_0_attn2','up_blocks_2_attentions_2_transformer_blocks_0_attn2',
+    #'up_blocks_3_attentions_0_transformer_blocks_0_attn2','up_blocks_3_attentions_1_transformer_blocks_0_attn2','up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
+
