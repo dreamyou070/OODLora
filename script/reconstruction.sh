474d75546a34351c537397c475078ec24124b412
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-01-31 21:40:47 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -39,7 +39,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --trg_layer_list "['down_blocks_0_attentions_0_transformer_blocks_0_attn2', 'down_blocks_0_attentions_1_transformer_blocks_0_attn2',
                      'down_blocks_1_attentions_0_transformer_blocks_0_attn2','down_blocks_1_attentions_1_transformer_blocks_0_attn2',
                      'down_blocks_2_attentions_0_transformer_blocks_0_attn2','down_blocks_2_attentions_1_transformer_blocks_0_attn2',
-    'up_blocks_0_attentions_0_transformer_blocks_0_attn2','up_blocks_0_attentions_1_transformer_blocks_0_attn2','up_blocks_0_attentions_2_transformer_blocks_0_attn2',
     'up_blocks_1_attentions_0_transformer_blocks_0_attn2','up_blocks_1_attentions_1_transformer_blocks_0_attn2','up_blocks_1_attentions_2_transformer_blocks_0_attn2',
-    'up_blocks_2_attentions_0_transformer_blocks_0_attn2','up_blocks_2_attentions_1_transformer_blocks_0_attn2','up_blocks_2_attentions_2_transformer_blocks_0_attn2']" \
+    'up_blocks_2_attentions_0_transformer_blocks_0_attn2','up_blocks_2_attentions_1_transformer_blocks_0_attn2','up_blocks_2_attentions_2_transformer_blocks_0_attn2',
+    'up_blocks_3_attentions_0_transformer_blocks_0_attn2','up_blocks_3_attentions_1_transformer_blocks_0_attn2','up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
   --only_zero_save
