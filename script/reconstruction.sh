1fcb99a4d2c9b8ad32ff56bab8550e09af25af1a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-02 08:12:49 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -2,7 +2,7 @@
 
 class_name="bagel"
 #            res_64_down_1_t_2_normal_attn_loss_weight_0.001_without_mahal_loss
-folder_name="res_64_down_1_t_2_normal_attn_loss_weight_0.001_without_mahal_loss_task_loss_weight_0.001"
+folder_name="res_64_down_1_t_2_normal_attn_loss_weight_1_mahal_loss_new_code"
 data_name="MVTec3D-AD"
 normality_folder='normal'
 
