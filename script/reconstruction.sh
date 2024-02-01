65df97b7e877ce99dcd5e9f8ff2e87fc62256faa
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-01 16:06:10 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 class_name="bagel"
-folder_name="res_64_up_2_all_text_arc_0.001_deact"
+folder_name="normal_res_64_down_attn1_text_attnloss_weight_0.001"
 data_name="MVTec3D-AD"
 normality_folder='normal'
 
@@ -35,10 +35,10 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --use_pixel_mask \
   --class_name ${class_name} \
   --anormal_thred 0.5 \
-  --trg_layer 'up_blocks_3_attentions_1_transformer_blocks_0_attn2' \
+  --training_test \
+  --trg_layer 'down_blocks_0_attentions_1_transformer_blocks_0_attn2' \
   --only_zero_save \
-  --trg_layer_list "['down_blocks_0_attentions_0_transformer_blocks_0_attn2','down_blocks_0_attentions_1_transformer_blocks_0_attn2',
-                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2']"
+  --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2']"
 
   #, 'down_blocks_0_attentions_1_transformer_blocks_0_attn2',
   #                  'down_blocks_1_attentions_0_transformer_blocks_0_attn2', 'down_blocks_1_attentions_1_transformer_blocks_0_attn2',
