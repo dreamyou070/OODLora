8d4296da5e273e85b601f43da0049ee90f5ef0ae
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-01 17:47:47 +0900

    s
    s

--- lora/test.py
+++ lora/test.py
@@ -1,5 +1,15 @@
 import torch
+import numpy as np
+from scipy.spatial.distance import mahalanobis
 
-a = torch.randn(8,1024, 2)
-s = a.mean(dim=0)
-print(s.shape)
+normal_vectors = torch.randn((10,30))
+normal_vector_mean = torch.mean(normal_vectors, dim=0)
+
+normal_vectors_mean = torch.mean(normal_vectors, dim=0).numpy()  # dim
+normal_vectors_cov = np.cov(np.array(normal_vectors.cpu()), rowvar=False)
+mahalanobis_dists = [mahalanobis(feat.cpu().numpy(), normal_vectors_mean, normal_vectors_cov) for feat in normal_vectors]
+
+
+normal_vector_mean_torch = torch.mean(normal_vectors, dim=0)
+normal_vectors_cov_torch = torch.cov(normal_vectors.transpose(0, 1))
+mahalanobis_dists = [mahalanobis(feat.detach().numpy(), normal_vector_mean_torch, normal_vectors_cov_torch) for feat in normal_vectors]
--- lora/train_mahal.py
+++ lora/train_mahal.py
@@ -541,10 +541,13 @@ class NetworkTrainer:
                         features.add(feat)
                 features = list(features)
                 normal_vectors = torch.cat(features, dim=0)  # sample, dim
-                normal_vectors_mean = torch.mean(normal_vectors.detach().numpy(),dim=0).numpy()  # dim
-                normal_vectors_cov = np.cov(np.array(normal_vectors.cpu()), rowvar=False)
-                mahalanobis_dists = [mahalanobis(feat.cpu().numpy(), normal_vectors_mean, normal_vectors_cov)
-                                     for feat in features]
+
+                normal_vector_mean_torch = torch.mean(normal_vectors, dim=0)
+                normal_vectors_cov_torch = torch.cov(normal_vectors.transpose(0, 1))
+                mahalanobis_dists = [
+                    mahalanobis(feat.detach().numpy(), normal_vector_mean_torch, normal_vectors_cov_torch) for feat in
+                    normal_vectors]
+
                 dist_loss = torch.tensor(mahalanobis_dists)
                 dist_loss = dist_loss.mean()
 
