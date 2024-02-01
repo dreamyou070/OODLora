d2ba52838e366e8cbda919d01f1b29294cefa08d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-01 09:53:38 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -6,7 +6,7 @@ data_name="MVTec3D-AD"
 normality_folder='normal'
 
 network_weight_folder="../result/${data_name}_experiment/${class_name}/lora_training/${normality_folder}/${folder_name}/models"
-detection_network_weights="../result/${data_name}_experiment/${class_name}/lora_training/normal/${folder_name}/models/epoch-000011.safetensors"
+detection_network_weights="../result/${data_name}_experiment/${class_name}/lora_training/normal/${folder_name}/models/epoch-000019.safetensors"
 img_folder="../../../MyData/anomaly_detection/${data_name}/${class_name}"
 
 port_number=50004
