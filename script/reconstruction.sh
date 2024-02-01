53f41e9c4322f718893865c984c1d6fae19de1a4
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-01 20:11:42 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -35,6 +35,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --use_pixel_mask \
   --class_name ${class_name} \
   --anormal_thred 0.5 \
+  --training_test \
   --trg_layer 'down_blocks_0_attentions_1_transformer_blocks_0_attn2' \
   --only_zero_save \
   --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2']"
