115161e38f8361078a5d3e804ba2af8bf0541b2c
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-01 18:40:21 +0900

    Update train_mahal.sh

--- script/train_mahal.sh
+++ script/train_mahal.sh
@@ -6,12 +6,12 @@ data_folder='MVTec3D-AD'
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
 all_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/train_ex2/rgb"
 normal_folder='normal'
-save_folder="res_64_down_1_all_text_mahal_loss"
+save_folder="res_64_down_1_all_text_mahal_loss_real"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
 network_weights="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/res_64_down_attn1_up_attn12_all_text_attnloss_weight_0.001/models/epoch-000004.safetensors"
 
 start_epoch=0
-port_number=59516
+port_number=59533
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
   --main_process_port $port_number ../lora/train_mahal.py \
@@ -43,6 +43,6 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --normal_weight 0.001 \
   --concat_query \
   --do_check_anormal \
-  --training_layer 'up_blocks_3_attentions_2_transformer_blocks_0_attn2'
+  --training_layer 'down_blocks_0_attentions_1_transformer_blocks_0_attn2'
   #--network_weights "$network_weights" \
 
