8988b542d68dcfd8f73b69816c11638d13a3fe1c
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-02 00:15:45 +0900

    Update train_mahal.sh

--- script/train_mahal.sh
+++ script/train_mahal.sh
@@ -6,14 +6,14 @@ data_folder='MVTec3D-AD'
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
 #all_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/train_ex2/rgb"
 normal_folder='normal'
-save_folder="res_64_down_1_t_2_normal_attn_loss_weight_0.001_mahal_loss"
+save_folder="res_64_up_2_t_2_normal_attn_loss_weight_0.001_mahal_loss"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
 #network_weights="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/res_64_down_attn1_up_attn12_all_text_attnloss_weight_0.001/models/epoch-000004.safetensors"
 
 start_epoch=0
 port_number=59532
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config \
   --main_process_port $port_number ../lora/train_mahal.py \
   --process_title parksooyeon \
   --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
@@ -34,9 +34,9 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --start_epoch $start_epoch \
   --output_dir "$output_dir" \
   --cross_map_res "[64]" \
-  --detail_64_down \
-  --trg_position "['down']" \
-  --trg_part '["attn_1"]' \
+  --detail_64_up \
+  --trg_position "['up']" \
+  --trg_part '["attn_2"]' \
   --act_deact \
   --back_training --cls_training \
   --mahalanobis_loss_weight 1 \
