60a8da9919d36e20e685e5125420325d41cdb94d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-02 13:37:56 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 class_name="bagel"
-folder_name="res_64_down_1_t_2_normal_attn_loss_weight_1_mahal_loss_new_code_disloss_2_data_10_act_deact_query_add_random_strict_training"
+folder_name="res_64_down_1_t_2_mahal_loss_new_code_disloss_2_data_10_act_deact_query_add_random_strict_training_only_object"
 data_name="MVTec3D-AD"
 normality_folder='normal'
 network_weight_folder="../result/${data_name}_experiment/${class_name}/lora_training/${normality_folder}/${folder_name}/models"
 detection_network_weights="../result/${data_name}_experiment/${class_name}/lora_training/normal/${folder_name}/models/epoch-000001.safetensors"
 img_folder="../../../MyData/anomaly_detection/${data_name}/${class_name}"
-port_number=50042
+port_number=50045
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port ${port_number} ../lora/reconstruction.py \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --sample_sampler ddim \
@@ -19,8 +19,8 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --network_weights ${network_weight_folder}  \
   --detection_network_weights ${detection_network_weights} \
   --cross_map_res [64,32,16] \
-  --trg_position "['down']" \
-  --trg_part "['attn_0','attn_1','attn_2']" \
+  --trg_position "['up']" \
+  --trg_part "['attn_2']" \
   --num_ddim_steps 30 \
   --prompt 'good' \
   --negative_prompt "low quality, worst quality, bad anatomy, bad composition, poor, low effort" \
