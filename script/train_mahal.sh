f42884686131fca21fb4213bd9397ad43cd728b2
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-03 00:26:00 +0900

    Update train_mahal.sh

--- script/train_mahal.sh
+++ script/train_mahal.sh
@@ -5,11 +5,11 @@ data_source='train_normal'
 data_folder='MVTec3D-AD'
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
 normal_folder='normal'
-save_folder="object_detection"
+save_folder="res_64_down_1_no_task_mahal_attn_0.008_act_deact_text_frozen"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
-#network_weights="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${normal_folder}/res_64_down_1_mahal_attn_0.001_act_deact/models/epoch-000020.safetensors"
+network_weights="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${normal_folder}/res_64_down_1_mahal_attn_0.001_act_deact/models/epoch-000002.safetensors"
 start_epoch=0
-port_number=58822
+port_number=58837
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
   --main_process_port $port_number ../lora/train_mahal.py --process_title parksooyeon \
@@ -23,9 +23,9 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --start_epoch $start_epoch --max_train_steps 500 --max_train_epochs 500 \
   --train_data_dir "$train_data_dir" --resolution '512,512' --class_caption 'good' \
   --output_dir "$output_dir" \
-  --do_task_loss --task_loss_weight 1 \
   --mahalanobis_loss_weight 1 \
   --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2']" \
   --attn_loss --attn_loss_weight 1 --cls_training --back_training \
   --act_deact --act_deact_weight 0 \
-  #--text_frozen --network_weights "$network_weights"
+  --text_frozen --network_weights "$network_weights"
+  #--do_task_loss --task_loss_weight 1 \
