a50b6fca3036d12a75196bb65feaee01483e1cec
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-02 12:19:56 +0900

    Update train_mahal.sh

--- script/train_mahal.sh
+++ script/train_mahal.sh
@@ -4,10 +4,10 @@ data_source='train_normal'
 data_folder='MVTec3D-AD'
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
 normal_folder='normal'
-save_folder="res_64_down_1_t_2_normal_attn_loss_weight_1_mahal_loss_new_code_disloss_2_data_10_act_deact_query_add_random"
+save_folder="res_64_down_1_t_2_mahal_loss_new_code_disloss_2_data_10_act_deact_query_add_random_strict_training_all_object_deact"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
 start_epoch=0
-port_number=58822
+port_number=58825
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
   --main_process_port $port_number ../lora/train_mahal.py \
   --process_title parksooyeon \
@@ -36,10 +36,9 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2
   --act_deact_weight 1.0 \
   --back_training --cls_training \
   --mahalanobis_loss_weight 1 \
-  --attn_loss_weight 0.001 \
+  --attn_loss_weight 1 \
   --task_loss_weight 1 \
   --do_task_loss \
   --truncate_pad --truncate_length 2 \
   --normal_weight 1 \
-  --query_add_random \
-  --partial
+  --query_add_random
