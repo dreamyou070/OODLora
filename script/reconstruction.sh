838e4d46cc2a5868cd785402ade51451078ee556
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-02 17:01:56 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 class_name="bagel"
-folder_name="res_64_up_2_t_2_normal_attn_loss_weight_1_mahal_loss_new_code_disloss_2_data_10_act_deact_query_add_random_strict_training"
+folder_name="res_64_down_1_t_2_anomal_add_query_unet_frozen"
 data_name="MVTec3D-AD"
 normality_folder='normal'
 network_weight_folder="../result/${data_name}_experiment/${class_name}/lora_training/${normality_folder}/${folder_name}/models"
 detection_network_weights="../result/${data_name}_experiment/${class_name}/lora_training/normal/${folder_name}/models/epoch-000006.safetensors"
 img_folder="../../../MyData/anomaly_detection/${data_name}/${class_name}"
-port_number=50044
+port_number=50046
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port ${port_number} ../lora/reconstruction.py \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --sample_sampler ddim \
@@ -19,7 +19,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --network_weights ${network_weight_folder}  \
   --detection_network_weights ${detection_network_weights} \
   --cross_map_res [64,32,16] \
-  --trg_position "['up']" \
+  --trg_position "['down']" \
   --trg_part "['attn_2']" \
   --num_ddim_steps 30 \
   --prompt 'good' \
@@ -31,6 +31,6 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --use_pixel_mask \
   --class_name ${class_name} \
   --anormal_thred 0.5 \
-  --trg_layer 'up_blocks_3_attentions_2_transformer_blocks_0_attn2' \
+  --trg_layer 'down_blocks_0_attentions_1_transformer_blocks_0_attn2' \
   --only_zero_save \
-  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']"
+  --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2']"
