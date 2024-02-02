a05c1c65fe7c322f87c03b1224e9c6e92632561d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-02 19:06:14 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 class_name="bagel"
-folder_name="res_64_down_1_t_2_anomal_add_query_unet_frozen"
+folder_name="res_64_down_1_t_2_anomal_add_query_text_frozen"
 data_name="MVTec3D-AD"
 normality_folder='normal'
 network_weight_folder="../result/${data_name}_experiment/${class_name}/lora_training/${normality_folder}/${folder_name}/models"
