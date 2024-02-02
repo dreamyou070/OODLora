cba405f8723d6b4ef0222d3784fc2df4e46e4189
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-02 20:45:39 +0900

    Update train_mahal.sh

--- script/train_mahal.sh
+++ script/train_mahal.sh
@@ -5,7 +5,7 @@ data_source='train_normal'
 data_folder='MVTec3D-AD'
 train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
 normal_folder='normal'
-save_folder="res_64_down_1"
+save_folder="res_64_down_1_attn_1"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
 network_weights="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${normal_folder}/res_64_down_1_t_2_anomal_add_query/models/epoch-000004.safetensors"
 start_epoch=0
@@ -25,7 +25,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_c
   --output_dir "$output_dir" \
   --cross_map_res "[64]" --detail_64_up --trg_position "['down']" --trg_part '["attn_1"]' \
   --do_task_loss --task_loss_weight 1 \
-  --attn_loss --attn_loss_weight 0.001 \
+  --attn_loss --attn_loss_weight 1 \
   --mahalanobis_loss_weight 1 \
   --cls_training --back_training \
   --act_deact --act_deact_weight 1.0
