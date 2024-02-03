f6adc091a986a908555b3d02541151c30693d3fc
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-03 10:52:21 +0900

    Update check_mahalanobis_dists.sh

--- script/check_mahalanobis_dists.sh
+++ script/check_mahalanobis_dists.sh
@@ -15,18 +15,13 @@ port_number=59517
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
   --main_process_port $port_number ../lora/check_mahalanobis_dists.py \
   --process_title parksooyeon \
-  --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --network_module networks.lora \
-  --network_dim 64 --network_alpha 4 --train_batch_size 1 \
+  --network_dim 64 --network_alpha 4 \
   --optimizer_type AdamW --lr_scheduler cosine_with_restarts \
-  --lr_warmup_steps 144 --learning_rate 0.0003 --unet_lr 0.0001 --text_encoder_lr 0.00005 --resolution '512,512' --save_every_n_epochs 1 \
-  --sample_every_n_epochs 1 \
-  --sample_prompts ../../../MyData/anomaly_detection/inference.txt \
+  --resolution '512,512' \
   --seed 42 --class_caption 'good' \
-  --wandb_init_name ${class_name} \
   --all_data_dir "${all_data_dir}" \
-  --start_epoch $start_epoch \
   --output_dir "$output_dir" \
   --do_check_anormal \
   --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
