25c148865bcdf4364a2be711c4cb44c42be3af48
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-03 21:13:52 +0900

    train mahal

---
+++ lora/recon_test.py
@@ -0,0 +1,295 @@
+import argparse
+from accelerate.utils import set_seed
+import os
+import random
+import library.train_util as train_util
+import library.config_util as config_util
+import library.custom_train_functions as custom_train_functions
+import torch
+from PIL import Image
+import sys, importlib
+from utils.image_utils import image2latent, load_image
+from utils.scheduling_utils import get_scheduler
+from utils.model_utils import get_state_dict, init_prompt
+from attention_store import AttentionStore
+import torch.nn as nn
+from utils.model_utils import call_unet
+from utils.common_utils import get_lora_epoch, save_latent
+from utils.model_utils import get_crossattn_map
+from utils.pipeline import AnomalyDetectionStableDiffusionPipeline
+import numpy as np
+
+try:
+    from setproctitle import setproctitle
+except (ImportError, ModuleNotFoundError):
+    setproctitle = lambda x: None
+
+
+def get_position(layer_name, attn):
+    res = int(attn.shape[1] ** 0.5)
+    if 'down' in layer_name:
+        pos = 'down'
+    elif 'up' in layer_name:
+        pos = 'up'
+    else:
+        pos = 'mid'
+    if 'attentions_0' in layer_name:
+        part = 'attn_0'
+    elif 'attentions_1' in layer_name:
+        part = 'attn_1'
+    else:
+        part = 'attn_2'
+    return res, pos, part
+
+
+def register_attention_control(unet: nn.Module, controller: AttentionStore,
+                               mask_threshold: float = 1):  # if mask_threshold is 1, use itself
+
+    def ca_forward(self, layer_name):
+
+        def forward(hidden_states, context=None, trg_indexs_list=None, mask=None):
+            is_cross_attention = False
+            if context is not None:
+                is_cross_attention = True
+            self_head_query = self.to_q(hidden_states)
+
+            context = context if context is not None else hidden_states
+            key = self.to_k(context)
+            value = self.to_v(context)
+            query = self.reshape_heads_to_batch_dim(self_head_query)
+            key = self.reshape_heads_to_batch_dim(key)
+            value = self.reshape_heads_to_batch_dim(value)
+            if self.upcast_attention:
+                query = query.float()
+                key = key.float()
+            attention_scores = torch.baddbmm(
+                torch.empty(query.shape[0], query.shape[1], key.shape[1], dtype=query.dtype,
+                            device=query.device), query, key.transpose(-1, -2), beta=0, alpha=self.scale, )
+            attention_probs = attention_scores.softmax(dim=-1)
+            attention_probs = attention_probs.to(value.dtype)
+
+            if is_cross_attention:
+                controller.store(attention_probs[:, :, :args.truncate_length], layer_name)
+
+            if is_cross_attention and mask is not None:
+                if layer_name == args.trg_layer :
+                    controller.save_query(self_head_query, layer_name)
+
+            hidden_states = torch.bmm(attention_probs, value)
+            hidden_states = self.reshape_batch_dim_to_heads(hidden_states)
+            hidden_states = self.to_out[0](hidden_states)
+            return hidden_states
+
+        return forward
+
+    def register_recr(net_, count, layer_name):
+        if net_.__class__.__name__ == 'CrossAttention':
+            net_.forward = ca_forward(net_, layer_name)
+            return count + 1
+        elif hasattr(net_, 'children'):
+            for name__, net__ in net_.named_children():
+                full_name = f'{layer_name}_{name__}'
+                count = register_recr(net__, count, full_name)
+        return count
+
+    cross_att_count = 0
+    for net in unet.named_children():
+        if "down" in net[0]:
+            cross_att_count += register_recr(net[1], 0, net[0])
+        elif "up" in net[0]:
+            cross_att_count += register_recr(net[1], 0, net[0])
+        elif "mid" in net[0]:
+            cross_att_count += register_recr(net[1], 0, net[0])
+    controller.num_att_layers = cross_att_count
+
+
+def main(args):
+    parent = os.path.split(args.network_weights)[0]  # unique_folder,
+    args.output_dir = os.path.join(parent, 'reconstruction_with_test_data')
+    if args.training_test :
+        args.output_dir = os.path.join(parent, 'reconstruction_with_training_data')
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    print(f' \n step 1. setting')
+    train_util.verify_training_args(args)
+    train_util.prepare_dataset_args(args, True)
+    if args.seed is None:
+        args.seed = random.randint(0, 2 ** 32)
+    set_seed(args.seed)
+
+    print(f'\n step 2. preparing accelerator')
+    accelerator = train_util.prepare_accelerator(args)
+    weight_dtype, save_dtype = train_util.prepare_dtype(args)
+    vae_dtype = torch.float32 if args.no_half_vae else weight_dtype
+    tokenizer = train_util.load_tokenizer(args)
+    device = accelerator.device
+
+    print(f'\n step 3. save directory and save config')
+
+    if accelerator.is_main_process:
+
+        # (1) call basic model
+        text_encoder, vae, unet, _ = train_util._load_target_model(args, weight_dtype, device,
+                                                                   unet_use_linear_projection_in_v2=False, )
+        unet, text_encoder = unet.to(device), text_encoder.to(device)
+        vae.to(accelerator.device, dtype=vae_dtype)
+        scheduler_cls = get_scheduler(args.sample_sampler, args.v_parameterization)[0]
+        scheduler = scheduler_cls(num_train_timesteps=args.scheduler_timesteps, beta_start=args.scheduler_linear_start,
+                                  beta_end=args.scheduler_linear_end, beta_schedule=args.scheduler_schedule)
+        # (2) make scratch network
+        sys.path.append(os.path.dirname(__file__))
+        network_module = importlib.import_module(args.network_module)
+        net_kwargs = {}
+        network = network_module.create_network(1.0, args.network_dim, args.network_alpha, None,
+                                            text_encoder, unet, neuron_dropout=args.network_dropout, **net_kwargs, )
+        network.apply_to(text_encoder, unet, True, True)
+
+        test_folder = os.path.join(args.concept_image_folder, 'test')
+        if args.training_test :
+            test_folder = os.path.join(args.concept_image_folder, 'train_normal_test')
+
+        classes = os.listdir(test_folder)
+
+        for class_name in classes:
+            flag = True
+            if args.only_normal_infer:
+                if 'good' not in class_name:
+                    flag = False
+            if flag:
+                image_folder = os.path.join(test_folder, f'{class_name}/rgb')
+                mask_folder = os.path.join(test_folder, f'{class_name}/gt')
+                test_images = os.listdir(image_folder)
+
+                for j, test_image in enumerate(test_images):
+
+                    print(f' {class_name} : {test_image}')
+                    name, ext = os.path.splitext(test_image)
+
+                    test_img_dir = os.path.join(image_folder, test_image)
+                    mask_img_dir = os.path.join(mask_folder, test_image)
+                    org_h, org_w = Image.open(mask_img_dir).size
+
+                    if accelerator.is_main_process:
+                        with torch.no_grad():
+
+                            org_img = load_image(test_img_dir, 512, 512)
+                            org_vae_latent = image2latent(org_img, vae, device, weight_dtype)
+                            # ------------------------------------- [1] anomal mask ------------------------------ #
+                            # real network is getting good !
+                            weight_dir = args.network_weights
+                            network.load_weights(weight_dir)
+                            network.to(device)
+                            controller = AttentionStore()
+                            register_attention_control(unet, controller)
+                            with torch.no_grad():
+                                context = init_prompt(tokenizer, text_encoder, device, args.prompt)
+                            uncon, con = torch.chunk(context, 2)
+                            call_unet(unet, org_vae_latent, 0, con[:, :args.truncate_length, :], None, None)
+                            attn_stores = controller.step_store
+                            controller.reset()
+                            recon_mask = get_crossattn_map(args, attn_stores, args.trg_layer,
+                                                            thredhold=args.anormal_thred)
+                            recon_mask = (recon_mask.unsqueeze(0).unsqueeze(0)).repeat(1, 4, 1, 1)
+
+                            # ---------------------------------- [2] reconstruction ------------------------------ #
+                            pipeline = AnomalyDetectionStableDiffusionPipeline(vae=vae,
+                                           text_encoder=text_encoder,tokenizer=tokenizer,unet=unet,scheduler=scheduler,
+                                           safety_checker=None,feature_extractor=None, requires_safety_checker=False, )
+                            latents = pipeline(prompt=args.prompt,height=512,width=512,num_inference_steps=args.num_ddim_steps,
+                                               guidance_scale=args.guidance_scale,negative_prompt=args.negative_prompt,
+                                               reference_image =org_vae_latent,mask=recon_mask)
+                            recon_image = pipeline.latents_to_image(latents[-1])[0].resize((org_h, org_w))
+                            img_dir = os.path.join(class_base_folder, f'{name}_recon{ext}')
+                            recon_image.save(img_dir)
+
+                            # ----------------------------- [4] anomaly map -------------------------------------- #
+                            # (1) original
+                            org_img = pipeline.latents_to_image(org_vae_latent)[0].resize((org_h, org_w))
+                            org_img.save(os.path.join(class_base_folder, f'{name}_org{ext}'))
+                            call_unet(unet, org_vae_latent, 0, con, None, 1)
+                            trg_layer = args.trg_layer_list[0]
+                            org_query = controller.query_dict[trg_layer][0].squeeze(0)
+                            org_query = org_query / (torch.norm(org_query, dim=1, keepdim=True))
+                            controller.reset()
+
+                            # (2) recon
+                            recon_latent = latents
+                            call_unet(unet, recon_latent, 0, con, None, 1)
+                            recon_query = controller.query_dict[trg_layer][0].squeeze(0)
+                            controller.reset()
+                            recon_query = recon_query / (torch.norm(recon_query, dim=1, keepdim=True))
+
+                            # (3) anomaly score
+                            anomaly_score = (org_query @ recon_query.T).cpu()
+                            pix_num = anomaly_score.shape[0]
+                            anomaly_score = (torch.eye(pix_num) * anomaly_score).sum(dim=0)
+                            anomaly_score = anomaly_score / anomaly_score.max()  # 0 ~ 1
+                            anomaly_score = anomaly_score.unsqueeze(0).reshape(64, 64)
+                            anomaly_score = anomaly_score.numpy()
+
+                            anomaly_score_pil = Image.fromarray((255 - (anomaly_score * 255)).astype(np.uint8))
+                            anomaly_score_pil = anomaly_score_pil.resize((org_h, org_w))
+                            anomaly_mask_save_dir = os.path.join(class_base_folder, f'{name}{ext}')
+                            tiff_anomaly_mask_save_dir = os.path.join(evaluate_class_dir, f'{name}.tiff')
+                            anomaly_score_pil.save(anomaly_mask_save_dir)
+                            anomaly_score_pil.save(tiff_anomaly_mask_save_dir)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # step 1. setting
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--process_title", type=str, default='parksooyeon')
+    train_util.add_sd_models_arguments(parser)
+    train_util.add_dataset_arguments(parser, True, True, True)
+    train_util.add_training_arguments(parser, True)
+    train_util.add_optimizer_arguments(parser)
+    config_util.add_config_arguments(parser)
+    custom_train_functions.add_custom_train_arguments(parser)
+    # step 2. model
+    parser.add_argument("--no_half_vae", action="store_true",
+                        help="do not use fp16/bf16 VAE in mixed precision (use float VAE) / mixed precision", )
+    parser.add_argument("--network_module", type=str, default=None,
+                        help="network module to train")
+    parser.add_argument("--base_weights", type=str, default=None, nargs="*",
+                        help="network weights to merge into the model before training", )
+    parser.add_argument("--base_weights_multiplier", type=float, default=None, nargs="*",
+                        help="multiplier for network weights to merge into the model before training", )
+    parser.add_argument("--network_dim", type=int, default=None,
+                        help="network dimensions (depends on each network)")
+    parser.add_argument("--network_alpha", type=float, default=1,
+                        help="alpha for LoRA weight scaling, default 1", )
+    parser.add_argument("--network_dropout", type=float, default=None, )
+    parser.add_argument("--network_args", type=str, default=None, nargs="*", )
+    parser.add_argument("--dim_from_weights", action="store_true", )
+    parser.add_argument("--network_weights", type=str, default=None, help="pretrained weights for network")
+    parser.add_argument("--concept_image_folder", type=str,)
+    parser.add_argument("--num_ddim_steps", type=int, default=50)
+    parser.add_argument("--scheduler_linear_start", type=float, default=0.00085)
+    parser.add_argument("--scheduler_linear_end", type=float, default=0.012)
+    parser.add_argument("--scheduler_timesteps", type=int, default=1000)
+    parser.add_argument("--scheduler_schedule", type=str, default="scaled_linear")
+    parser.add_argument("--class_name", type=str, default="bagel")
+    parser.add_argument("--save_origin", action='store_true')
+    parser.add_argument("--only_zero_save", action='store_true')
+    parser.add_argument("--truncate_pad", action='store_true')
+    parser.add_argument("--truncate_length", type=int, default=2)
+    parser.add_argument("--only_normal_infer", action='store_true')
+    parser.add_argument("--latent_diff_thred", type=float, default=0.5)
+    parser.add_argument("--anormal_thred", type=float, default=0.5)
+    parser.add_argument("--detection_network_weights", type=str, )
+    parser.add_argument("--training_test", action = 'store_true')
+    import ast
+    def arg_as_list(arg):
+        v = ast.literal_eval(arg)
+        if type(v) is not list:
+            raise argparse.ArgumentTypeError("Argument \"%s\" is not a list" % (arg))
+        return v
+    parser.add_argument("--trg_layer", type=str)
+    parser.add_argument("--trg_layer_list", type=arg_as_list, )
+    parser.add_argument("--trg_lora_epoch", type=str)
+    parser.add_argument("--guidance_scale", type=float, default=8.5)
+    parser.add_argument("--prompt", type=str, default='teddy bear, wearing like a super hero')
+    parser.add_argument("--negative_prompt", type=str)
+    args = parser.parse_args()
+    main(args)
---
+++ script/recon_test.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+
+class_name="bagel"
+folder_name="res_64_down_1_task_loss_mahal_dist_attn_loss_0.008_actdeact_mahal_anomal"
+data_name="MVTec3D-AD"
+normality_folder='normal'
+network_weight_folder="../result/${data_name}_experiment/${class_name}/lora_training/${normality_folder}/${folder_name}/models/"
+
+img_folder="../../../MyData/anomaly_detection/${data_name}/${class_name}"
+port_number=50015
+
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config --main_process_port ${port_number} ../lora/reconstruction.py \
+  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
+  --sample_sampler ddim \
+  --seed 42 \
+  --concept_image_folder "${img_folder}" \
+  --resolution '512,512' \
+  --network_module networks.lora \
+  --network_dim 64 \
+  --network_alpha 4 \
+  --network_weights ${network_weight_folder} \
+  --detection_network_weights ${detection_network_weights} \
+  --num_ddim_steps 30 \
+  --prompt 'good' \
+  --negative_prompt "low quality, worst quality, bad anatomy, bad composition, poor, low effort" \
+  --guidance_scale 8.5 \
+  --truncate_length 2 \
+  --only_zero_save \
+  --class_name ${class_name} \
+  --anormal_thred 0.5 \
+  --trg_layer "down_blocks_0_attentions_1_transformer_blocks_0_attn2" \
+  --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2']"
