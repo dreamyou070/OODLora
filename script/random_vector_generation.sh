cb49300d53c7052c44ab962494193775994036f9
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-03 23:16:36 +0900

    Update random_vector_generation.sh

--- script/random_vector_generation.sh
+++ script/random_vector_generation.sh
@@ -6,7 +6,7 @@ all_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/tra
 normal_folder='normal'
 save_folder="random_vector_generating"
 output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
-network_weights="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/res_64_down_1_task_loss_mahal_dist_attn_loss_0.008_actdeact_mahal_anomal/models/epoch-000007.safetensors"
+network_weights="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/res_64_down_1_task_loss_mahal_dist_attn_loss_0.008_actdeact_mahal_anomal/models/epoch-000002.safetensors"
 
 start_epoch=0
 port_number=59518
