00a1710c1fa89a237dc2e4d0d49a88eef90f7144
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-09 13:54:23 +0900

    Update train_sy.sh

--- script/train_sy.sh
+++ script/train_sy.sh
@@ -11,7 +11,7 @@ network_weights="../result/MVTec3D-AD_experiment/${class_name}/lora_training/${n
 start_epoch=0
 port_number=58860
 
-NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
   --main_process_port $port_number ../lora/train_sy.py --process_title parksooyeon \
   --wandb_init_name ${class_name} --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
@@ -20,7 +20,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_con
   --optimizer_type AdamW --lr_scheduler cosine_with_restarts \
   --lr_warmup_steps 144 --learning_rate 0.0003 --unet_lr 0.0001 --text_encoder_lr 0.00005 \
   --save_every_n_epochs 1 --sample_every_n_epochs 1 --sample_prompts ../../../MyData/anomaly_detection/inference.txt \
-  --start_epoch $start_epoch --max_train_steps 500 --max_train_epochs 500 \
+  --start_epoch $start_epoch --max_train_steps 10 --max_train_epochs 10 \
   --train_data_dir "$train_data_dir" --resolution '512,512' --class_caption 'good' \
   --output_dir "$output_dir" \
   --do_task_loss --task_loss_weight 1.0 \
