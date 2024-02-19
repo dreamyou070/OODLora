645f26a3ff85e61a0f262e07f40d91c85626a3fe
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-19 11:54:14 +0900

    Update train_anomal.sh

--- script/train_anomal.sh
+++ script/train_anomal.sh
@@ -11,7 +11,7 @@ start_epoch=0
 port_number=58875
 
 NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
-  --main_process_port $port_number ../lora/train_anomal.py --process_title parksooyeon \
+  --main_process_port $port_number ../lora/train_anormal.py --process_title parksooyeon \
   --wandb_init_name ${class_name} --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
   --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
   --network_module networks.lora --network_dim 64 --network_alpha 4 \
