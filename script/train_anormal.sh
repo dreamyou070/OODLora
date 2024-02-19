3387ad1917657aa8d444bc814b2fb515df18cebb
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-19 12:09:56 +0900

    Update train_anormal.sh

---
+++ script/train_anormal.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+class_name="rope"
+data_source='train_ex'
+data_folder='MVTec3D-AD'
+normal_folder='anormal_20240219'
+save_folder="anormal_20240219_code_test"
+train_data_dir="../../../MyData/anomaly_detection/${data_folder}/${class_name}/${data_source}/rgb"
+output_dir="../result/${data_folder}_experiment/${class_name}/lora_training/${normal_folder}/${save_folder}"
+
+start_epoch=0
+port_number=57774
+
+NCCL_P2P_DISABLE=1 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
+  --main_process_port $port_number ../lora/train_anormal.py \
+  --process_title parksooyeon \
+  --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc  \
+  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
+  --network_module networks.lora \
+  --network_dim 64 --network_alpha 4 --train_batch_size 1 \
+  --optimizer_type AdamW --lr_scheduler cosine_with_restarts \
+  --lr_warmup_steps 144 --learning_rate 0.0003 --unet_lr 0.0001 --text_encoder_lr 0.00005 --resolution '512,512' --save_every_n_epochs 1 \
+  --sample_every_n_epochs 1 \
+  --sample_prompts ../../../MyData/anomaly_detection/inference.txt \
+  --max_train_steps 1000 --attn_loss --task_loss_weight 1.0 --seed 42 --class_caption 'good' \
+  --max_train_epochs 500 \
+  --train_data_dir ${train_data_dir} \
+  --start_epoch $start_epoch \
+  --output_dir "$output_dir" \
+  --cross_map_res "[64]" \
+  --detail_64_up \
+  --trg_position "['up']" \
+  --trg_part '["attn_2"]' \
+  --attn_loss_weight 0.001 \
+  --cls_training
